da69feff6e55643c09a8a5133ea06750cca1318e
austinobombino <hainesaustinb@gmail.com>
2023-03-30 16:32:20 -0700

    params

--- x/commitment/types/params.pb.go
+++ x/commitment/types/params.pb.go
@@ -25,6 +25,7 @@ const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 // Params defines the parameters for the module.
 type Params struct {
+	VestingInfos []*VestingInfo `protobuf:"bytes,1,rep,name=vesting_infos,json=vestingInfos,proto3" json:"vesting_infos,omitempty"`
 }
 
 func (m *Params) Reset()      { *m = Params{} }
@@ -59,24 +60,109 @@ func (m *Params) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Params proto.InternalMessageInfo
 
+func (m *Params) GetVestingInfos() []*VestingInfo {
+	if m != nil {
+		return m.VestingInfos
+	}
+	return nil
+}
+
+type VestingInfo struct {
+	BaseDenom       string `protobuf:"bytes,1,opt,name=base_denom,json=baseDenom,proto3" json:"base_denom,omitempty"`
+	VestingDenom    string `protobuf:"bytes,2,opt,name=vesting_denom,json=vestingDenom,proto3" json:"vesting_denom,omitempty"`
+	EpochIdentifier string `protobuf:"bytes,3,opt,name=epoch_identifier,json=epochIdentifier,proto3" json:"epoch_identifier,omitempty"`
+	NumEpochs       int64  `protobuf:"varint,4,opt,name=num_epochs,json=numEpochs,proto3" json:"num_epochs,omitempty"`
+}
+
+func (m *VestingInfo) Reset()         { *m = VestingInfo{} }
+func (m *VestingInfo) String() string { return proto.CompactTextString(m) }
+func (*VestingInfo) ProtoMessage()    {}
+func (*VestingInfo) Descriptor() ([]byte, []int) {
+	return fileDescriptor_92e317feaf73ff7e, []int{1}
+}
+func (m *VestingInfo) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *VestingInfo) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_VestingInfo.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *VestingInfo) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_VestingInfo.Merge(m, src)
+}
+func (m *VestingInfo) XXX_Size() int {
+	return m.Size()
+}
+func (m *VestingInfo) XXX_DiscardUnknown() {
+	xxx_messageInfo_VestingInfo.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_VestingInfo proto.InternalMessageInfo
+
+func (m *VestingInfo) GetBaseDenom() string {
+	if m != nil {
+		return m.BaseDenom
+	}
+	return ""
+}
+
+func (m *VestingInfo) GetVestingDenom() string {
+	if m != nil {
+		return m.VestingDenom
+	}
+	return ""
+}
+
+func (m *VestingInfo) GetEpochIdentifier() string {
+	if m != nil {
+		return m.EpochIdentifier
+	}
+	return ""
+}
+
+func (m *VestingInfo) GetNumEpochs() int64 {
+	if m != nil {
+		return m.NumEpochs
+	}
+	return 0
+}
+
 func init() {
 	proto.RegisterType((*Params)(nil), "elysnetwork.elys.commitment.Params")
+	proto.RegisterType((*VestingInfo)(nil), "elysnetwork.elys.commitment.VestingInfo")
 }
 
 func init() { proto.RegisterFile("elys/commitment/params.proto", fileDescriptor_92e317feaf73ff7e) }
 
 var fileDescriptor_92e317feaf73ff7e = []byte{
-	// 157 bytes of a gzipped FileDescriptorProto
+	// 299 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x49, 0xcd, 0xa9, 0x2c,
 	0xd6, 0x4f, 0xce, 0xcf, 0xcd, 0xcd, 0x2c, 0xc9, 0x4d, 0xcd, 0x2b, 0xd1, 0x2f, 0x48, 0x2c, 0x4a,
 	0xcc, 0x2d, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x92, 0x06, 0xc9, 0xe6, 0xa5, 0x96, 0x94,
 	0xe7, 0x17, 0x65, 0xeb, 0x81, 0xd8, 0x7a, 0x08, 0x95, 0x52, 0x22, 0xe9, 0xf9, 0xe9, 0xf9, 0x60,
-	0x75, 0xfa, 0x20, 0x16, 0x44, 0x8b, 0x12, 0x1f, 0x17, 0x5b, 0x00, 0xd8, 0x08, 0x2b, 0x96, 0x19,
-	0x0b, 0xe4, 0x19, 0x9c, 0x3c, 0x4f, 0x3c, 0x92, 0x63, 0xbc, 0xf0, 0x48, 0x8e, 0xf1, 0xc1, 0x23,
-	0x39, 0xc6, 0x09, 0x8f, 0xe5, 0x18, 0x2e, 0x3c, 0x96, 0x63, 0xb8, 0xf1, 0x58, 0x8e, 0x21, 0x4a,
-	0x3f, 0x3d, 0xb3, 0x24, 0xa3, 0x34, 0x09, 0x64, 0xa4, 0x3e, 0xc8, 0x6c, 0x5d, 0xa8, 0x45, 0x60,
-	0x8e, 0x7e, 0x05, 0xb2, 0xa3, 0x4a, 0x2a, 0x0b, 0x52, 0x8b, 0x93, 0xd8, 0xc0, 0x36, 0x18, 0x03,
-	0x02, 0x00, 0x00, 0xff, 0xff, 0x98, 0xb9, 0x0e, 0xa7, 0xb4, 0x00, 0x00, 0x00,
+	0x75, 0xfa, 0x20, 0x16, 0x44, 0x8b, 0x52, 0x2c, 0x17, 0x5b, 0x00, 0xd8, 0x08, 0x21, 0x5f, 0x2e,
+	0xde, 0xb2, 0xd4, 0xe2, 0x92, 0xcc, 0xbc, 0xf4, 0xf8, 0xcc, 0xbc, 0xb4, 0xfc, 0x62, 0x09, 0x46,
+	0x05, 0x66, 0x0d, 0x6e, 0x23, 0x0d, 0x3d, 0x3c, 0x86, 0xea, 0x85, 0x41, 0x74, 0x78, 0xe6, 0xa5,
+	0xe5, 0x07, 0xf1, 0x94, 0x21, 0x38, 0xc5, 0x56, 0x2c, 0x33, 0x16, 0xc8, 0x33, 0x28, 0xcd, 0x66,
+	0xe4, 0xe2, 0x46, 0x52, 0x23, 0x24, 0xcb, 0xc5, 0x95, 0x94, 0x58, 0x9c, 0x1a, 0x9f, 0x92, 0x9a,
+	0x97, 0x9f, 0x2b, 0xc1, 0xa8, 0xc0, 0xa8, 0xc1, 0x19, 0xc4, 0x09, 0x12, 0x71, 0x01, 0x09, 0x08,
+	0x29, 0x23, 0xdc, 0x00, 0x51, 0xc1, 0x04, 0x56, 0x01, 0x33, 0x19, 0xa2, 0x48, 0x93, 0x4b, 0x20,
+	0xb5, 0x20, 0x3f, 0x39, 0x23, 0x3e, 0x33, 0x25, 0x35, 0xaf, 0x24, 0x33, 0x2d, 0x33, 0xb5, 0x48,
+	0x82, 0x19, 0xac, 0x8e, 0x1f, 0x2c, 0xee, 0x09, 0x17, 0x06, 0x59, 0x97, 0x57, 0x9a, 0x1b, 0x0f,
+	0x16, 0x2e, 0x96, 0x60, 0x51, 0x60, 0xd4, 0x60, 0x0e, 0xe2, 0xcc, 0x2b, 0xcd, 0x75, 0x05, 0x0b,
+	0x38, 0x79, 0x9e, 0x78, 0x24, 0xc7, 0x78, 0xe1, 0x91, 0x1c, 0xe3, 0x83, 0x47, 0x72, 0x8c, 0x13,
+	0x1e, 0xcb, 0x31, 0x5c, 0x78, 0x2c, 0xc7, 0x70, 0xe3, 0xb1, 0x1c, 0x43, 0x94, 0x7e, 0x7a, 0x66,
+	0x49, 0x46, 0x69, 0x12, 0xc8, 0xab, 0xfa, 0x20, 0x3f, 0xeb, 0x42, 0x03, 0x00, 0xcc, 0xd1, 0xaf,
+	0x40, 0x8e, 0x81, 0x92, 0xca, 0x82, 0xd4, 0xe2, 0x24, 0x36, 0x70, 0x70, 0x1a, 0x03, 0x02, 0x00,
+	0x00, 0xff, 0xff, 0x19, 0xca, 0x7b, 0x51, 0xa1, 0x01, 0x00, 0x00,
 }
 
 func (m *Params) Marshal() (dAtA []byte, err error) {
@@ -99,6 +185,69 @@ func (m *Params) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
+	if len(m.VestingInfos) > 0 {
+		for iNdEx := len(m.VestingInfos) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.VestingInfos[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintParams(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0xa
+		}
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *VestingInfo) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *VestingInfo) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *VestingInfo) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.NumEpochs != 0 {
+		i = encodeVarintParams(dAtA, i, uint64(m.NumEpochs))
+		i--
+		dAtA[i] = 0x20
+	}
+	if len(m.EpochIdentifier) > 0 {
+		i -= len(m.EpochIdentifier)
+		copy(dAtA[i:], m.EpochIdentifier)
+		i = encodeVarintParams(dAtA, i, uint64(len(m.EpochIdentifier)))
+		i--
+		dAtA[i] = 0x1a
+	}
+	if len(m.VestingDenom) > 0 {
+		i -= len(m.VestingDenom)
+		copy(dAtA[i:], m.VestingDenom)
+		i = encodeVarintParams(dAtA, i, uint64(len(m.VestingDenom)))
+		i--
+		dAtA[i] = 0x12
+	}
+	if len(m.BaseDenom) > 0 {
+		i -= len(m.BaseDenom)
+		copy(dAtA[i:], m.BaseDenom)
+		i = encodeVarintParams(dAtA, i, uint64(len(m.BaseDenom)))
+		i--
+		dAtA[i] = 0xa
+	}
 	return len(dAtA) - i, nil
 }
 
@@ -119,6 +268,36 @@ func (m *Params) Size() (n int) {
 	}
 	var l int
 	_ = l
+	if len(m.VestingInfos) > 0 {
+		for _, e := range m.VestingInfos {
+			l = e.Size()
+			n += 1 + l + sovParams(uint64(l))
+		}
+	}
+	return n
+}
+
+func (m *VestingInfo) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.BaseDenom)
+	if l > 0 {
+		n += 1 + l + sovParams(uint64(l))
+	}
+	l = len(m.VestingDenom)
+	if l > 0 {
+		n += 1 + l + sovParams(uint64(l))
+	}
+	l = len(m.EpochIdentifier)
+	if l > 0 {
+		n += 1 + l + sovParams(uint64(l))
+	}
+	if m.NumEpochs != 0 {
+		n += 1 + sovParams(uint64(m.NumEpochs))
+	}
 	return n
 }
 
@@ -157,6 +336,205 @@ func (m *Params) Unmarshal(dAtA []byte) error {
 			return fmt.Errorf("proto: Params: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field VestingInfos", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowParams
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthParams
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthParams
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.VestingInfos = append(m.VestingInfos, &VestingInfo{})
+			if err := m.VestingInfos[len(m.VestingInfos)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipParams(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthParams
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *VestingInfo) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowParams
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: VestingInfo: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: VestingInfo: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field BaseDenom", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowParams
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthParams
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthParams
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.BaseDenom = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field VestingDenom", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowParams
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthParams
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthParams
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.VestingDenom = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 3:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field EpochIdentifier", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowParams
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthParams
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthParams
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.EpochIdentifier = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 4:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field NumEpochs", wireType)
+			}
+			m.NumEpochs = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowParams
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.NumEpochs |= int64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
 		default:
 			iNdEx = preIndex
 			skippy, err := skipParams(dAtA[iNdEx:])
