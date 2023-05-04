8b5a7ec70eb9897289688eabbfbc93a105d6936c
jelysn <jun@elys.network>
2023-05-04 19:49:30 +0800

    complete dockernet test setup

--- dockernet/tests/check.sh
+++ dockernet/tests/check.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+set -eux
 
-elysd query oracle list-price-feeder
-elysd query oracle list-price
-elysd query epochs epoch-infos
-elysd query oracle params
-elysd query oracle list-asset-info
+source "./assert.sh"
 
-elysd query oracle show-price BTC --source=band
-elysd query oracle show-price BTC --source=elys
+# elysd query oracle list-price-feeder
+# elysd query oracle list-price
+# elysd query epochs epoch-infos
+# elysd query oracle params
+# elysd query oracle list-asset-info
+
+# elysd query oracle show-price BTC --source=band
+# elysd query oracle show-price BTC --source=elys
+
+band_price=$(elysd query oracle show-price BTC --source=band)
+assert_contain "$band_price" "asset: BTC" "band price check failure"
+
+elys_price=$(elysd query oracle show-price BTC --source=binance)
+assert_contain "$elys_price" "asset: BTC" "elys price check failure"
+echo "All passed"
