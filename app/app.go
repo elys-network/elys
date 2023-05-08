ab62963da23cf02b2455a068879c7c3ed4404449
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2023-05-08 18:38:29 +0200

    fix: 🐛 duplicate maccperms

--- app/app.go
+++ app/app.go
@@ -206,7 +206,6 @@ var (
 		govtypes.ModuleName:              {authtypes.Burner},
 		ibctransfertypes.ModuleName:      {authtypes.Minter, authtypes.Burner},
 		commitmentmoduletypes.ModuleName: {authtypes.Minter, authtypes.Burner},
-		incentivemoduletypes.ModuleName:  {authtypes.Minter, authtypes.Burner},
 		burnermoduletypes.ModuleName:     {authtypes.Burner},
 		incentivemoduletypes.ModuleName:  nil,
 		// this line is used by starport scaffolding # stargate/app/maccPerms
