94d8e62299e00800add5fbfe0c6104cc072f9b41
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2023-05-18 11:27:31 +0200

    feat: 🎸 add pool map

--- x/amm/types/genesis.go
+++ x/amm/types/genesis.go
@@ -1,7 +1,7 @@
 package types
 
 import (
-// this line is used by starport scaffolding # genesis/types/import
+	"fmt"
 )
 
 // DefaultIndex is the default global index
@@ -10,6 +10,7 @@ const DefaultIndex uint64 = 1
 // DefaultGenesis returns the default genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
+		PoolList: []Pool{},
 		// this line is used by starport scaffolding # genesis/types/default
 		Params: DefaultParams(),
 	}
@@ -18,6 +19,16 @@ func DefaultGenesis() *GenesisState {
 // Validate performs basic genesis state validation returning an error upon any
 // failure.
 func (gs GenesisState) Validate() error {
+	// Check for duplicated index in pool
+	poolIndexMap := make(map[string]struct{})
+
+	for _, elem := range gs.PoolList {
+		index := string(PoolKey(elem.PoolId))
+		if _, ok := poolIndexMap[index]; ok {
+			return fmt.Errorf("duplicated index for pool")
+		}
+		poolIndexMap[index] = struct{}{}
+	}
 	// this line is used by starport scaffolding # genesis/types/validate
 
 	return gs.Params.Validate()
