3d8d2de4b69337dd5ca64824d20141c76a9f73d8
jelysn <jun@elys.network>
2023-05-23 10:21:19 +0800

    Write basic unit test scenario for join/swap/exit

--- x/gamm/keeper/msg_server_test.go
+++ x/gamm/keeper/msg_server_test.go
@@ -356,3 +356,167 @@ func (suite *KeeperTestSuite) TestExitPool_Events() {
 		})
 	}
 }
+
+// TODO: implement
+// func (server msgServer) JoinPool(goCtx context.Context, msg *types.MsgJoinPool)
+// Oracle based pool - JoinPool
+// 1) scenario1
+// - JUNO/USDC pool
+// - JUNO price $1
+// - $1000 JUNO / $1000 USDC
+// - External liquidity on Osmosis $10000 JUNO / $10000 USDC
+// - Slippage reduction: 90%
+// - Target Weight 50% / 50%
+// - Weight Broken Threshold: 20%
+// - Join with JUNO token
+// - Check weight breaking fee
+// - Check slippage
+// - Check bonus being zero
+// 2) scenario2
+// - JUNO/USDC pool
+// - JUNO price $1
+// - $1000 JUNO / $1000 USDC
+// - External liquidity on Osmosis $10000 JUNO / $10000 USDC
+// - Slippage reduction: 90%
+// - Target Weight 50% / 50%
+// - Weight Broken Threshold: 20%
+// - Join with JUNO+USDC token ($50 JUNO /$50 USDC)
+// - Check weight breaking fee
+// - Check slippage
+// - Check bonus being zero
+// 3) scenario3
+// - JUNO/USDC pool
+// - JUNO price $1
+// - $500 JUNO / $1500 USDC
+// - External liquidity on Osmosis $10000 JUNO / $10000 USDC
+// - Slippage reduction: 90%
+// - Target Weight 50% / 50%
+// - Weight Broken Threshold: 20%
+// - Join JUNO ($50)
+// - Check weight breaking fee zero
+// - Check slippage
+// - Check bonus not be zero
+// 4) scenario4
+// - JUNO/USDC pool
+// - JUNO price $1
+// - $500 JUNO / $1500 USDC
+// - External liquidity on Osmosis $10000 JUNO / $10000 USDC
+// - Slippage reduction: 90%
+// - Target Weight 50% / 50%
+// - Weight Broken Threshold: 20%
+// - Join USDC ($50)
+// - Check weight breaking fee
+// - Check slippage
+// - Check bonus be zero
+// 5) scenario5
+// - JUNO/USDC pool
+// - JUNO price $1
+// - $500 JUNO / $1500 USDC
+// - External liquidity on Osmosis $10000 JUNO / $10000 USDC
+// - Slippage reduction: 90%
+// - Target Weight 50% / 50%
+// - Weight Broken Threshold: 20%
+// - Join JUNO+USDC ($50 JUNO /$50 USDC)
+// - Check weight breaking fee
+// - Check slippage
+// - Check bonus be zero
+
+// func (server msgServer) SwapExactAmountIn(goCtx context.Context, msg *types.MsgSwapExactAmountIn)
+// Oracle based pool - Swap
+// 1) scenario1
+// - JUNO/USDC pool
+// - JUNO price $1
+// - $1000 JUNO / $1000 USDC
+// - External liquidity on Osmosis $10000 JUNO / $10000 USDC
+// - Slippage reduction: 90%
+// - Target Weight 50% / 50%
+// - Weight Broken Threshold: 20%
+// - Swap JUNO -> USDC
+// - Check weight breaking fee
+// - Check slippage
+// - Check bonus being zero
+// 2) scenario2
+// - JUNO/USDC pool
+// - JUNO price $1
+// - $500 JUNO / $1500 USDC
+// - External liquidity on Osmosis $10000 JUNO / $10000 USDC
+// - Slippage reduction: 90%
+// - Target Weight 50% / 50%
+// - Weight Broken Threshold: 20%
+// - Swap JUNO -> USDC
+// - Check weight breaking fee zero
+// - Check slippage
+// - Check bonus not be zero
+// 3) scenario3
+// - JUNO/USDC pool
+// - JUNO price $1
+// - $500 JUNO / $1500 USDC
+// - External liquidity on Osmosis $10000 JUNO / $10000 USDC
+// - Slippage reduction: 90%
+// - Target Weight 50% / 50%
+// - Weight Broken Threshold: 20%
+// - Swap USDC -> JUNO
+// - Check weight breaking fee
+// - Check slippage
+// - Check bonus be zero
+
+// func (server msgServer) ExitPool(goCtx context.Context, msg *types.MsgExitPool)
+// Oracle based pool - Exit
+// 1) scenario1
+// - JUNO/USDC pool
+// - JUNO price $1
+// - $1000 JUNO / $1000 USDC
+// - External liquidity on Osmosis $10000 JUNO / $10000 USDC
+// - Slippage reduction: 90%
+// - Target Weight 50% / 50%
+// - Weight Broken Threshold: 20%
+// - Exit JUNO
+// - Check weight breaking fee
+// - Check slippage
+// 2) scenario2
+// - JUNO/USDC pool
+// - JUNO price $1
+// - $1000 JUNO / $1000 USDC
+// - External liquidity on Osmosis $10000 JUNO / $10000 USDC
+// - Slippage reduction: 90%
+// - Target Weight 50% / 50%
+// - Weight Broken Threshold: 20%
+// - Exit JUNO+USDC
+// - Check weight breaking fee
+// - Check slippage
+// 3) scenario3
+// - JUNO/USDC pool
+// - JUNO price $1
+// - $500 JUNO / $1500 USDC
+// - External liquidity on Osmosis $10000 JUNO / $10000 USDC
+// - Slippage reduction: 90%
+// - Target Weight 50% / 50%
+// - Weight Broken Threshold: 20%
+// - Exit JUNO
+// - Check weight breaking fee zero
+// - Check slippage
+// 4) scenario4
+// - JUNO/USDC pool
+// - JUNO price $1
+// - $500 JUNO / $1500 USDC
+// - External liquidity on Osmosis $10000 JUNO / $10000 USDC
+// - Slippage reduction: 90%
+// - Target Weight 50% / 50%
+// - Weight Broken Threshold: 20%
+// - Exit USDC
+// - Check weight breaking fee
+// - Check slippage
+// 5) scenario5
+// - JUNO/USDC pool
+// - JUNO price $1
+// - $500 JUNO / $1500 USDC
+// - External liquidity on Osmosis $10000 JUNO / $10000 USDC
+// - Slippage reduction: 90%
+// - Target Weight 50% / 50%
+// - Weight Broken Threshold: 20%
+// - Exit JUNO+USDC
+// - Check weight breaking fee
+// - Check slippage
+
+// TODO: handle case bonus pool does not have enough amount
+// TODO: check fee distribution
