9046c725600d47c26a094e3022ba7117f447217b
jelysn <jun@elys.network>
2023-06-21 15:53:56 +0800

    remove unrequired commented code on test

--- x/amm/keeper/msg_server_swap_exact_amount_in_test.go
+++ x/amm/keeper/msg_server_swap_exact_amount_in_test.go
@@ -224,28 +224,3 @@ func (suite *KeeperTestSuite) TestMsgServerSwapExactAmountIn() {
 		})
 	}
 }
-
-// func (k msgServer) SwapExactAmountIn(goCtx context.Context, msg *types.MsgSwapExactAmountIn) (*types.MsgSwapExactAmountInResponse, error) {
-// 	ctx := sdk.UnwrapSDKContext(goCtx)
-
-// 	sender, err := sdk.AccAddressFromBech32(msg.Sender)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	tokenOutAmount, err := k.RouteExactAmountIn(ctx, sender, msg.Routes, msg.TokenIn, math.Int(msg.TokenOutMinAmount))
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	// Swap event is handled elsewhere
-// 	ctx.EventManager().EmitEvents(sdk.Events{
-// 		sdk.NewEvent(
-// 			sdk.EventTypeMessage,
-// 			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
-// 			sdk.NewAttribute(sdk.AttributeKeySender, msg.Sender),
-// 		),
-// 	})
-
-// 	return &types.MsgSwapExactAmountInResponse{TokenOutAmount: tokenOutAmount}, nil
-// }
