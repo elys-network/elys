9318ad5b78ba7afd2035c89e2fa90f49702c9c0d
jelysn <jun@elys.network>
2023-08-22 10:25:20 +0800

    filter invalid swap requests on msg server handler step

--- x/amm/keeper/msg_server_swap_exact_amount_out.go
+++ x/amm/keeper/msg_server_swap_exact_amount_out.go
@@ -10,29 +10,30 @@ import (
 func (k msgServer) SwapExactAmountOut(goCtx context.Context, msg *types.MsgSwapExactAmountOut) (*types.MsgSwapExactAmountOutResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
+	sender, err := sdk.AccAddressFromBech32(msg.Sender)
+	if err != nil {
+		return nil, err
+	}
+
+	// Try executing the tx on cached context environment, to filter invalid transactions out
+	cacheCtx, _ := ctx.CacheContext()
+	tokenInAmount, err := k.RouteExactAmountOut(cacheCtx, sender, msg.Routes, msg.TokenInMaxAmount, msg.TokenOut)
+	if err != nil {
+		return nil, err
+	}
+
 	lastSwapIndex := k.GetLastSwapRequestIndex(ctx)
 	k.SetSwapExactAmountOutRequests(ctx, msg, lastSwapIndex+1)
 	k.SetLastSwapRequestIndex(ctx, lastSwapIndex+1)
-	return &types.MsgSwapExactAmountOutResponse{}, nil
 
-	// sender, err := sdk.AccAddressFromBech32(msg.Sender)
-	// if err != nil {
-	// 	return nil, err
-	// }
+	// Swap event is handled elsewhere
+	ctx.EventManager().EmitEvents(sdk.Events{
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
+			sdk.NewAttribute(sdk.AttributeKeySender, msg.Sender),
+		),
+	})
 
-	// tokenInAmount, err := k.RouteExactAmountOut(ctx, sender, msg.Routes, msg.TokenInMaxAmount, msg.TokenOut)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// // Swap event is handled elsewhere
-	// ctx.EventManager().EmitEvents(sdk.Events{
-	// 	sdk.NewEvent(
-	// 		sdk.EventTypeMessage,
-	// 		sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
-	// 		sdk.NewAttribute(sdk.AttributeKeySender, msg.Sender),
-	// 	),
-	// })
-
-	// return &types.MsgSwapExactAmountOutResponse{TokenInAmount: tokenInAmount}, nil
+	return &types.MsgSwapExactAmountOutResponse{TokenInAmount: tokenInAmount}, nil
 }
