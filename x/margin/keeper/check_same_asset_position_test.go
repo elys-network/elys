93fbf4889e4835f0ff1c8083d3b544a87c66a29f
kenta-elys <kenta@elys.network>
2023-09-13 12:50:39 +0000

    fix: go mod that breaks build

--- x/margin/keeper/check_same_asset_position_test.go
+++ x/margin/keeper/check_same_asset_position_test.go
@@ -5,11 +5,13 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/elys-network/elys/x/margin/keeper"
+	"github.com/elys-network/elys/x/margin/types"
 	"github.com/elys-network/elys/x/margin/types/mocks"
+	ptypes "github.com/elys-network/elys/x/parameter/types"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCheckSameAssets_OpenPositionsBelowMax(t *testing.T) {
+func TestCheckSameAssets_NewPosition(t *testing.T) {
 	// Setup the mock checker
 	mockChecker := new(mocks.PositionChecker)
 
@@ -19,14 +21,21 @@ func TestCheckSameAssets_OpenPositionsBelowMax(t *testing.T) {
 	}
 
 	ctx := sdk.Context{} // mock or setup a context
+	mtp := types.NewMTP("creator", ptypes.USDC, ptypes.ATOM, types.Position_LONG, sdk.NewDec(5), 1)
+	k.SetMTP(ctx, mtp)
 
-	// Mock behavior
-	mockChecker.On("GetOpenMTPCount", ctx).Return(uint64(5))
-	mockChecker.On("GetMaxOpenPositions", ctx).Return(uint64(10))
+	msg := &types.MsgOpen{
+		Creator:          "creator",
+		CollateralAsset:  ptypes.ATOM,
+		CollateralAmount: sdk.NewInt(100),
+		BorrowAsset:      ptypes.ATOM,
+		Position:         types.Position_SHORT,
+		Leverage:         sdk.NewDec(1),
+	}
 
-	err := k.CheckMaxOpenPositions(ctx)
+	mtp = k.CheckSamePosition(ctx, msg)
 
 	// Expect no error
-	assert.Nil(t, err)
+	assert.Nil(t, mtp)
 	mockChecker.AssertExpectations(t)
 }
