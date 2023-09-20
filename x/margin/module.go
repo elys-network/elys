10d3056dcad7dfc69001a86c6e11fa7e81378d95
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2023-09-20 16:06:23 +0200

    fix: missing arg

--- x/margin/module.go
+++ x/margin/module.go
@@ -147,8 +147,8 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 func (AppModule) ConsensusVersion() uint64 { return 2 }
 
 // BeginBlock contains the logic that is automatically triggered at the beginning of each block
-func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {
-	am.keeper.BeginBlocker()
+func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {
+	am.keeper.BeginBlocker(ctx)
 }
 
 // EndBlock contains the logic that is automatically triggered at the end of each block
