db833655588c466bcf958469f8212fab6545202b
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2023-09-20 12:33:46 +0200

    feat: add abci logic to margin module

--- x/margin/module.go
+++ x/margin/module.go
@@ -147,7 +147,9 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 func (AppModule) ConsensusVersion() uint64 { return 2 }
 
 // BeginBlock contains the logic that is automatically triggered at the beginning of each block
-func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {}
+func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {
+	am.keeper.BeginBlocker()
+}
 
 // EndBlock contains the logic that is automatically triggered at the end of each block
 func (am AppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
