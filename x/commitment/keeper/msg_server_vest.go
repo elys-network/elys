b1e0d25d5a5537a4329829ec3cd81c341765c2ca
kenta-elys <kenta@elys.network>
2023-09-26 08:47:09 +0000

    chore: set max cap in creating vestings per user

--- x/commitment/keeper/msg_server_vest.go
+++ x/commitment/keeper/msg_server_vest.go
@@ -18,13 +18,22 @@ func (k msgServer) Vest(goCtx context.Context, msg *types.MsgVest) (*types.MsgVe
 		return nil, sdkerrors.Wrapf(types.ErrInvalidDenom, "denom: %s", msg.Denom)
 	}
 
+	commitments, found := k.GetCommitments(ctx, msg.Creator)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrCommitmentsNotFound, "creator: %s", msg.Creator)
+	}
+
+	// Create vesting tokens entry and add to commitments
+	vestingTokens := commitments.GetVestingTokens()
+	if vestingInfo.NumMaxVestings <= (int64)(len(vestingTokens)) {
+		return nil, sdkerrors.Wrapf(types.ErrExceedMaxVestings, "creator: %s", msg.Creator)
+	}
+
 	commitments, err := k.DeductCommitments(ctx, msg.Creator, msg.Denom, msg.Amount)
 	if err != nil {
 		return nil, err
 	}
 
-	// Create vesting tokens entry and add to commitments
-	vestingTokens := commitments.GetVestingTokens()
 	vestingTokens = append(vestingTokens, &types.VestingTokens{
 		Denom:           vestingInfo.VestingDenom,
 		TotalAmount:     msg.Amount,
