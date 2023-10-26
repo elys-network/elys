785fd1c3704232e727b08a74d383cee63fbeac99
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2023-10-26 12:07:04 +0200

    feat: add margin open take profit price flag

--- x/margin/client/cli/tx_open.go
+++ x/margin/client/cli/tx_open.go
@@ -16,10 +16,13 @@ var _ = strconv.Itoa(0)
 
 func CmdOpen() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "open [position] [leverage] [collateral-asset] [collateral-amount] [borrow-asset] [flags]",
-		Short:   "Open margin position",
-		Example: `elysd tx margin open long 5 uusdc 100000000 uatom --from=treasury --keyring-backend=test --chain-id=elystestnet-1 --yes --gas=1000000`,
-		Args:    cobra.ExactArgs(5),
+		Use:   "open [position] [leverage] [collateral-asset] [collateral-amount] [borrow-asset] [flags]",
+		Short: "Open margin position",
+		Example: `Infinte profitability:
+elysd tx margin open long 5 uusdc 100000000 uatom --from=treasury --keyring-backend=test --chain-id=elystestnet-1 --yes --gas=1000000
+Finite profitability:
+elysd tx margin open short 5 uusdc 100000000 uatom --take-profit 100 --from=treasury --keyring-backend=test --chain-id=elystestnet-1 --yes --gas=1000000`,
+		Args: cobra.ExactArgs(5),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
@@ -47,6 +50,24 @@ func CmdOpen() *cobra.Command {
 
 			argBorrowAsset := args[4]
 
+			takeProfitPriceStr, err := cmd.Flags().GetString("take-profit")
+			if err != nil {
+				return err
+			}
+
+			var takeProfitPrice sdk.Dec
+			if takeProfitPriceStr != types.InfinitePriceString {
+				takeProfitPrice, err = sdk.NewDecFromStr(takeProfitPriceStr)
+				if err != nil {
+					return errors.New("invalid take profit price")
+				}
+			} else {
+				takeProfitPrice, err = sdk.NewDecFromStr(types.TakeProfitPriceDefault)
+				if err != nil {
+					return errors.New("failed to set default take profit price")
+				}
+			}
+
 			msg := types.NewMsgOpen(
 				signer.String(),
 				argCollateralAsset,
@@ -54,6 +75,7 @@ func CmdOpen() *cobra.Command {
 				argBorrowAsset,
 				argPosition,
 				argLeverage,
+				takeProfitPrice,
 			)
 
 			if err := msg.ValidateBasic(); err != nil {
@@ -62,6 +84,8 @@ func CmdOpen() *cobra.Command {
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
 		},
 	}
+
+	cmd.Flags().String("take-profit", types.InfinitePriceString, "Optional take profit price")
 
 	flags.AddTxFlagsToCmd(cmd)
 
