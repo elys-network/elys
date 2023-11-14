cb3db31975c2dbbe25f4050c7e3f6c8a9179f32e
jelysn <jun@elys.network>
2023-11-14 20:02:34 +0800

    remove not required performMsgStakeElys and  performMsgCommit

--- x/commitment/client/wasm/msg_stake.go
+++ x/commitment/client/wasm/msg_stake.go
@@ -6,13 +6,8 @@ import (
 	errorsmod "cosmossdk.io/errors"
 	wasmvmtypes "github.com/CosmWasm/wasmvm/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
-	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
-	wasmbindingstypes "github.com/elys-network/elys/wasmbindings/types"
 	commitmentkeeper "github.com/elys-network/elys/x/commitment/keeper"
 	commitmenttypes "github.com/elys-network/elys/x/commitment/types"
-	types "github.com/elys-network/elys/x/commitment/types"
-	paramtypes "github.com/elys-network/elys/x/parameter/types"
 )
 
 func (m *Messenger) msgStake(ctx sdk.Context, contractAddr sdk.AccAddress, msgStake *commitmenttypes.MsgStake) ([]sdk.Event, [][]byte, error) {
@@ -40,63 +35,4 @@ func (m *Messenger) msgStake(ctx sdk.Context, contractAddr sdk.AccAddress, msgSt
 	resp := [][]byte{responseBytes}
 
 	return nil, resp, nil
-}
-
-func performMsgStakeElys(f *stakingkeeper.Keeper, ctx sdk.Context, contractAddr sdk.AccAddress, msgStake *types.MsgStake) (*wasmbindingstypes.RequestResponse, error) {
-	if msgStake == nil {
-		return nil, wasmvmtypes.InvalidRequest{Err: "Invalid staking parameter"}
-	}
-
-	msgServer := stakingkeeper.NewMsgServerImpl(f)
-	address, err := sdk.AccAddressFromBech32(msgStake.Creator)
-	if err != nil {
-		return nil, errorsmod.Wrap(err, "invalid address")
-	}
-
-	validator_address, err := sdk.ValAddressFromBech32(msgStake.ValidatorAddress)
-	if err != nil {
-		return nil, errorsmod.Wrap(err, "invalid address")
-	}
-
-	amount := sdk.NewCoin(msgStake.Asset, msgStake.Amount)
-	msgMsgDelegate := stakingtypes.NewMsgDelegate(address, validator_address, amount)
-
-	if err := msgMsgDelegate.ValidateBasic(); err != nil {
-		return nil, errorsmod.Wrap(err, "failed validating msgMsgDelegate")
-	}
-
-	_, err = msgServer.Delegate(ctx, msgMsgDelegate) // Discard the response because it's empty
-	if err != nil {
-		return nil, errorsmod.Wrap(err, "elys stake msg")
-	}
-
-	var resp = &wasmbindingstypes.RequestResponse{
-		Code:   paramtypes.RES_OK,
-		Result: "Staking succeed",
-	}
-
-	return resp, nil
-}
-
-func performMsgCommit(f *commitmentkeeper.Keeper, ctx sdk.Context, contractAddr sdk.AccAddress, msgStake *types.MsgStake) (*wasmbindingstypes.RequestResponse, error) {
-	if msgStake == nil {
-		return nil, wasmvmtypes.InvalidRequest{Err: "Invalid staking parameter"}
-	}
-	msgServer := commitmentkeeper.NewMsgServerImpl(*f)
-	msgMsgCommit := commitmenttypes.NewMsgCommitClaimedRewards(msgStake.Creator, msgStake.Amount, msgStake.Asset)
-
-	if err := msgMsgCommit.ValidateBasic(); err != nil {
-		return nil, errorsmod.Wrap(err, "failed validating msgMsgCommit")
-	}
-
-	_, err := msgServer.CommitClaimedRewards(ctx, msgMsgCommit) // Discard the response because it's empty
-	if err != nil {
-		return nil, errorsmod.Wrap(err, "commit msg")
-	}
-
-	var resp = &wasmbindingstypes.RequestResponse{
-		Code:   paramtypes.RES_OK,
-		Result: "Staking succeed",
-	}
-	return resp, nil
 }
