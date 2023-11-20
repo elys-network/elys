0f1cd27e151e5f7ce2855927e622c5ed3d0ca033
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2023-11-20 12:01:22 +0100

    fix: add migrations

---
+++ x/margin/migrations/v3_migration.go
@@ -0,0 +1,12 @@
+package migrations
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/elys-network/elys/x/margin/types"
+)
+
+func (m Migrator) V3Migration(ctx sdk.Context) error {
+	params := types.NewParams()
+	m.keeper.SetParams(ctx, &params)
+	return nil
+}
