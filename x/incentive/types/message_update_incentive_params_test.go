7b6953e161af8b7f7fa3c4fe29928dfb7bce5e32
kenta-elys <kenta@elys.enetwork>
2023-11-27 17:00:15 +0000

    chore: fix unit test with update incentive param test

--- x/incentive/types/message_update_incentive_params_test.go
+++ x/incentive/types/message_update_incentive_params_test.go
@@ -3,6 +3,7 @@ package types
 import (
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/elys-network/elys/testutil/sample"
 	"github.com/stretchr/testify/require"
@@ -17,13 +18,25 @@ func TestMsgUpdateIncentiveParams_ValidateBasic(t *testing.T) {
 		{
 			name: "invalid address",
 			msg: MsgUpdateIncentiveParams{
-				Authority: "invalid_address",
+				Authority:                   "invalid_address",
+				RewardPortionForLps:         sdk.NewDecWithPrec(65, 2),
+				MaxEdenRewardAprStakers:     sdk.NewDecWithPrec(3, 1),
+				MaxEdenRewardAprLps:         sdk.NewDecWithPrec(3, 1),
+				DistributionEpochForStakers: 10,
+				DistributionEpochForLps:     10,
+				ElysStakeTrackingRate:       10,
 			},
 			err: sdkerrors.ErrInvalidAddress,
 		}, {
 			name: "valid address",
 			msg: MsgUpdateIncentiveParams{
-				Authority: sample.AccAddress(),
+				Authority:                   sample.AccAddress(),
+				RewardPortionForLps:         sdk.NewDecWithPrec(65, 2),
+				MaxEdenRewardAprStakers:     sdk.NewDecWithPrec(3, 1),
+				MaxEdenRewardAprLps:         sdk.NewDecWithPrec(3, 1),
+				DistributionEpochForStakers: 10,
+				DistributionEpochForLps:     10,
+				ElysStakeTrackingRate:       10,
 			},
 		},
 	}
