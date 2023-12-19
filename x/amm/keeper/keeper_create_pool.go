05c817ec881cebc5970c6d68308bb45c3485f3ae
jelysn <jun@elys.network>
2023-12-19 23:31:04 +0800

    amm refactoring

--- x/amm/keeper/keeper_create_pool.go
+++ x/amm/keeper/keeper_create_pool.go
@@ -5,6 +5,7 @@ import (
 
 	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/elys-network/elys/x/amm/types"
 	"github.com/elys-network/elys/x/amm/utils"
 	assetprofiletypes "github.com/elys-network/elys/x/assetprofile/types"
@@ -76,4 +77,59 @@ func (k Keeper) CreatePool(ctx sdk.Context, msg *types.MsgCreatePool) (uint64, e
 
 	// emitCreatePoolEvents(ctx, poolId, msg)
 	return pool.GetPoolId(), nil
+}
+
+// This function:
+// - saves the pool to state
+// - Mints LP shares to the pool creator
+// - Sets bank metadata for the LP denom
+// - Records total liquidity increase
+// - Calls the AfterPoolCreated hook
+func (k Keeper) InitializePool(ctx sdk.Context, pool *types.Pool, sender sdk.AccAddress) (err error) {
+	tvl, err := pool.TVL(ctx, k.oracleKeeper)
+	if err != nil {
+		return err
+	}
+
+	if tvl.IsPositive() {
+		pool.TotalShares = sdk.NewCoin(pool.TotalShares.Denom, tvl.Mul(sdk.NewDecFromInt(types.OneShare)).RoundInt())
+	}
+
+	// Mint the initial pool shares share token to the sender
+	err = k.MintPoolShareToAccount(ctx, *pool, sender, pool.GetTotalShares().Amount)
+	if err != nil {
+		return err
+	}
+
+	// Finally, add the share token's meta data to the bank keeper.
+	poolShareBaseDenom := types.GetPoolShareDenom(pool.GetPoolId())
+	poolShareDisplayDenom := fmt.Sprintf("AMM-%d", pool.GetPoolId())
+	k.bankKeeper.SetDenomMetaData(ctx, banktypes.Metadata{
+		Description: fmt.Sprintf("The share token of the amm pool %d", pool.GetPoolId()),
+		DenomUnits: []*banktypes.DenomUnit{
+			{
+				Denom:    poolShareBaseDenom,
+				Exponent: 0,
+				Aliases: []string{
+					"attopoolshare",
+				},
+			},
+			{
+				Denom:    poolShareDisplayDenom,
+				Exponent: types.OneShareExponent,
+				Aliases:  nil,
+			},
+		},
+		Base:    poolShareBaseDenom,
+		Display: poolShareDisplayDenom,
+	})
+
+	if err := k.SetPool(ctx, *pool); err != nil {
+		return err
+	}
+
+	if k.hooks != nil {
+		k.hooks.AfterPoolCreated(ctx, sender, *pool)
+	}
+	return nil
 }
--- x/amm/keeper/msg_server_update_pool_params.go
+++ x/amm/keeper/msg_server_update_pool_params.go
@@ -7,7 +7,33 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	"github.com/elys-network/elys/x/amm/types"
+	assetprofiletypes "github.com/elys-network/elys/x/assetprofile/types"
+	ptypes "github.com/elys-network/elys/x/parameter/types"
 )
+
+// UpdatePoolParams updates the pool params
+func (k Keeper) UpdatePoolParams(ctx sdk.Context, poolId uint64, poolParams types.PoolParams) (uint64, types.PoolParams, error) {
+	pool, found := k.GetPool(ctx, poolId)
+	if !found {
+		return 0, types.PoolParams{}, types.ErrPoolNotFound
+	}
+
+	entry, found := k.assetProfileKeeper.GetEntry(ctx, ptypes.BaseCurrency)
+	if !found {
+		return 0, types.PoolParams{}, errorsmod.Wrapf(assetprofiletypes.ErrAssetProfileNotFound, "asset %s not found", ptypes.BaseCurrency)
+	}
+	baseCurrency := entry.Denom
+
+	// If the fee denom is empty, set it to the base currency
+	if poolParams.FeeDenom == "" {
+		poolParams.FeeDenom = baseCurrency
+	}
+
+	pool.PoolParams = poolParams
+	k.SetPool(ctx, pool)
+
+	return pool.PoolId, pool.PoolParams, nil
+}
 
 func (k msgServer) UpdatePoolParams(goCtx context.Context, msg *types.MsgUpdatePoolParams) (*types.MsgUpdatePoolParamsResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
