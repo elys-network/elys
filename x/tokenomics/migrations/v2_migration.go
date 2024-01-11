c7c1c6f5e8d5fac50148705edccbe098afde5965
jelysn <jun@elys.network>
2024-01-11 08:44:50 +0800

    tokenomics genesis inflation store update

---
+++ x/tokenomics/migrations/v2_migration.go
@@ -0,0 +1,25 @@
+package migrations
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/elys-network/elys/x/tokenomics/types"
+)
+
+func (m Migrator) V2Migration(ctx sdk.Context) error {
+	// reset genesis inflation param
+	inflation := types.GenesisInflation{
+		Authority:             "elys10d07y265gmmuvt4z0w9aw880jnsr700j6z2zm3",
+		SeedVesting:           6000000000000,
+		StrategicSalesVesting: 180000000000000,
+		Inflation: &types.InflationEntry{
+			CommunityFund:     9750000000000,
+			IcsStakingRewards: 0,
+			LmRewards:         0,
+			StrategicReserve:  18750000000000,
+			TeamTokensVested:  0,
+		},
+	}
+
+	m.keeper.SetGenesisInflation(ctx, inflation)
+	return nil
+}
