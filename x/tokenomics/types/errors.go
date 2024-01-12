5fda1b0b5e45057ba6ab400f39dadc19636ce6ee
jelysn <jun@elys.network>
2024-01-12 20:55:34 +0800

    add expiry addition on airdrop

--- x/tokenomics/types/errors.go
+++ x/tokenomics/types/errors.go
@@ -2,11 +2,9 @@ package types
 
 // DONTCOVER
 
-import (
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-)
+import "cosmossdk.io/errors"
 
 // x/tokenomics module sentinel errors
 var (
-	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrAirdropExpired = errors.Register(ModuleName, 1, "Expired airdrop")
 )
