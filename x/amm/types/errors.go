ba9086296236cbcc783c90f7a6a06261a0671fd7
jelysn <jun@elys.network>
2024-01-16 01:14:05 +0800

    use latest errors package

--- x/amm/types/errors.go
+++ x/amm/types/errors.go
@@ -3,39 +3,39 @@ package types
 // DONTCOVER
 
 import (
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"cosmossdk.io/errors"
 )
 
 // x/amm module sentinel errors
 var (
-	ErrLimitMaxAmount      = sdkerrors.Register(ModuleName, 6, "calculated amount is larger than max amount")
-	ErrLimitMinAmount      = sdkerrors.Register(ModuleName, 7, "calculated amount is lesser than min amount")
-	ErrInvalidMathApprox   = sdkerrors.Register(ModuleName, 8, "invalid calculated result")
-	ErrInvalidPool         = sdkerrors.Register(ModuleName, 10, "attempting to create an invalid pool")
-	ErrDenomNotFoundInPool = sdkerrors.Register(ModuleName, 11, "denom does not exist in pool")
+	ErrLimitMaxAmount      = errors.Register(ModuleName, 6, "calculated amount is larger than max amount")
+	ErrLimitMinAmount      = errors.Register(ModuleName, 7, "calculated amount is lesser than min amount")
+	ErrInvalidMathApprox   = errors.Register(ModuleName, 8, "invalid calculated result")
+	ErrInvalidPool         = errors.Register(ModuleName, 10, "attempting to create an invalid pool")
+	ErrDenomNotFoundInPool = errors.Register(ModuleName, 11, "denom does not exist in pool")
 
-	ErrEmptyRoutes     = sdkerrors.Register(ModuleName, 21, "routes not defined")
-	ErrNegativeSwapFee = sdkerrors.Register(ModuleName, 23, "swap fee is negative")
-	ErrNegativeExitFee = sdkerrors.Register(ModuleName, 24, "exit fee is negative")
-	ErrTooMuchSwapFee  = sdkerrors.Register(ModuleName, 25, "swap fee should be lesser than 1 (100%)")
-	ErrTooMuchExitFee  = sdkerrors.Register(ModuleName, 26, "exit fee should be lesser than 1 (100%)")
+	ErrEmptyRoutes     = errors.Register(ModuleName, 21, "routes not defined")
+	ErrNegativeSwapFee = errors.Register(ModuleName, 23, "swap fee is negative")
+	ErrNegativeExitFee = errors.Register(ModuleName, 24, "exit fee is negative")
+	ErrTooMuchSwapFee  = errors.Register(ModuleName, 25, "swap fee should be lesser than 1 (100%)")
+	ErrTooMuchExitFee  = errors.Register(ModuleName, 26, "exit fee should be lesser than 1 (100%)")
 
-	ErrTooManyTokensOut = sdkerrors.Register(ModuleName, 31, "tx is trying to get more tokens out of the pool than exist")
+	ErrTooManyTokensOut = errors.Register(ModuleName, 31, "tx is trying to get more tokens out of the pool than exist")
 
-	ErrInvalidPoolId      = sdkerrors.Register(ModuleName, 91, "invalid pool id")
-	ErrInvalidSwapMsgType = sdkerrors.Register(ModuleName, 92, "unexpected swap message type")
+	ErrInvalidPoolId      = errors.Register(ModuleName, 91, "invalid pool id")
+	ErrInvalidSwapMsgType = errors.Register(ModuleName, 92, "unexpected swap message type")
 
-	ErrSameDenom              = sdkerrors.Register(ModuleName, 101, "denom in and denom out are the same")
-	ErrPoolNotFound           = sdkerrors.Register(ModuleName, 102, "pool not found")
-	ErrAmountTooLow           = sdkerrors.Register(ModuleName, 103, "amount too low")
-	ErrInvalidDenom           = sdkerrors.Register(ModuleName, 104, "invalid denom")
-	ErrInvalidDiscount        = sdkerrors.Register(ModuleName, 105, "invalid discount")
-	ErrInitialSpotPriceIsZero = sdkerrors.Register(ModuleName, 106, "initial spot price is zero")
-	ErrSpotPriceIsZero        = sdkerrors.Register(ModuleName, 107, "spot price is zero")
+	ErrSameDenom              = errors.Register(ModuleName, 101, "denom in and denom out are the same")
+	ErrPoolNotFound           = errors.Register(ModuleName, 102, "pool not found")
+	ErrAmountTooLow           = errors.Register(ModuleName, 103, "amount too low")
+	ErrInvalidDenom           = errors.Register(ModuleName, 104, "invalid denom")
+	ErrInvalidDiscount        = errors.Register(ModuleName, 105, "invalid discount")
+	ErrInitialSpotPriceIsZero = errors.Register(ModuleName, 106, "initial spot price is zero")
+	ErrSpotPriceIsZero        = errors.Register(ModuleName, 107, "spot price is zero")
 
-	ErrSwapFeeShouldNotExceedTwoPercent = sdkerrors.Register(ModuleName, 108, "swap fee should not exceed 2%")
-	ErrExitFeeShouldNotExceedTwoPercent = sdkerrors.Register(ModuleName, 109, "exit fee should not exceed 2%")
-	ErrFeeShouldNotBeNegative           = sdkerrors.Register(ModuleName, 110, "fee should not be negative")
+	ErrSwapFeeShouldNotExceedTwoPercent = errors.Register(ModuleName, 108, "swap fee should not exceed 2%")
+	ErrExitFeeShouldNotExceedTwoPercent = errors.Register(ModuleName, 109, "exit fee should not exceed 2%")
+	ErrFeeShouldNotBeNegative           = errors.Register(ModuleName, 110, "fee should not be negative")
 )
 
 const (
