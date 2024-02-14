c148ea189cee549e3c9aa61f1c5f7a7defbcbbcd
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-02-14 16:24:30 +0100

    fix: open estimation min collateral calc

--- x/perpetual/keeper/calc_min_collateral.go
+++ x/perpetual/keeper/calc_min_collateral.go
@@ -8,7 +8,7 @@ import (
 )
 
 // CalcMinCollateral calculates the minimum collateral required to open a position
-func (k Keeper) CalcMinCollateral(ctx sdk.Context, leverage sdk.Dec) (math.Int, error) {
+func (k Keeper) CalcMinCollateral(ctx sdk.Context, leverage sdk.Dec, price sdk.Dec, decimals uint64) (math.Int, error) {
 	// leverage must be greater than 1
 	if leverage.LTE(sdk.NewDec(1)) {
 		return sdk.ZeroInt(), errorsmod.Wrapf(types.ErrInvalidLeverage, "leverage must be greater than 1")
@@ -17,10 +17,13 @@ func (k Keeper) CalcMinCollateral(ctx sdk.Context, leverage sdk.Dec) (math.Int, 
 	// get min borrow rate
 	borrowInterestRateMin := k.GetBorrowInterestRateMin(ctx)
 
-	// min_collateral = ( 1 / (( leverage - 1 ) * borrow_interest_rate_min )) / 10 ^ collateral_decimals
-	minCollateral := sdk.NewDec(1).Quo(
+	// round up price
+	price = price.Ceil()
+
+	// min_collateral = ( trading_asset_rate_in_usdc / (( leverage - 1 ) * borrow_interest_rate_min )) + 10 ^ collateral_decimals
+	minCollateral := price.Quo(
 		leverage.Sub(sdk.NewDec(1)).Mul(borrowInterestRateMin),
-	)
+	).Add(sdk.MustNewDecFromStr("10").Power(decimals))
 
 	return minCollateral.TruncateInt(), nil
 }
