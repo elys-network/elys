72a067685fde6f972bc5d765689800f7b615ae17
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-02-22 09:09:46 +0100

    test: add gov proposals state to test software upgrade

--- scripts/upgrade-assure/export.go
+++ scripts/upgrade-assure/export.go
@@ -7,7 +7,7 @@ import (
 
 func export(cmdPath, homePath, genesisFilePath string) {
 	// Command and arguments
-	args := []string{"export", "--home", homePath, "--output-document", genesisFilePath, "--modules-to-export", "amm,assetprofile,auth,authz,bank,burner,capability,clock,commitment,consensus,crisis,epochs,evidence,feegrant,genutil,group,ibc,incentive,interchainaccounts,leveragelp,perpetual,mint,oracle,parameter,params,poolaccounted,stablestake,staking,tokenomics,transfer,transferhook,upgrade,vesting"}
+	args := []string{"export", "--home", homePath, "--output-document", genesisFilePath, "--modules-to-export", "amm,assetprofile,auth,authz,bank,burner,capability,clock,commitment,consensus,crisis,epochs,evidence,feegrant,genutil,gov,group,ibc,incentive,interchainaccounts,leveragelp,perpetual,mint,oracle,parameter,params,poolaccounted,stablestake,staking,tokenomics,transfer,transferhook,upgrade,vesting"}
 
 	// Execute the command and capture the output
 	if err := exec.Command(cmdPath, args...).Run(); err != nil {
