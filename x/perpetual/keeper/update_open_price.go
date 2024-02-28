e116dbc4902179a43137c7990129e85ed5b10343
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-02-28 09:21:23 +0100

    fix: update open price for short positions (#390)

--- x/perpetual/keeper/update_open_price.go
+++ x/perpetual/keeper/update_open_price.go
@@ -15,14 +15,34 @@ func (k Keeper) UpdateOpenPrice(ctx sdk.Context, mtp *types.MTP, ammPool ammtype
 	if mtp.CollateralAsset != baseCurrency {
 		C, err := k.EstimateSwap(ctx, sdk.NewCoin(mtp.CollateralAsset, mtp.Collateral), baseCurrency, ammPool)
 		if err != nil {
-			return errors.Wrap(err, fmt.Sprintf("error estimating swap: %s", mtp.CustodyAsset))
+			return errors.Wrap(err, fmt.Sprintf("error estimating swap: %s %s", mtp.CollateralAsset, mtp.Collateral))
 		}
 		collateralAmountInBaseCurrency = C
 	}
 
+	liabilitiesAmountInBaseCurrency := mtp.Liabilities
+	if mtp.LiabilitiesAsset != baseCurrency {
+		L, err := k.EstimateSwap(ctx, sdk.NewCoin(mtp.LiabilitiesAsset, mtp.Liabilities), baseCurrency, ammPool)
+		if err != nil {
+			return errors.Wrap(err, fmt.Sprintf("error estimating swap: %s %s", mtp.LiabilitiesAsset, mtp.Liabilities))
+		}
+		liabilitiesAmountInBaseCurrency = L
+	}
+
+	custodyAmountInTradingAsset := mtp.Custody
+	if mtp.CustodyAsset != mtp.TradingAsset {
+		C, err := k.EstimateSwap(ctx, sdk.NewCoin(mtp.CustodyAsset, mtp.Custody), mtp.TradingAsset, ammPool)
+		if err != nil {
+			return errors.Wrap(err, fmt.Sprintf("error estimating swap: %s %s", mtp.CustodyAsset, mtp.Custody))
+		}
+		custodyAmountInTradingAsset = C
+	}
+
 	// open price = (collateral + liabilities) / custody
 	mtp.OpenPrice = math.LegacyNewDecFromBigInt(
-		collateralAmountInBaseCurrency.Add(mtp.Liabilities).Quo(mtp.Custody).BigInt(),
+		collateralAmountInBaseCurrency.Add(liabilitiesAmountInBaseCurrency).BigInt(),
+	).Quo(
+		math.LegacyNewDecFromBigInt(custodyAmountInTradingAsset.BigInt()),
 	)
 
 	err := k.SetMTP(ctx, mtp)
