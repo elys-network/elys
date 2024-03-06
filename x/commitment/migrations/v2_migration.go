bfeb44e31d14fb7b64b6ee2103b904518aa7f80a
jelysn <jun@elys.network>
2024-03-06 10:05:36 +0800

    add migration script to migrate eden vesting to 12hrs

---
+++ x/commitment/migrations/v2_migration.go
@@ -0,0 +1,19 @@
+package migrations
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	ptypes "github.com/elys-network/elys/x/parameter/types"
+)
+
+func (m Migrator) V2Migration(ctx sdk.Context) error {
+	params := m.keeper.GetParams(ctx)
+	if params.VestingInfos != nil {
+		for _, vi := range params.VestingInfos {
+			if vi != nil && vi.BaseDenom == ptypes.Eden {
+				vi.NumEpochs = 4320 // 10s * 4320 = 12 hrs
+			}
+		}
+	}
+	m.keeper.SetParams(ctx, params)
+	return nil
+}
