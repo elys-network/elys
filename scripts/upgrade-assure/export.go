9dd5140d67db1d50befc3ad2b9ab679edab1bb68
Warao Gil <wari@elys.network>
2024-03-06 10:58:34 -0400

    Print stacktrace

--- scripts/upgrade-assure/export.go
+++ scripts/upgrade-assure/export.go
@@ -10,9 +10,11 @@ func export(cmdPath, homePath, genesisFilePath string) {
 	args := []string{"export", "--home", homePath, "--output-document", genesisFilePath, "--modules-to-export", "amm,assetprofile,auth,authz,bank,burner,capability,clock,commitment,consensus,crisis,epochs,evidence,feegrant,genutil,gov,group,ibc,incentive,interchainaccounts,leveragelp,perpetual,mint,oracle,parameter,params,poolaccounted,stablestake,staking,tokenomics,transfer,transferhook,upgrade,vesting"}
 
 	// Execute the command and capture the output
-	if err := exec.Command(cmdPath, args...).Run(); err != nil {
-		log.Fatalf(Red+"Command execution failed: %v", err)
+	cmd := exec.Command(cmdPath, args...)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("Command execution failed: %v\nOutput: %s", err, out)
 	}
 
-	log.Printf(Yellow+"Output successfully written to %s", genesisFilePath)
+	log.Printf("Output successfully written to %s", genesisFilePath)
 }
