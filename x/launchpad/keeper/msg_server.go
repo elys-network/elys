057545afc45dcc7471bfba064e0e7f12b8328754
jelysn <jun@elys.network>
2024-03-19 09:39:43 +0800

    register interfaces for msg_server and query

--- x/launchpad/keeper/msg_server.go
+++ x/launchpad/keeper/msg_server.go
@@ -1,6 +1,11 @@
 package keeper
 
 import (
+	"context"
+
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	"github.com/elys-network/elys/x/launchpad/types"
 )
 
@@ -15,3 +20,33 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 }
 
 var _ types.MsgServer = msgServer{}
+
+func (k msgServer) BuyElys(goCtx context.Context, msg *types.MsgBuyElys) (*types.MsgBuyElysResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	_ = ctx
+	// TODO: implement
+
+	return &types.MsgBuyElysResponse{}, nil
+}
+
+func (k msgServer) ReturnElys(goCtx context.Context, msg *types.MsgReturnElys) (*types.MsgReturnElysResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	_ = ctx
+	// TODO: implement
+
+	return &types.MsgReturnElysResponse{}, nil
+}
+
+func (k msgServer) WithdrawRaised(goCtx context.Context, msg *types.MsgWithdrawRaised) (*types.MsgWithdrawRaisedResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	params := k.GetParams(ctx)
+	if params.WithdrawAddress != msg.Sender {
+		return nil, errorsmod.Wrapf(govtypes.ErrInvalidSigner, "expected %s, got %s", params.WithdrawAddress, msg.Sender)
+	}
+
+	// TODO: implement
+
+	return &types.MsgWithdrawRaisedResponse{}, nil
+}
--- x/launchpad/keeper/query.go
+++ x/launchpad/keeper/query.go
@@ -1,7 +1,67 @@
 package keeper
 
 import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/elys-network/elys/x/launchpad/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 var _ types.QueryServer = Keeper{}
+
+func (k Keeper) Bonus(goCtx context.Context, req *types.QueryBonusRequest) (*types.QueryBonusResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	_ = ctx
+	// TODO: implement
+
+	return &types.QueryBonusResponse{}, nil
+}
+
+func (k Keeper) BuyElysEst(goCtx context.Context, req *types.QueryBuyElysEstRequest) (*types.QueryBuyElysEstResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	_ = ctx
+	// TODO: implement
+
+	return &types.QueryBuyElysEstResponse{}, nil
+}
+
+func (k Keeper) ReturnElysEst(goCtx context.Context, req *types.QueryReturnElysEstRequest) (*types.QueryReturnElysEstResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	_ = ctx
+	// TODO: implement
+
+	return &types.QueryReturnElysEstResponse{}, nil
+}
+
+func (k Keeper) Orders(goCtx context.Context, req *types.QueryOrdersRequest) (*types.QueryOrdersResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	_ = ctx
+	// TODO: implement
+
+	return &types.QueryOrdersResponse{}, nil
+}
+
+func (k Keeper) AllOrders(goCtx context.Context, req *types.QueryAllOrdersRequest) (*types.QueryAllOrdersResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	_ = ctx
+	// TODO: implement
+
+	return &types.QueryAllOrdersResponse{}, nil
+}
