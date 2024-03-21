8942a7356505b01eed23dee48e1eaa2f49896890
jelysn <jun@elys.network>
2024-03-21 09:35:57 +0800

    define genesis and default params for launchpad

--- x/launchpad/types/params.go
+++ x/launchpad/types/params.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"gopkg.in/yaml.v2"
 )
@@ -19,7 +20,32 @@ func NewParams() Params {
 
 // DefaultParams returns a default set of parameters
 func DefaultParams() Params {
-	return NewParams()
+	return Params{
+		InitialPrice:       sdk.NewDecWithPrec(75, 2),  // 0.75
+		TotalReserve:       sdk.NewInt(4500000_000000), // 4.5 million
+		SoldAmount:         sdk.NewInt(0),
+		WithdrawAddress:    "",
+		WithdrawnAmount:    sdk.NewCoins(),
+		LaunchpadStarttime: 1710984623,  // 2024-03-21
+		LaunchpadDuration:  86400 * 100, // 100 days
+		ReturnDuration:     86400 * 180, // 6 months
+		MaxReturnPercent:   50,          // 50%
+		SpendingTokens:     []string{"uusdc", "uatom", "wei"},
+		BonusInfo: Bonus{
+			//   0-20% raise 100% bonus
+			//   20-30% raise 90% bonus
+			//   30-40% raise bonus 80%
+			//   40-50% raise bonus 70%
+			//   50-60% raise bonus 60%
+			//   60-70% raise bonus 50%
+			//   70-80% raise bonus 40%
+			//   80-100% raise bonus 30%
+			RaisePercents:   []uint64{20, 30, 40, 50, 60, 70, 80, 100},
+			BonusPercents:   []uint64{100, 90, 80, 70, 60, 50, 40, 30},
+			LockDuration:    86400 * 180, // 6 months
+			VestingDuration: 86400 * 365, // 1 year
+		},
+	}
 }
 
 // ParamSetPairs get the params.ParamSet
