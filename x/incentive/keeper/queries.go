20e680884478891bd121703fea98ba4952649112
jelysn <jun@elys.network>
2024-03-25 09:53:43 +0800

    pool rewards query update to include individual daily rewards and rewards in usd

--- x/incentive/keeper/queries.go
+++ x/incentive/keeper/queries.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	ammtypes "github.com/elys-network/elys/x/amm/types"
 	"github.com/elys-network/elys/x/incentive/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -54,4 +55,74 @@ func (k Keeper) PoolAprs(goCtx context.Context, req *types.QueryPoolAprsRequest)
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	data := k.CalculatePoolAprs(ctx, req.PoolIds)
 	return &types.QueryPoolAprsResponse{Data: data}, nil
+}
+
+func (k Keeper) PoolRewards(goCtx context.Context, req *types.QueryPoolRewardsRequest) (*types.QueryPoolRewardsResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	pools := make([]types.PoolRewards, 0)
+	skipCount := uint64(0)
+
+	// If we have pool Ids specified,
+	if len(req.PoolIds) > 0 {
+		for _, pId := range req.PoolIds {
+			pool, found := k.amm.GetPool(ctx, pId)
+			if !found {
+				continue
+			}
+
+			// check offset if pagination available
+			if req.Pagination != nil && skipCount < req.Pagination.Offset {
+				skipCount++
+				continue
+			}
+
+			// check maximum limit if pagination available
+			if req.Pagination != nil && len(pools) >= int(req.Pagination.Limit) {
+				break
+			}
+
+			// Construct earn pool
+			earnPool := k.generatePoolRewards(ctx, &pool)
+			pools = append(pools, earnPool)
+		}
+	} else {
+		k.amm.IterateLiquidityPools(ctx, func(p ammtypes.Pool) bool {
+			// check offset if pagination available
+			if req.Pagination != nil && skipCount < req.Pagination.Offset {
+				skipCount++
+				return false
+			}
+
+			// check maximum limit if pagination available
+			if req.Pagination != nil && len(pools) >= int(req.Pagination.Limit) {
+				return true
+			}
+
+			// Construct earn pool
+			poolRewards := k.generatePoolRewards(ctx, &p)
+			pools = append(pools, poolRewards)
+
+			return false
+		})
+	}
+
+	return &types.QueryPoolRewardsResponse{
+		Pools: pools,
+	}, nil
+}
+
+// Generate earn pool struct
+func (k *Keeper) generatePoolRewards(ctx sdk.Context, ammPool *ammtypes.Pool) types.PoolRewards {
+	// Get rewards amount
+	rewardsUsd, rewardCoins := k.GetDailyRewardsAmountForPool(ctx, ammPool.PoolId)
+
+	return types.PoolRewards{
+		PoolId:      ammPool.PoolId,
+		RewardsUsd:  rewardsUsd,
+		RewardCoins: rewardCoins,
+	}
 }
