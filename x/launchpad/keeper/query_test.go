494fb113c05222a386acc2e28d2371442ea9b433
jelysn <jun@elys.network>
2024-03-27 20:24:28 +0800

    add unit test for ReturnElysEst

--- x/launchpad/keeper/query_test.go
+++ x/launchpad/keeper/query_test.go
@@ -2,12 +2,14 @@ package keeper_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/cometbft/cometbft/crypto/ed25519"
 	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 	simapp "github.com/elys-network/elys/app"
+	"github.com/elys-network/elys/x/launchpad/keeper"
 	"github.com/elys-network/elys/x/launchpad/types"
 	oracletypes "github.com/elys-network/elys/x/oracle/types"
 	ptypes "github.com/elys-network/elys/x/parameter/types"
@@ -198,5 +200,53 @@ func TestBuyElysEst(t *testing.T) {
 	require.Equal(t, response.Orders[1].VestingStarted, false)
 }
 
-// TODO:
-// func (k Keeper) ReturnElysEst(goCtx context.Context, req *types.QueryReturnElysEstRequest) (*types.QueryReturnElysEstResponse, error)
+func TestReturnElysEst(t *testing.T) {
+	app := simapp.InitElysTestApp(true)
+	ctx := app.BaseApp.NewContext(true, tmproto.Header{})
+	k := app.LaunchpadKeeper
+	SetupStableCoinPrices(app, ctx)
+
+	coins := sdk.Coins{sdk.NewInt64Coin(ptypes.Elys, 1000_000_000_000_000)}
+	err := app.BankKeeper.MintCoins(ctx, minttypes.ModuleName, coins)
+	require.NoError(t, err)
+	err = app.BankKeeper.SendCoinsFromModuleToModule(ctx, minttypes.ModuleName, types.ModuleName, coins)
+	require.NoError(t, err)
+
+	buyer := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address())
+	coins = sdk.Coins{sdk.NewInt64Coin(ptypes.BaseCurrency, 1100_000_000_000)}
+	err = app.BankKeeper.MintCoins(ctx, minttypes.ModuleName, coins)
+	require.NoError(t, err)
+	err = app.BankKeeper.SendCoinsFromModuleToAccount(ctx, minttypes.ModuleName, buyer, coins)
+	require.NoError(t, err)
+
+	now := time.Now()
+	ctx = ctx.WithBlockTime(now)
+	params := k.GetParams(ctx)
+	params.LaunchpadStarttime = uint64(now.Unix())
+	k.SetParams(ctx, params)
+
+	msgServer := keeper.NewMsgServerImpl(k)
+	response, err := msgServer.BuyElys(ctx, &types.MsgBuyElys{
+		Sender:        buyer.String(),
+		SpendingToken: ptypes.BaseCurrency,
+		TokenAmount:   sdk.NewInt(1000_000),
+	})
+	require.NoError(t, err)
+	require.Len(t, response.OrderIds, 1)
+	require.Equal(t, response.OrderIds[0], uint64(1))
+	orders := k.GetAllOrders(ctx)
+	require.Len(t, orders, 1)
+	require.Equal(t, orders[0].OrderId, uint64(1))
+	require.Equal(t, orders[0].OrderMaker, buyer.String())
+	require.Equal(t, orders[0].SpendingToken, ptypes.BaseCurrency)
+	require.Equal(t, orders[0].TokenAmount.String(), "1000000")
+	require.Equal(t, orders[0].ElysAmount.String(), "1333333")
+	require.Equal(t, orders[0].ReturnedElysAmount.String(), "0")
+
+	resp, err := k.ReturnElysEst(ctx, &types.QueryReturnElysEstRequest{
+		OrderId:    1,
+		ElysAmount: sdk.NewInt(333333),
+	})
+	require.NoError(t, err)
+	require.Equal(t, resp.Amount.String(), "249999")
+}
