f49041050a7e6f7c5568f59f3ecce56cc4c61892
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-04-10 09:35:16 +0200

    ci: already override stdout

--- scripts/upgrade-assure/export.go
+++ scripts/upgrade-assure/export.go
@@ -65,7 +65,7 @@ func export(cmdPath, homePath, genesisFilePath string) {
 
 	// Redirect the output to a file
 	cmd.Stdout = outFile
-	cmd.Stderr = outFile // You can also direct stderr to a different file or os.Stderr
+	// cmd.Stderr = outFile // You can also direct stderr to a different file or os.Stderr
 
 	// Execute the command
 	out, err := cmd.CombinedOutput()
