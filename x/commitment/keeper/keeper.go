5605f96cb52426eed6c03e92b39f0eabe9391b4f
jelysn <jun@elys.network>
2024-04-18 20:21:57 +0800

    resolve issues in single node start

--- x/commitment/keeper/keeper.go
+++ x/commitment/keeper/keeper.go
@@ -54,11 +54,6 @@ func NewKeeper(
 	assetProfileKeeper types.AssetProfileKeeper,
 	authority string,
 ) *Keeper {
-	// set KeyTable if it has not already been set
-	if !ps.HasKeyTable() {
-		ps = ps.WithKeyTable(types.ParamKeyTable())
-	}
-
 	return &Keeper{
 		cdc:        cdc,
 		storeKey:   storeKey,
