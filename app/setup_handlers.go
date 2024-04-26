893b7268bf2b734806c2f0cef55ac80f0b9a0dfc
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-04-26 16:26:18 +0200

    ci: update setup upgrade version

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -25,7 +25,7 @@ func setUpgradeHandler(app *ElysApp) {
 		func(ctx sdk.Context, plan upgradetypes.Plan, vm m.VersionMap) (m.VersionMap, error) {
 			app.Logger().Info("Running upgrade handler for " + version.Version)
 
-			if version.Version == "v0.30.1" {
+			if version.Version == "v0.31.0" {
 				app.Logger().Info("Deleting proposals with ID <= 185")
 				store := ctx.KVStore(app.keys[govtypes.StoreKey])
 				for i := uint64(1); i <= 185; i++ {
