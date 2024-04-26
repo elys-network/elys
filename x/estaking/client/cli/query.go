022ff9643adda8a5f4be798c44d36ec61290e006
jelysn <jun@elys.network>
2024-04-26 09:03:43 +0800

    add query command for all rewards for a delegator

--- x/estaking/client/cli/query.go
+++ x/estaking/client/cli/query.go
@@ -24,7 +24,10 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 
-	cmd.AddCommand(CmdQueryParams())
+	cmd.AddCommand(
+		CmdQueryParams(),
+		CmdQueryRewards(),
+	)
 	// this line is used by starport scaffolding # 1
 
 	return cmd
--- x/estaking/client/cli/query_params.go
+++ x/estaking/client/cli/query_params.go
@@ -34,3 +34,31 @@ func CmdQueryParams() *cobra.Command {
 
 	return cmd
 }
+
+func CmdQueryRewards() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "rewards",
+		Short: "shows the rewards of an account",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+			res, err := queryClient.Rewards(cmd.Context(), &types.QueryRewardsRequest{
+				Address: args[0],
+			})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
