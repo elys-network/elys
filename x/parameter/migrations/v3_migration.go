aa7e0fa1275e6bb8cb78216e8e7b564e4aada084
jelysn <jun@elys.network>
2024-05-14 23:55:10 +0800

    update parameter migration script to keep old brokerAddress

--- x/parameter/migrations/v3_migration.go
+++ x/parameter/migrations/v3_migration.go
@@ -7,12 +7,13 @@ import (
 
 func (m Migrator) V3Migration(ctx sdk.Context) error {
 	// reset params
+	legacy := m.keeper.GetLegacyParams(ctx)
 	params := types.NewParams(
-		sdk.NewDecWithPrec(5, 2),  // min commission 0.05
-		sdk.NewDecWithPrec(66, 2), // max voting power 0.66
-		sdk.NewInt(1),             // min self delegation
-		"elys1m3hduhk4uzxn8mxuvpz02ysndxfwgy5mq60h4c34qqn67xud584qeee3m4", // broker address
-		6307200, // total blocks per year
+		legacy.MinCommissionRate, // min commission 0.05
+		legacy.MaxVotingPower,    // max voting power 0.66
+		legacy.MinSelfDelegation, // min self delegation
+		legacy.BrokerAddress,     // broker address
+		6307200,                  // total blocks per year
 	)
 	m.keeper.SetParams(ctx, params)
 
