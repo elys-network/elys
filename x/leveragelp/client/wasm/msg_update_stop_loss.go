83b9303cb0f24534e3c7e9631a9233df808c382d
Amit Yadav <amy29981@gmail.com>
2024-06-07 17:01:19 +0530

    update error message

--- x/leveragelp/client/wasm/msg_update_stop_loss.go
+++ x/leveragelp/client/wasm/msg_update_stop_loss.go
@@ -12,22 +12,22 @@ import (
 
 func (m *Messenger) msgUpdateStopLoss(ctx sdk.Context, contractAddr sdk.AccAddress, msgUpdateStopLoss *leveragelptypes.MsgUpdateStopLoss) ([]sdk.Event, [][]byte, error) {
 	if msgUpdateStopLoss == nil {
-		return nil, nil, wasmvmtypes.InvalidRequest{Err: "Open null msg"}
+		return nil, nil, wasmvmtypes.InvalidRequest{Err: "update stop loss null msg"}
 	}
 
 	brokerAddress := m.parameterKeeper.GetParams(ctx).BrokerAddress
 	if msgUpdateStopLoss.Creator != contractAddr.String() && contractAddr.String() != brokerAddress {
-		return nil, nil, wasmvmtypes.InvalidRequest{Err: "open wrong sender"}
+		return nil, nil, wasmvmtypes.InvalidRequest{Err: "update stop loss wrong sender"}
 	}
 
 	res, err := PerformMsgUpdateStopLoss(m.keeper, ctx, contractAddr, msgUpdateStopLoss)
 	if err != nil {
-		return nil, nil, errorsmod.Wrap(err, "perform open")
+		return nil, nil, errorsmod.Wrap(err, "perform update stop loss")
 	}
 
 	responseBytes, err := json.Marshal(*res)
 	if err != nil {
-		return nil, nil, errorsmod.Wrap(err, "failed to serialize open response")
+		return nil, nil, errorsmod.Wrap(err, "failed to serialize update stop loss response")
 	}
 
 	resp := [][]byte{responseBytes}
