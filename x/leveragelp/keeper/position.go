4e2f5c2d3535b8cbedc02ae66afd114c173974cc
Amit Yadav <amy29981@gmail.com>
2024-06-26 10:34:48 +0530

    fix overflow

--- x/leveragelp/keeper/position.go
+++ x/leveragelp/keeper/position.go
@@ -98,6 +98,9 @@ func (k Keeper) DestroyPosition(ctx sdk.Context, positionAddress string, id uint
 
 	// decrement open position count
 	openCount := k.GetOpenPositionCount(ctx)
+	if openCount != 0 {
+		openCount--
+	}
 	openCount--
 
 	// Set open Position count
---
+++ x/leveragelp/migrations/v5_migration.go
@@ -0,0 +1,11 @@
+package migrations
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (m Migrator) V5Migration(ctx sdk.Context) error {
+	count := m.keeper.GetPositionCount(ctx)
+	m.keeper.SetOpenPositionCount(ctx, count)
+	return nil
+}
