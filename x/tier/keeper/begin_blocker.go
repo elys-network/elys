4210dadcf50d99145922da924384b52c7eb877d1
Amit Yadav <amy29981@gmail.com>
2024-06-27 10:41:28 +0530

    change to 100

--- x/tier/keeper/begin_blocker.go
+++ x/tier/keeper/begin_blocker.go
@@ -10,6 +10,6 @@ func (k Keeper) BeginBlocker(ctx sdk.Context) {
 	year, month, day := ctx.BlockTime().Date()
 	dateToday := time.Date(year, month, day, 0, 0, 0, 0, ctx.BlockTime().Location())
 	deleteDate := dateToday.AddDate(0, 0, -8)
-	// Remove last 500 values at each block
-	k.RemovePortfolioLast(ctx, deleteDate.String(), 500)
+	// Remove last 100 values at each block
+	k.RemovePortfolioLast(ctx, deleteDate.String(), 100)
 }
