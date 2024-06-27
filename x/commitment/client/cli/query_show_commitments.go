dd4e426c8d30f4ea76bfad6569738330fcf2a5a3
jelysn <129082781+jelysn@users.noreply.github.com>
2024-06-27 22:31:12 +0800

    Locked tokens query (#626)
    * amm hook fix when executed through leveragelp
    
    * add query for locked tokens in commitment and locked tokens in leveragelp position

--- x/commitment/client/cli/query_show_commitments.go
+++ x/commitment/client/cli/query_show_commitments.go
@@ -36,6 +36,35 @@ func CmdShowCommitments() *cobra.Command {
 	return cmd
 }
 
+func CmdCommittedTokensLocked() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "committed-tokens-locked [address]",
+		Short: "Show locked coins in commitment not unlockable",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+			params := &types.QueryCommittedTokensLockedRequest{
+				Address: args[0],
+			}
+			res, err := queryClient.CommittedTokensLocked(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
 func CmdNumberOfCommitments() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "number-of-commitments",
