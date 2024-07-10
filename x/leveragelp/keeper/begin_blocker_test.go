678c869ef608861613d7bb40bba869374a3d3af8
Amit Yadav <amy29981@gmail.com>
2024-07-10 13:03:35 +0530

    add sorted list test

--- x/leveragelp/keeper/begin_blocker_test.go
+++ x/leveragelp/keeper/begin_blocker_test.go
@@ -1,11 +1,16 @@
 package keeper_test
 
 import (
+	"fmt"
 	"time"
 
 	"cosmossdk.io/math"
 	"github.com/cometbft/cometbft/crypto/ed25519"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
+	"github.com/elys-network/elys/x/leveragelp/types"
+	stablestakekeeper "github.com/elys-network/elys/x/stablestake/keeper"
+	stablestaketypes "github.com/elys-network/elys/x/stablestake/types"
 )
 
 func (suite KeeperTestSuite) TestBeginBlocker() {
@@ -73,6 +78,118 @@ func (suite KeeperTestSuite) TestLiquidatePositionIfUnhealthy() {
 	suite.Require().False(earlyReturn)
 	_, err = k.GetPosition(cacheCtx, position.Address, position.Id)
 	suite.Require().Error(err)
+}
+
+func (suite KeeperTestSuite) TestLiquidatePositionSorted() {
+	k := suite.app.LeveragelpKeeper
+	addr := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address())
+	position, _ := suite.OpenPosition(addr)
+
+	// open positions with other addresses
+	addr2 := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address())
+	addr3 := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address())
+	addr4 := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address())
+	addr5 := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address())
+
+	usdcTokenTotal := sdk.NewInt64Coin("uusdc", 500000)
+	usdcToken := sdk.NewInt64Coin("uusdc", 100000)
+	err := suite.app.BankKeeper.MintCoins(suite.ctx, minttypes.ModuleName, sdk.Coins{usdcTokenTotal})
+	suite.Require().NoError(err)
+	err = suite.app.BankKeeper.SendCoinsFromModuleToAccount(suite.ctx, minttypes.ModuleName, addr2, sdk.Coins{usdcToken})
+	suite.Require().NoError(err)
+	err = suite.app.BankKeeper.SendCoinsFromModuleToAccount(suite.ctx, minttypes.ModuleName, addr3, sdk.Coins{usdcToken})
+	suite.Require().NoError(err)
+	err = suite.app.BankKeeper.SendCoinsFromModuleToAccount(suite.ctx, minttypes.ModuleName, addr4, sdk.Coins{usdcToken})
+	suite.Require().NoError(err)
+	err = suite.app.BankKeeper.SendCoinsFromModuleToAccount(suite.ctx, minttypes.ModuleName, addr5, sdk.Coins{usdcToken})
+	suite.Require().NoError(err)
+
+	stableMsgServer := stablestakekeeper.NewMsgServerImpl(suite.app.StablestakeKeeper)
+	_, err = stableMsgServer.Bond(sdk.WrapSDKContext(suite.ctx), &stablestaketypes.MsgBond{
+		Creator: addr2.String(),
+		Amount:  sdk.NewInt(100000),
+	})
+	suite.Require().NoError(err)
+
+	position3, err := k.OpenLong(suite.ctx, &types.MsgOpen{
+		Creator:          addr3.String(),
+		CollateralAsset:  "uusdc",
+		CollateralAmount: sdk.NewInt(2000),
+		AmmPoolId:        1,
+		Leverage:         sdk.NewDec(2),
+	})
+	suite.Require().NoError(err)
+
+	position4, err := k.OpenLong(suite.ctx, &types.MsgOpen{
+		Creator:          addr4.String(),
+		CollateralAsset:  "uusdc",
+		CollateralAmount: sdk.NewInt(2000),
+		AmmPoolId:        1,
+		Leverage:         sdk.NewDec(6),
+	})
+	suite.Require().NoError(err)
+
+	position5, err := k.OpenLong(suite.ctx, &types.MsgOpen{
+		Creator:          addr5.String(),
+		CollateralAsset:  "uusdc",
+		CollateralAmount: sdk.NewInt(2000),
+		AmmPoolId:        1,
+		Leverage:         sdk.NewDec(4),
+	})
+	suite.Require().NoError(err)
+
+	ammPool, found := suite.app.AmmKeeper.GetPool(suite.ctx, position3.AmmPoolId)
+	suite.Require().True(found)
+	health, err := k.GetPositionHealth(suite.ctx, *position3, ammPool)
+	suite.Require().NoError(err)
+	suite.Require().Equal(health.String(), "2.000000000000000000") // slippage disabled on amm
+
+	health, err = k.GetPositionHealth(suite.ctx, *position, ammPool)
+	suite.Require().NoError(err)
+	suite.Require().Equal(health.String(), "1.250000000000000000") // slippage disabled on amm
+
+	health, err = k.GetPositionHealth(suite.ctx, *position4, ammPool)
+	suite.Require().NoError(err)
+	suite.Require().Equal(health.String(), "1.200000000000000000") // slippage disabled on amm
+
+	health, err = k.GetPositionHealth(suite.ctx, *position5, ammPool)
+	suite.Require().NoError(err)
+	suite.Require().Equal(health.String(), "1.333333333333333333") // slippage disabled on amm
+
+	// Check order in list
+	suite.app.LeveragelpKeeper.IteratePoolPosIdsLiquidationSorted(suite.ctx, position.AmmPoolId, func(posId types.AddressId) bool {
+		position, _ := k.GetPosition(suite.ctx, posId.Address, posId.Id)
+		fmt.Printf("Address: %s, Id: %d, value: %s", position.Address, position.Id, position.PositionHealth.String())
+		return true
+	})
+
+	// Partial close, add collateral and add more lev should result in correct order
+
+	// suite.ctx = suite.ctx.WithBlockTime(suite.ctx.BlockTime().Add(time.Hour * 24 * 500))
+	// suite.app.StablestakeKeeper.BeginBlocker(suite.ctx)
+	// health, err = k.GetPositionHealth(suite.ctx, *position, ammPool)
+	// suite.Require().NoError(err)
+	// // suite.Require().Equal(health.String(), "1.024543738200125865") // slippage enabled on amm
+	// suite.Require().Equal(health.String(), "1.025220422390814025") // slippage disabled on amm
+
+	// cacheCtx, _ := suite.ctx.CacheContext()
+	// params := k.GetParams(cacheCtx)
+	// params.SafetyFactor = sdk.NewDecWithPrec(11, 1)
+	// k.SetParams(cacheCtx, &params)
+	// isHealthy, earlyReturn := k.LiquidatePositionIfUnhealthy(cacheCtx, position, pool, ammPool)
+	// suite.Require().False(isHealthy)
+	// suite.Require().False(earlyReturn)
+	// _, err = k.GetPosition(cacheCtx, position.Address, position.Id)
+	// suite.Require().Error(err)
+
+	// cacheCtx, _ = suite.ctx.CacheContext()
+	// position.StopLossPrice = math.LegacyNewDec(100000)
+	// k.SetPosition(cacheCtx, position, sdk.NewInt(0))
+	// underStopLossPrice, earlyReturn := k.ClosePositionIfUnderStopLossPrice(cacheCtx, position, pool, ammPool)
+	// suite.Require().True(underStopLossPrice)
+	// suite.Require().False(earlyReturn)
+	// _, err = k.GetPosition(cacheCtx, position.Address, position.Id)
+	// suite.Require().Error(err)
 }
 
 // Test sorted liquidity flow
