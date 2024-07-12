3a5e3a5f7455d7a169db9d756a0a4fda76b06d30
Amit Yadav <amy29981@gmail.com>
2024-07-12 15:36:37 +0530

    liquidate

--- x/leveragelp/migrations/v7_migration.go
+++ x/leveragelp/migrations/v7_migration.go
@@ -1,12 +1,15 @@
 package migrations
 
 import (
+	"fmt"
+
+	"cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/elys-network/elys/x/leveragelp/types"
 )
 
 func (m Migrator) V7Migration(ctx sdk.Context) error {
 	// Traverse positions and update lp amount and health
-	// Liquidate <1.1 positions
 	// Update data structure
 	positions := m.keeper.GetAllPositions(ctx)
 	pools := m.keeper.GetAllPools(ctx)
@@ -18,5 +21,38 @@ func (m Migrator) V7Migration(ctx sdk.Context) error {
 		m.keeper.SetSortedLiquidationAndStopLoss(ctx, position)
 	}
 
+	// Liquidate <1.1 positions
+	// Q: What will happen if there won't be enough liquidity to return to users(as health for some positions must be below 1) ? Do we need to fill the pool ?
+	for _, pool := range pools {
+		ammPool, err := m.keeper.GetAmmPool(ctx, pool.AmmPoolId)
+		if err != nil {
+			ctx.Logger().Error(errors.Wrap(err, fmt.Sprintf("error getting amm pool: %d", pool.AmmPoolId)).Error())
+			continue
+		}
+		m.keeper.IteratePoolPosIdsLiquidationSorted(ctx, pool.AmmPoolId, func(posId types.AddressId) bool {
+			position, err := m.keeper.GetPosition(ctx, posId.Address, posId.Id)
+			if err != nil {
+				return false
+			}
+			isHealthy, earlyReturn := m.keeper.LiquidatePositionIfUnhealthy(ctx, &position, pool, ammPool)
+			if !earlyReturn && isHealthy {
+				return true
+			}
+			return false
+		})
+
+		// Close stopLossPrice reached positions
+		m.keeper.IteratePoolPosIdsStopLossSorted(ctx, pool.AmmPoolId, func(posId types.AddressId) bool {
+			position, err := m.keeper.GetPosition(ctx, posId.Address, posId.Id)
+			if err != nil {
+				return false
+			}
+			underStopLossPrice, earlyReturn := m.keeper.ClosePositionIfUnderStopLossPrice(ctx, &position, pool, ammPool)
+			if !earlyReturn && underStopLossPrice {
+				return true
+			}
+			return false
+		})
+	}
 	return nil
 }
