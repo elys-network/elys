8d6dcde69eb593083f74357139dd81c9f3350222
Warao Gil <wari@elys.network>
2024-07-27 15:58:38 -0400

    Adds debug ante handler

--- app/ante/ante.go
+++ app/ante/ante.go
@@ -1,13 +1,110 @@
 package ante
 
 import (
+	"bytes"
+	"fmt"
+
 	errorsmod "cosmossdk.io/errors"
 	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	sdkante "github.com/cosmos/cosmos-sdk/x/auth/ante"
+	"github.com/cosmos/cosmos-sdk/x/auth/migrations/legacytx"
+	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
+	"github.com/cosmos/cosmos-sdk/x/auth/types"
 	ibcante "github.com/cosmos/ibc-go/v7/modules/core/ante"
 )
+
+type DebugDecorator struct{
+	ak              sdkante.AccountKeeper
+}
+
+func NewDebugDecorator(ak sdkante.AccountKeeper) DebugDecorator {
+    return DebugDecorator{
+		ak: ak,
+	}
+}
+
+var (
+	// simulation signature values used to estimate gas consumption
+	key                = make([]byte, secp256k1.PubKeySize)
+	simSecp256k1Pubkey = &secp256k1.PubKey{Key: key}
+	simSecp256k1Sig    [64]byte
+
+	_ authsigning.SigVerifiableTx = (*legacytx.StdTx)(nil) // assert StdTx implements SigVerifiableTx
+)
+
+func (dd DebugDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
+	fmt.Println("AnteHandler")
+	sigTx, ok := tx.(authsigning.SigVerifiableTx)
+	if !ok {
+		fmt.Println("invalid")
+		return ctx, sdkerrors.Wrap(sdkerrors.ErrTxDecode, "invalid tx type")
+	}
+	
+
+	pubkeys, err := sigTx.GetPubKeys()
+	if err != nil {
+		return ctx, err
+	}
+	signers := sigTx.GetSigners()
+
+	for i, pk := range pubkeys {
+		// PublicKey was omitted from slice since it has already been set in context
+		if pk == nil {
+			if !simulate {
+				continue
+			}
+			pk = simSecp256k1Pubkey
+		}
+		// Only make check if simulate=false
+		if !simulate && !bytes.Equal(pk.Address(), signers[i]) {
+			return ctx, sdkerrors.Wrapf(sdkerrors.ErrInvalidPubKey,
+				"pubKey does not match signer address %s with signer index: %d", signers[i], i)
+		}
+
+		acc, err := GetSignerAcc(ctx, dd.ak, signers[i])
+		fmt.Println("acc", acc)
+		if err != nil {
+			return ctx, err
+		}
+		// account already has pubkey set,no need to reset
+		if acc.GetPubKey() != nil {
+			continue
+		}
+		err = acc.SetPubKey(pk)
+		if err != nil {
+			return ctx, sdkerrors.Wrap(sdkerrors.ErrInvalidPubKey, err.Error())
+		}
+		dd.ak.SetAccount(ctx, acc)
+	}
+	
+    // if !ok {
+    //     return ctx, sdkerrors.Wrap(sdkerrors.ErrTxDecode, "Tx must be auth.StdTx")
+    // }
+
+    // Log the transaction details
+    // fmt.Printf("DebugDecorator: Transaction: %v\n", stdTx)
+
+    // Log signer information
+    // for _, sig := range stdTx.Signatures {
+    //     fmt.Printf("DebugDecorator: Signer: %v\n", sig.PubKey.Address().String())
+    // }
+
+    // Continue with the default AnteHandler chain
+    return next(ctx, tx, simulate)
+}
+
+// GetSignerAcc returns an account for a given address that is expected to sign
+// a transaction.
+func GetSignerAcc(ctx sdk.Context, ak sdkante.AccountKeeper, addr sdk.AccAddress) (types.AccountI, error) {
+	if acc := ak.GetAccount(ctx, addr); acc != nil {
+		return acc, nil
+	}
+
+	return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownAddress, "account %s does not exist", addr)
+}
 
 // NewAnteHandler returns an AnteHandler that checks and increments sequence
 // numbers, checks signatures & account numbers, and deducts fees from the first
@@ -60,6 +157,7 @@ func NewAnteHandler(options HandlerOptions) (sdk.AnteHandler, error) {
 		sdkante.NewSigVerificationDecorator(options.AccountKeeper, options.SignModeHandler),
 		sdkante.NewIncrementSequenceDecorator(options.AccountKeeper),
 		ibcante.NewRedundantRelayDecorator(options.IBCKeeper),
+		NewDebugDecorator(options.AccountKeeper),
 	}
 
 	return sdk.ChainAnteDecorators(anteDecorators...), nil
