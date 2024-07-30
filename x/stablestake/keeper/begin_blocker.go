eaec0a310a2345f21c0f8d24875c539d14a85343
Amit Yadav <amy29981@gmail.com>
2024-07-30 20:54:56 +0530

    add todo

--- x/stablestake/keeper/begin_blocker.go
+++ x/stablestake/keeper/begin_blocker.go
@@ -13,6 +13,7 @@ func (k Keeper) BeginBlocker(ctx sdk.Context) {
 		params := k.GetParams(ctx)
 		rate := k.InterestRateComputation(ctx)
 		params.InterestRate = rate
+		// TODO: store interest rate per block or when it is changed for more accurate calculation
 		k.SetParams(ctx, params)
 	}
 }
