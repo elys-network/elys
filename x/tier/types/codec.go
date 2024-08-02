2cf2017875dc0cf4b7422f126ed7172f469c7482
Warao <wari@elys.network>
2024-08-02 12:58:17 -0400

    Fix modules codecs (#690)
    * Fix estaking module
    
    * Update amm module
    
    * Update assetprofile module
    
    * Update burner module
    
    * Update commitment module
    
    * Update leveragelp module
    
    * Update masterchef module
    
    * Update oracle module
    
    * Update parameter module
    
    * Update perpetual module
    
    * Update stablestake module
    
    * Update tier module
    
    * Update tokenomics module

--- x/tier/types/codec.go
+++ x/tier/types/codec.go
@@ -2,13 +2,18 @@ package types
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/legacy"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
+	authzcodec "github.com/cosmos/cosmos-sdk/x/authz/codec"
+	govcodec "github.com/cosmos/cosmos-sdk/x/gov/codec"
+	groupcodec "github.com/cosmos/cosmos-sdk/x/group/codec"
 )
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
-	cdc.RegisterConcrete(&MsgSetPortfolio{}, "membershiptier/SetPortfolio", nil)
+	legacy.RegisterAminoMsg(cdc, &MsgSetPortfolio{}, "membershiptier/SetPortfolio")
 	// this line is used by starport scaffolding # 2
 }
 
@@ -25,3 +30,15 @@ var (
 	Amino     = codec.NewLegacyAmino()
 	ModuleCdc = codec.NewProtoCodec(cdctypes.NewInterfaceRegistry())
 )
+
+func init() {
+    RegisterCodec(Amino)
+    cryptocodec.RegisterCrypto(Amino)
+    sdk.RegisterLegacyAminoCodec(Amino)
+
+    // Register all Amino interfaces and concrete types on the authz  and gov Amino codec so that this can later be
+    // used to properly serialize MsgGrant, MsgExec and MsgSubmitProposal instances
+    RegisterCodec(authzcodec.Amino)
+    RegisterCodec(govcodec.Amino)
+    RegisterCodec(groupcodec.Amino)
+}
