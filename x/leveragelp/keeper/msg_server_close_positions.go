6dc2df5fd62dea508ff08e49c6c843a6fc944a3a
Amit Yadav <amy29981@gmail.com>
2024-08-02 23:17:52 +0530

    add logic

--- x/leveragelp/keeper/msg_server_close_positions.go
+++ x/leveragelp/keeper/msg_server_close_positions.go
@@ -10,8 +10,33 @@ import (
 func (k msgServer) ClosePositions(goCtx context.Context, msg *types.MsgClosePositions) (*types.MsgClosePositionsResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// Handle liquidations
+	for _, val := range msg.Liquidate {
+		position, err := k.GetPosition(ctx, val.Address, val.Id)
+		if err != nil {
+			continue
+		}
+		pool, found := k.GetPool(ctx, position.AmmPoolId)
+		ammPool, err := k.GetAmmPool(ctx, position.AmmPoolId)
+		if !found || err != nil {
+			continue
+		}
+		_, _ = k.LiquidatePositionIfUnhealthy(ctx, &position, pool, ammPool)
+	}
+
+	// Handle stop loss
+	for _, val := range msg.Stoploss {
+		position, err := k.GetPosition(ctx, val.Address, val.Id)
+		if err != nil {
+			continue
+		}
+		pool, found := k.GetPool(ctx, position.AmmPoolId)
+		ammPool, err := k.GetAmmPool(ctx, position.AmmPoolId)
+		if !found || err != nil {
+			continue
+		}
+		_, _ = k.ClosePositionIfUnderStopLossPrice(ctx, &position, pool, ammPool)
+	}
 
 	return &types.MsgClosePositionsResponse{}, nil
 }
