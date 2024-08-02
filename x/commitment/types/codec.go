b34880b396e721825c5204b89e914df903e1db72
Warao Gil <wari@elys.network>
2024-08-02 12:25:21 -0400

    Update commitment module

--- x/commitment/types/codec.go
+++ x/commitment/types/codec.go
@@ -2,24 +2,29 @@ package types
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/legacy"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
+	authzcodec "github.com/cosmos/cosmos-sdk/x/authz/codec"
+	govcodec "github.com/cosmos/cosmos-sdk/x/gov/codec"
+	groupcodec "github.com/cosmos/cosmos-sdk/x/group/codec"
 )
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
-	cdc.RegisterConcrete(&MsgCommitClaimedRewards{}, "commitment/CommitClaimedRewards", nil)
-	cdc.RegisterConcrete(&MsgUncommitTokens{}, "commitment/UncommitTokens", nil)
-	cdc.RegisterConcrete(&MsgClaimReward{}, "commitment/ClaimReward", nil)
-	cdc.RegisterConcrete(&MsgVest{}, "commitment/Vest", nil)
-	cdc.RegisterConcrete(&MsgClaimVesting{}, "commitment/ClaimVesting", nil)
-	cdc.RegisterConcrete(&MsgCancelVest{}, "commitment/CancelVest", nil)
-	cdc.RegisterConcrete(&MsgVestNow{}, "commitment/VestNow", nil)
-	cdc.RegisterConcrete(&MsgUpdateVestingInfo{}, "commitment/UpdateVestingInfo", nil)
-	cdc.RegisterConcrete(&MsgVestLiquid{}, "commitment/VestLiquid", nil)
-	cdc.RegisterConcrete(&MsgClaimRewards{}, "commitment/ClaimRewards", nil)
-	cdc.RegisterConcrete(&MsgStake{}, "commitment/Stake", nil)
-	cdc.RegisterConcrete(&MsgUnstake{}, "commitment/Unstake", nil)
+	legacy.RegisterAminoMsg(cdc, &MsgCommitClaimedRewards{}, "commitment/CommitClaimedRewards")
+	legacy.RegisterAminoMsg(cdc, &MsgUncommitTokens{}, "commitment/UncommitTokens")
+	legacy.RegisterAminoMsg(cdc, &MsgClaimReward{}, "commitment/ClaimReward")
+	legacy.RegisterAminoMsg(cdc, &MsgVest{}, "commitment/Vest")
+	legacy.RegisterAminoMsg(cdc, &MsgClaimVesting{}, "commitment/ClaimVesting")
+	legacy.RegisterAminoMsg(cdc, &MsgCancelVest{}, "commitment/CancelVest")
+	legacy.RegisterAminoMsg(cdc, &MsgVestNow{}, "commitment/VestNow")
+	legacy.RegisterAminoMsg(cdc, &MsgUpdateVestingInfo{}, "commitment/UpdateVestingInfo")
+	legacy.RegisterAminoMsg(cdc, &MsgVestLiquid{}, "commitment/VestLiquid")
+	legacy.RegisterAminoMsg(cdc, &MsgClaimRewards{}, "commitment/ClaimRewards")
+	legacy.RegisterAminoMsg(cdc, &MsgStake{}, "commitment/Stake")
+	legacy.RegisterAminoMsg(cdc, &MsgUnstake{}, "commitment/Unstake")
 	// this line is used by starport scaffolding # 2
 }
 
@@ -47,3 +52,15 @@ var (
 	Amino     = codec.NewLegacyAmino()
 	ModuleCdc = codec.NewProtoCodec(cdctypes.NewInterfaceRegistry())
 )
+
+func init() {
+    RegisterCodec(Amino)
+    cryptocodec.RegisterCrypto(Amino)
+    sdk.RegisterLegacyAminoCodec(Amino)
+
+    // Register all Amino interfaces and concrete types on the authz  and gov Amino codec so that this can later be
+    // used to properly serialize MsgGrant, MsgExec and MsgSubmitProposal instances
+    RegisterCodec(authzcodec.Amino)
+    RegisterCodec(govcodec.Amino)
+    RegisterCodec(groupcodec.Amino)
+}
