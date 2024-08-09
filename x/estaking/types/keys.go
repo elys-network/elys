7cf5e19e1cd9ed782e32876971bcad0edbc97390
Abhinav Kumar <57705190+avkr003@users.noreply.github.com>
2024-08-09 21:16:57 +0530

    Avkr003/estaking keys (#707)
    * Optimizing ElysStakedKey key
    
    * Optimizing ElysStakeChangeKey key
    
    * Optimizing estaking params key prefix
    
    * Removing redundant code
    
    * Update x/estaking/keeper/elys_staked.go
    
    Co-authored-by: Amit Yadav <amy29981@gmail.com>
    
    ---------
    
    Co-authored-by: Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
    Co-authored-by: Amit Yadav <amy29981@gmail.com>

--- x/estaking/types/keys.go
+++ x/estaking/types/keys.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
+)
+
 const (
 	// ModuleName defines the module name
 	ModuleName = "estaking"
@@ -13,19 +18,32 @@ const (
 	// MemStoreKey defines the in-memory store key
 	MemStoreKey = "mem_estaking"
 
-	// ParamsKey is the prefix to retrieve all Params
-	ParamsKey = "Params/value/"
-
-	ElysStakedKeyPrefix      = "ElysStaked/value/"
-	ElysStakeChangeKeyPrefix = "ElysStakeChanged/value/"
+	LegacyParamsKey                = "Params/value/"
+	LegacyElysStakedKeyPrefix      = "ElysStaked/value/"
+	LegacyElysStakeChangeKeyPrefix = "ElysStakeChanged/value/"
 )
 
-func KeyPrefix(p string) []byte {
+var (
+	ElysStakedKeyPrefix      = []byte{0x01}
+	ElysStakeChangeKeyPrefix = []byte{0x02}
+	ParamsKeyPrefix          = []byte{0x03}
+)
+
+// remove after migration
+func LegacyKeyPrefix(p string) []byte {
 	return []byte(p)
 }
 
-// ElysStakedKey returns the store key to retrieve a ElysStaked from the address fields
-func ElysStakedKey(address string) []byte {
+func GetElysStakedKey(acc sdk.AccAddress) []byte {
+	return append(ElysStakedKeyPrefix, address.MustLengthPrefix(acc)...)
+}
+
+func GetElysStakeChangeKey(acc sdk.AccAddress) []byte {
+	return append(ElysStakeChangeKeyPrefix, address.MustLengthPrefix(acc)...)
+}
+
+// remove after migration
+func LegacyElysStakedKey(address string) []byte {
 	var key []byte
 
 	addressBytes := []byte(address)
