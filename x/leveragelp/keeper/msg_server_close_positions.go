bb0a8126823ca1be1478a7a338944f859d96ee41
Amit Yadav <amy29981@gmail.com>
2024-08-20 18:07:48 +0530

    update

--- x/leveragelp/keeper/msg_server_close_positions.go
+++ x/leveragelp/keeper/msg_server_close_positions.go
@@ -13,7 +13,6 @@ func (k msgServer) ClosePositions(goCtx context.Context, msg *types.MsgClosePosi
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// Handle liquidations
-	leftToLiquidate := len(msg.Liquidate)
 	liqLog := []string{}
 	for _, val := range msg.Liquidate {
 		position, err := k.GetPosition(ctx, val.GetAccountAddress(), val.Id)
@@ -27,18 +26,15 @@ func (k msgServer) ClosePositions(goCtx context.Context, msg *types.MsgClosePosi
 		}
 		isHealthy, isEarly, health, err := k.LiquidatePositionIfUnhealthy(ctx, &position, pool, ammPool)
 		// position is liquidated
-		if !isHealthy && !isEarly {
-			leftToLiquidate--
-		} else if err != nil {
+		if err != nil {
 			// Add log about error or not liquidated
 			liqLog = append(liqLog, "Position: Address:%s Id:%d cannot be liquidated due to err: %s", position.Address, strconv.FormatUint(position.Id, 10), err.Error())
-		} else {
+		} else if !(!isHealthy && !isEarly) {
 			liqLog = append(liqLog, "Position: Address:%s Id:%s is healthy: %s", position.Address, strconv.FormatUint(position.Id, 10), health.String())
 		}
 	}
 
 	// Handle stop loss
-	leftToClose := len(msg.Stoploss)
 	closeLog := []string{}
 	for _, val := range msg.Stoploss {
 		position, err := k.GetPosition(ctx, val.GetAccountAddress(), val.Id)
@@ -51,23 +47,17 @@ func (k msgServer) ClosePositions(goCtx context.Context, msg *types.MsgClosePosi
 			continue
 		}
 		under, early, err := k.ClosePositionIfUnderStopLossPrice(ctx, &position, pool, ammPool)
-		if under && !early {
-			leftToClose--
-		} else if err != nil {
+		if err != nil {
 			// Add log about error or not closed
 			closeLog = append(closeLog, "Position: Address:%s Id:%s cannot be liquidated due to err: %s", position.Address, strconv.FormatUint(position.Id, 10), err.Error())
-		} else {
+		} else if !(under && !early) {
 			closeLog = append(closeLog, "Position: Address:%s Id:%s is not under stop loss", position.Address, strconv.FormatUint(position.Id, 10))
 		}
 	}
 
 	ctx.EventManager().EmitEvent(sdk.NewEvent(types.EventClosePositions,
-		sdk.NewAttribute("liquidations_total", strconv.Itoa(len(msg.Liquidate))),
-		sdk.NewAttribute("liquidations_not_liquidated", strconv.Itoa(leftToLiquidate)),
 		sdk.NewAttribute("liquidations", strings.Join(liqLog, "\n")),
 		sdk.NewAttribute("stop_loss", strings.Join(closeLog, "\n")),
-		sdk.NewAttribute("stop_loss_total", strconv.Itoa(len(msg.Stoploss))),
-		sdk.NewAttribute("stop_loss_not_closed", strconv.Itoa(leftToClose)),
 	))
 
 	return &types.MsgClosePositionsResponse{}, nil
