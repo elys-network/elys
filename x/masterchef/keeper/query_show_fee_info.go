c85b547a378f734140ee16b5465181d2d594bb2b
Amit <amy29981@gmail.com>
2024-08-27 11:17:50 +0530

    add fee queries

---
+++ x/masterchef/keeper/query_show_fee_info.go
@@ -0,0 +1,23 @@
+package keeper
+
+import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/elys-network/elys/x/masterchef/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (k Keeper) ShowFeeInfo(goCtx context.Context, req *types.QueryShowFeeInfoRequest) (*types.QueryShowFeeInfoResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// TODO: Process the query
+	_ = ctx
+
+	return &types.QueryShowFeeInfoResponse{}, nil
+}
