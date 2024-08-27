dc03e63b2ef31f7f621c36f716b450642eb10aab
Amit <amy29981@gmail.com>
2024-08-27 10:18:48 +0530

    fee keeper

--- x/masterchef/keeper/fee_info.go
+++ x/masterchef/keeper/fee_info.go
@@ -1,47 +1,54 @@
-// package keeper
+package keeper
 
-// import (
-// 	sdk "github.com/cosmos/cosmos-sdk/types"
-// 	"github.com/elys-network/elys/x/masterchef/types"
-// )
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/elys-network/elys/x/masterchef/types"
+)
 
-// func (k Keeper) SetFeeInfo(ctx sdk.Context, poolReward types.FeeInfo) {
-// 	store := ctx.KVStore(k.storeKey)
-// 	key := types.GetFeeInfoKey(poolReward.PoolId, poolReward.RewardDenom)
-// 	b := k.cdc.MustMarshal(&poolReward)
-// 	store.Set(key, b)
-// }
+func (k Keeper) SetFeeInfo(ctx sdk.Context, info types.FeeInfo) {
+	store := ctx.KVStore(k.storeKey)
 
-// func (k Keeper) GetFeeInfo(ctx sdk.Context, poolId uint64, rewardDenom string) (val types.FeeInfo, found bool) {
-// 	store := ctx.KVStore(k.storeKey)
-// 	key := types.GetFeeInfoKey(poolId, rewardDenom)
+	// Get the current block time and format it as a string
+	currentTime := ctx.BlockTime()
+	dateString := currentTime.Format("2006-01-02") // YYYY-MM-DD format
 
-// 	b := store.Get(key)
-// 	if b == nil {
-// 		return val, false
-// 	}
+	key := types.GetFeeInfoKey(dateString)
+	b := k.cdc.MustMarshal(&info)
+	store.Set(key, b)
+}
 
-// 	k.cdc.MustUnmarshal(b, &val)
-// 	return val, true
-// }
+func (k Keeper) GetFeeInfo(ctx sdk.Context, timestamp string) (val types.FeeInfo, found bool) {
+	store := ctx.KVStore(k.storeKey)
+	key := types.GetFeeInfoKey(timestamp)
 
-// func (k Keeper) RemoveFeeInfo(ctx sdk.Context, poolId uint64, rewardDenom string) {
-// 	store := ctx.KVStore(k.storeKey)
-// 	key := types.GetFeeInfoKey(poolId, rewardDenom)
-// 	store.Delete(key)
-// }
+	b := store.Get(key)
+	if b == nil {
+		return val, false
+	}
 
-// func (k Keeper) GetAllFeeInfos(ctx sdk.Context) (list []types.FeeInfo) {
-// 	store := ctx.KVStore(k.storeKey)
-// 	iterator := sdk.KVStorePrefixIterator(store, types.FeeInfoKeyPrefix)
+	k.cdc.MustUnmarshal(b, &val)
+	return val, true
+}
 
-// 	defer iterator.Close()
+func (k Keeper) RemoveFeeInfo(ctx sdk.Context, timestamp string) {
+	store := ctx.KVStore(k.storeKey)
+	key := types.GetFeeInfoKey(timestamp)
+	if store.Has(key) {
+		store.Delete(key)
+	}
+}
 
-// 	for ; iterator.Valid(); iterator.Next() {
-// 		var val types.FeeInfo
-// 		k.cdc.MustUnmarshal(iterator.Value(), &val)
-// 		list = append(list, val)
-// 	}
+func (k Keeper) GetAllFeeInfos(ctx sdk.Context) (list []types.FeeInfo) {
+	store := ctx.KVStore(k.storeKey)
+	iterator := sdk.KVStorePrefixIterator(store, types.FeeInfoKeyPrefix)
 
-// 	return
-// }
+	defer iterator.Close()
+
+	for ; iterator.Valid(); iterator.Next() {
+		var val types.FeeInfo
+		k.cdc.MustUnmarshal(iterator.Value(), &val)
+		list = append(list, val)
+	}
+
+	return
+}
