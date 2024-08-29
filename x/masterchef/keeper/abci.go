f50912899d1f23e8e46fe500734565049fd03da6
Amit Yadav <amy29981@gmail.com>
2024-08-29 13:41:39 +0530

    [Masterchef]: Store 7 days fees and add queries to display (#754)
    * add data type
    
    * fee keeper
    
    * add info
    
    * add tests
    
    * delete old data
    
    * add fee queries
    
    * call
    
    * add query tests

--- x/masterchef/keeper/abci.go
+++ x/masterchef/keeper/abci.go
@@ -16,6 +16,7 @@ import (
 func (k Keeper) EndBlocker(ctx sdk.Context) {
 
 	k.DeleteLegacyUserRewardInfos(ctx, 10_000)
+	k.DeleteFeeInfo(ctx)
 
 	// distribute LP rewards
 	k.ProcessLPRewardDistribution(ctx)
@@ -235,6 +236,8 @@ func (k Keeper) UpdateLPRewards(ctx sdk.Context) error {
 			dexRewardsAllocatedForPool = sdk.NewDec(0)
 		}
 
+		k.AddEdenInfo(ctx, newEdenAllocatedForPool)
+
 		// Distribute Eden
 		k.UpdateAccPerShare(ctx, pool.PoolId, ptypes.Eden, newEdenAllocatedForPool.TruncateInt())
 		// Distribute Gas fees + Dex rewards (USDC)
@@ -342,6 +345,8 @@ func (k Keeper) CollectGasFees(ctx sdk.Context, baseCurrency string) sdk.DecCoin
 	gasFeesForStakersDec := gasFeeCollectedDec.MulDecTruncate(params.RewardPortionForStakers)
 	gasFeesForProtocolDec := gasFeeCollectedDec.Sub(gasFeesForLpsDec).Sub(gasFeesForStakersDec)
 
+	k.AddFeeInfo(ctx, gasFeesForLpsDec.AmountOf(baseCurrency), gasFeesForStakersDec.AmountOf(baseCurrency), gasFeesForProtocolDec.AmountOf(baseCurrency), true)
+
 	lpsGasFeeCoins, _ := gasFeesForLpsDec.TruncateDecimal()
 	protocolGasFeeCoins, _ := gasFeesForProtocolDec.TruncateDecimal()
 
@@ -411,6 +416,11 @@ func (k Keeper) CollectDEXRevenue(ctx sdk.Context) (sdk.Coins, sdk.DecCoins, map
 		stakerRevenueCoins, _ := revenuePortionForStakers.TruncateDecimal()
 		protocolRevenueCoins, _ := revenuePortionForProtocol.TruncateDecimal()
 
+		baseCurrency, found := k.assetProfileKeeper.GetUsdcDenom(ctx)
+		if found {
+			k.AddFeeInfo(ctx, revenuePortionForLPs.AmountOf(baseCurrency), revenuePortionForStakers.AmountOf(baseCurrency), revenuePortionForProtocol.AmountOf(baseCurrency), false)
+		}
+
 		// Send coins to fee collector name
 		if stakerRevenueCoins.IsAllPositive() {
 			err := k.bankKeeper.SendCoinsFromModuleToModule(ctx, types.ModuleName, authtypes.FeeCollectorName, stakerRevenueCoins)
@@ -428,8 +438,10 @@ func (k Keeper) CollectDEXRevenue(ctx sdk.Context) (sdk.Coins, sdk.DecCoins, map
 			}
 		}
 
-		// Store revenue portion for Lps temporarilly
-		rewardsPerPool[poolId] = revenuePortionForLPs.AmountOf(ptypes.BaseCurrency)
+		// Store revenue portion for Lps temporarily
+		if found {
+			rewardsPerPool[poolId] = revenuePortionForLPs.AmountOf(baseCurrency)
+		}
 
 		// Sum total collected amount
 		amountTotalCollected = amountTotalCollected.Add(revenue...)
