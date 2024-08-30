b829816db474e3b7f58004dfdd30d14dbde5ea7d
CryptoKage2306 <26vivek06@gmail.com>
2024-08-30 16:59:40 +0530

    feat: add test

--- x/perpetual/types/msgs.go
+++ x/perpetual/types/msgs.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"fmt"
+
 	errorsmod "cosmossdk.io/errors"
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -15,6 +17,7 @@ const (
 	TypeMsgUpdateParams = "update_params"
 	TypeMsgWhitelist    = "whitelist"
 	TypeMsgDewhitelist  = "dewhitelist"
+	TypeMsgUpdateStopLoss= "update_stop_loss"
 )
 
 var (
@@ -23,6 +26,7 @@ var (
 	_ sdk.Msg = &MsgUpdateParams{}
 	_ sdk.Msg = &MsgWhitelist{}
 	_ sdk.Msg = &MsgDewhitelist{}
+	_ sdk.Msg = &MsgUpdateStopLoss{}
 )
 
 func NewMsgClose(creator string, id uint64, amount math.Int) *MsgClose {
@@ -289,6 +293,47 @@ func (msg *MsgDewhitelist) ValidateBasic() error {
 	_, err := sdk.AccAddressFromBech32(msg.Authority)
 	if err != nil {
 		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
+
+func NewMsgUpdateStopLoss(creator string, id uint64, price sdk.Dec) *MsgUpdateStopLoss {
+	return &MsgUpdateStopLoss{
+		Creator:  creator,
+		Id: id,
+		Price:    price,
+	}
+}
+
+func (msg *MsgUpdateStopLoss) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgUpdateStopLoss) Type() string {
+	return TypeMsgUpdateStopLoss
+}
+
+func (msg *MsgUpdateStopLoss) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgUpdateStopLoss) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgUpdateStopLoss) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+
+	if msg.Price.IsNegative() {
+		return fmt.Errorf("stop loss price cannot be negative")
 	}
 	return nil
 }
