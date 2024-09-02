e56fc3666a9bcf85d22c82a76cb24c50f078e4fc
Amit <amy29981@gmail.com>
2024-09-02 15:55:37 +0530

    add funding rate DS and logic

--- x/perpetual/keeper/pool.go
+++ x/perpetual/keeper/pool.go
@@ -150,3 +150,113 @@ func (k Keeper) GetBorrowRate(ctx sdk.Context, startBlock uint64, pool uint64, s
 		RoundInt()
 	return newInterest
 }
+
+func (k Keeper) SetFundingRate(ctx sdk.Context, block uint64, pool uint64, funding types.FundingRateBlock) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.FundingRatePrefix)
+	prev := types.GetFundingRateKey(block-1, pool)
+	key := types.GetFundingRateKey(block, pool)
+	if store.Has(prev) {
+		lastBlock := types.FundingRateBlock{}
+		bz := store.Get(prev)
+		k.cdc.MustUnmarshal(bz, &lastBlock)
+		funding.FundingRate = funding.FundingRate.Add(lastBlock.FundingRate)
+
+		bz = k.cdc.MustMarshal(&funding)
+		store.Set(key, bz)
+	} else {
+		bz := k.cdc.MustMarshal(&funding)
+		store.Set(key, bz)
+	}
+}
+
+// Test it out
+// Deletes all pool blocks at delBlock
+func (k Keeper) DeleteFundingRate(ctx sdk.Context, delBlock uint64) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), append(types.FundingRatePrefix, types.GetUint64Bytes(delBlock)...))
+	iterator := sdk.KVStorePrefixIterator(store, nil)
+	defer iterator.Close()
+
+	for ; iterator.Valid(); iterator.Next() {
+		store.Delete(iterator.Key())
+	}
+}
+
+func (k Keeper) GetAllFundingRate(ctx sdk.Context) []types.FundingRateBlock {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.FundingRatePrefix)
+	iterator := sdk.KVStorePrefixIterator(store, nil)
+	defer iterator.Close()
+
+	fundings := []types.FundingRateBlock{}
+	for ; iterator.Valid(); iterator.Next() {
+		funding := types.FundingRateBlock{}
+		k.cdc.MustUnmarshal(iterator.Value(), &funding)
+
+		fundings = append(fundings, funding)
+	}
+	return fundings
+}
+
+func (k Keeper) GetFundingRate(ctx sdk.Context, startBlock uint64, pool uint64, startTime uint64, custody sdk.Dec) sdk.Int {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.FundingRatePrefix)
+	currentBlockKey := types.GetFundingRateKey(uint64(ctx.BlockHeight()), pool)
+	startBlockKey := types.GetFundingRateKey(startBlock, pool)
+
+	// note: exclude start block
+	if store.Has(startBlockKey) && store.Has(currentBlockKey) && startBlock != uint64(ctx.BlockHeight()) {
+		bz := store.Get(startBlockKey)
+		startFundingBlock := types.FundingRateBlock{}
+		k.cdc.MustUnmarshal(bz, &startFundingBlock)
+
+		bz = store.Get(currentBlockKey)
+		endFundingBlock := types.FundingRateBlock{}
+		k.cdc.MustUnmarshal(bz, &endFundingBlock)
+
+		totalFunding := endFundingBlock.FundingRate.Sub(startFundingBlock.FundingRate)
+		numberOfBlocks := ctx.BlockHeight() - int64(startBlock)
+
+		newFunding := custody.
+			Mul(totalFunding).
+			Mul(sdk.NewDec(ctx.BlockTime().Unix() - int64(startTime))).
+			Quo(sdk.NewDec(numberOfBlocks)).
+			Quo(sdk.NewDec(86400 * 365)).
+			RoundInt()
+		return newFunding
+	}
+
+	if !store.Has(startBlockKey) && store.Has(currentBlockKey) {
+		iterator := sdk.KVStorePrefixIterator(store, nil)
+		defer iterator.Close()
+
+		firstStoredBlock := uint64(0)
+		if iterator.Valid() {
+			fundingBlock := types.FundingRateBlock{}
+			firstStoredBlock = sdk.BigEndianToUint64(iterator.Key())
+			k.cdc.MustUnmarshal(iterator.Value(), &fundingBlock)
+		}
+		if firstStoredBlock > startBlock {
+			bz := store.Get(currentBlockKey)
+			endFundingBlock := types.FundingRateBlock{}
+			k.cdc.MustUnmarshal(bz, &endFundingBlock)
+
+			totalFunding := endFundingBlock.FundingRate
+			numberOfBlocks := ctx.BlockHeight() - int64(startBlock) + 1
+
+			newFunding := custody.Mul(totalFunding).
+				Mul(sdk.NewDec(ctx.BlockTime().Unix() - int64(startTime))).
+				Quo(sdk.NewDec(numberOfBlocks)).
+				Quo(sdk.NewDec(86400 * 365)).
+				RoundInt()
+			return newFunding
+		}
+	}
+	params, found := k.GetPool(ctx, pool)
+	if !found {
+		return sdk.ZeroInt()
+	}
+	newFunding := custody.
+		Mul(params.BorrowInterestRate).
+		Mul(sdk.NewDec(ctx.BlockTime().Unix() - int64(startTime))).
+		Quo(sdk.NewDec(86400 * 365)).
+		RoundInt()
+	return newFunding
+}
