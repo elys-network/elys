cfaa7a213192d781c3ad392157de797ac0c6396f
Amit <amy29981@gmail.com>
2024-09-06 13:34:35 +0530

    fix

--- x/stablestake/types/message_update_params_test.go
+++ x/stablestake/types/message_update_params_test.go
@@ -3,6 +3,7 @@ package types
 import (
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/elys-network/elys/testutil/sample"
 	"github.com/stretchr/testify/require"
@@ -18,12 +19,36 @@ func TestMsgUpdateParams_ValidateBasic(t *testing.T) {
 			name: "invalid address",
 			msg: MsgUpdateParams{
 				Authority: "invalid_address",
+				Params: &Params{
+					DepositDenom:         "stake",
+					RedemptionRate:       sdk.NewDecWithPrec(20, 1), // 2.0
+					EpochLength:          1000,
+					InterestRate:         sdk.NewDecWithPrec(3, 2), // 0.03
+					InterestRateMax:      sdk.NewDecWithPrec(5, 2), // 0.05
+					InterestRateMin:      sdk.NewDecWithPrec(1, 2), // 0.01
+					InterestRateIncrease: sdk.NewDecWithPrec(2, 2), // 0.02
+					InterestRateDecrease: sdk.NewDecWithPrec(1, 2), // 0.01
+					HealthGainFactor:     sdk.NewDecWithPrec(1, 1), // 0.1
+					TotalValue:           sdk.NewInt(1000),
+				},
 			},
 			err: sdkerrors.ErrInvalidAddress,
 		}, {
 			name: "valid address",
 			msg: MsgUpdateParams{
 				Authority: sample.AccAddress(),
+				Params: &Params{
+					DepositDenom:         "stake",
+					RedemptionRate:       sdk.NewDecWithPrec(20, 1), // 2.0
+					EpochLength:          1000,
+					InterestRate:         sdk.NewDecWithPrec(3, 2), // 0.03
+					InterestRateMax:      sdk.NewDecWithPrec(5, 2), // 0.05
+					InterestRateMin:      sdk.NewDecWithPrec(1, 2), // 0.01
+					InterestRateIncrease: sdk.NewDecWithPrec(2, 2), // 0.02
+					InterestRateDecrease: sdk.NewDecWithPrec(1, 2), // 0.01
+					HealthGainFactor:     sdk.NewDecWithPrec(1, 1), // 0.1
+					TotalValue:           sdk.NewInt(1000),
+				},
 			},
 		},
 	}
