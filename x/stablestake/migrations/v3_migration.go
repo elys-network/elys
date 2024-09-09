73c956359b65a5526c8aa1c37e3396e3c42bc1ef
avkr003 <abhinav@elys.network>
2024-09-09 16:36:24 +0530

    adding multi-step migration

--- x/stablestake/migrations/v3_migration.go
+++ x/stablestake/migrations/v3_migration.go
@@ -5,17 +5,12 @@ import (
 )
 
 func (m Migrator) V3Migration(ctx sdk.Context) error {
-
-	params := m.keeper.GetLegacyParams(ctx)
-	m.keeper.SetParams(ctx, params)
 	// Migrate the interest blocks
 	interests := m.keeper.GetAllLegacyInterest(ctx)
 
 	for _, interest := range interests {
 		m.keeper.SetInterest(ctx, interest.BlockHeight, interest)
 	}
-
-	m.keeper.V6_DebtMigration(ctx)
 
 	return nil
 }
