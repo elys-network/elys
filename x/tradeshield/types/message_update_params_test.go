3d7659cef766da34997c6050efe3c2726f03bd59
Amit Yadav <amy29981@gmail.com>
2024-09-18 16:15:57 +0530

    fix

--- x/tradeshield/types/message_update_params_test.go
+++ x/tradeshield/types/message_update_params_test.go
@@ -3,6 +3,7 @@ package types
 import (
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/elys-network/elys/testutil/sample"
 	"github.com/stretchr/testify/require"
@@ -18,13 +19,59 @@ func TestMsgUpdateParams_ValidateBasic(t *testing.T) {
 			name: "invalid address",
 			msg: MsgUpdateParams{
 				Authority: "invalid_address",
+				Params: &Params{
+					MarketOrderEnabled:   true,
+					StakeEnabled:         true,
+					ProcessOrdersEnabled: true,
+					SwapEnabled:          true,
+					PerpetualEnabled:     true,
+					RewardEnabled:        true,
+					LeverageEnabled:      true,
+					LimitProcessOrder:    1,
+					RewardPercentage:     sdk.MustNewDecFromStr("0.5"),
+					MarginError:          sdk.MustNewDecFromStr("0.1"),
+					MinimumDeposit:       sdk.NewInt(100),
+				},
 			},
 			err: sdkerrors.ErrInvalidAddress,
 		}, {
 			name: "valid address",
 			msg: MsgUpdateParams{
 				Authority: sample.AccAddress(),
+				Params: &Params{
+					MarketOrderEnabled:   true,
+					StakeEnabled:         true,
+					ProcessOrdersEnabled: true,
+					SwapEnabled:          true,
+					PerpetualEnabled:     true,
+					RewardEnabled:        true,
+					LeverageEnabled:      true,
+					LimitProcessOrder:    1,
+					RewardPercentage:     sdk.MustNewDecFromStr("0.5"),
+					MarginError:          sdk.MustNewDecFromStr("0.1"),
+					MinimumDeposit:       sdk.NewInt(100),
+				},
 			},
+		},
+		{
+			name: "negative RewardPercentage",
+			msg: MsgUpdateParams{
+				Authority: sample.AccAddress(),
+				Params: &Params{
+					MarketOrderEnabled:   true,
+					StakeEnabled:         true,
+					ProcessOrdersEnabled: true,
+					SwapEnabled:          true,
+					PerpetualEnabled:     true,
+					RewardEnabled:        true,
+					LeverageEnabled:      true,
+					LimitProcessOrder:    1,
+					RewardPercentage:     sdk.MustNewDecFromStr("-0.5"),
+					MarginError:          sdk.MustNewDecFromStr("0.1"),
+					MinimumDeposit:       sdk.NewInt(100),
+				},
+			},
+			err: sdkerrors.ErrInvalidRequest,
 		},
 	}
 	for _, tt := range tests {
