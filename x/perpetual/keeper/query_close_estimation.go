b8f5c55aef1a953b66adcdf6b9c0d0603f31c89f
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-10-02 20:47:57 +0200

    test(perpetual): fix tests

--- x/perpetual/keeper/query_close_estimation.go
+++ x/perpetual/keeper/query_close_estimation.go
@@ -4,13 +4,16 @@ import (
 	"context"
 
 	errorsmod "cosmossdk.io/errors"
+	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	assetprofiletypes "github.com/elys-network/elys/x/assetprofile/types"
+	ptypes "github.com/elys-network/elys/x/parameter/types"
 	"github.com/elys-network/elys/x/perpetual/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-func (k Keeper) CloseEstimation(goCtx context.Context, req *types.QueryCloseEstimationRequest) (*types.QueryCloseEstimationResponse, error) {
+func (k Keeper) CloseEstimation(goCtx context.Context, req *types.QueryCloseEstimationRequest) (res *types.QueryCloseEstimationResponse, err error) {
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
@@ -21,13 +24,13 @@ func (k Keeper) CloseEstimation(goCtx context.Context, req *types.QueryCloseEsti
 	if err != nil {
 		return &types.QueryCloseEstimationResponse{}, err
 	}
-	mtp, err := k.CloseLongChecker.GetMTP(ctx, address, req.PositionId)
+	mtp, err := k.CloseEstimationChecker.GetMTP(ctx, address, req.PositionId)
 	if err != nil {
 		return &types.QueryCloseEstimationResponse{}, err
 	}
 
 	// Retrieve Pool
-	pool, found := k.CloseLongChecker.GetPool(ctx, mtp.AmmPoolId)
+	pool, found := k.CloseEstimationChecker.GetPool(ctx, mtp.AmmPoolId)
 	if !found {
 		return &types.QueryCloseEstimationResponse{}, errorsmod.Wrap(types.ErrInvalidBorrowingAsset, "invalid pool id")
 	}
@@ -35,25 +38,56 @@ func (k Keeper) CloseEstimation(goCtx context.Context, req *types.QueryCloseEsti
 	_ = pool
 
 	// Retrieve AmmPool
-	ammPool, err := k.CloseLongChecker.GetAmmPool(ctx, mtp.AmmPoolId, mtp.CustodyAsset)
+	ammPool, err := k.CloseEstimationChecker.GetAmmPool(ctx, mtp.AmmPoolId, mtp.CustodyAsset)
 	if err != nil {
 		return &types.QueryCloseEstimationResponse{}, err
 	}
 
 	_ = ammPool
 
-	// TODO: estimate settle borrow interest
+	// get base currency entry
+	entry, found := k.assetProfileKeeper.GetEntry(ctx, ptypes.BaseCurrency)
+	if !found {
+		return &types.QueryCloseEstimationResponse{}, errorsmod.Wrapf(assetprofiletypes.ErrAssetProfileNotFound, "asset %s not found", ptypes.BaseCurrency)
+	}
+	baseCurrency := entry.Denom
 
-	// TODO: estimate take out custody
+	// init repay amount
+	var repayAmount math.Int
 
-	// TODO: estimate swap and repay
+	// if position is long, repay in collateral asset
+	if mtp.Position == types.Position_LONG {
+		custodyAmtTokenIn := sdk.NewCoin(mtp.CustodyAsset, mtp.Custody)
+		repayAmount, err = k.CloseEstimationChecker.EstimateSwap(ctx, custodyAmtTokenIn, mtp.CollateralAsset, ammPool)
+		if err != nil {
+			return &types.QueryCloseEstimationResponse{}, err
+		}
+	} else if mtp.Position == types.Position_SHORT {
+		// if position is short, repay in trading asset
+		custodyAmtTokenIn := sdk.NewCoin(mtp.CustodyAsset, mtp.Custody)
+		repayAmount, err = k.CloseEstimationChecker.EstimateSwap(ctx, custodyAmtTokenIn, mtp.TradingAsset, ammPool)
+		if err != nil {
+			return &types.QueryCloseEstimationResponse{}, err
+		}
+	} else {
+		return &types.QueryCloseEstimationResponse{}, types.ErrInvalidPosition
+	}
+
+	returnAmount, err := k.CalcReturnAmount(ctx, mtp, pool, ammPool, repayAmount, mtp.Custody, baseCurrency)
+	if err != nil {
+		return &types.QueryCloseEstimationResponse{}, err
+	}
+
+	// get swap fee param
+	swapFee := k.GetSwapFee(ctx)
 
 	return &types.QueryCloseEstimationResponse{
 		Position:     mtp.Position,
 		PositionSize: sdk.NewCoin(mtp.CustodyAsset, mtp.Custody),
 		Liabilities:  sdk.NewCoin(mtp.LiabilitiesAsset, mtp.Liabilities),
+		// TODO: price impact calculation
 		PriceImpact:  sdk.ZeroDec(),
-		SwapFee:      sdk.ZeroDec(),
-		RepayAmount:  sdk.Coin{},
+		SwapFee:      swapFee,
+		ReturnAmount: sdk.NewCoin(mtp.CollateralAsset, returnAmount),
 	}, nil
 }
--- x/perpetual/keeper/query_close_estimation_test.go
+++ x/perpetual/keeper/query_close_estimation_test.go
@@ -5,7 +5,9 @@ import (
 
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	keepertest "github.com/elys-network/elys/testutil/keeper"
 	ammtypes "github.com/elys-network/elys/x/amm/types"
+	atypes "github.com/elys-network/elys/x/assetprofile/types"
 	ptypes "github.com/elys-network/elys/x/parameter/types"
 	"github.com/elys-network/elys/x/perpetual/keeper"
 	"github.com/elys-network/elys/x/perpetual/types"
@@ -15,11 +17,11 @@ import (
 
 func TestCloseEstimation_InvalidRequest(t *testing.T) {
 	// Setup the mock checker
-	mockChecker := new(mocks.CloseLongChecker)
+	mockChecker := new(mocks.CloseEstimationChecker)
 
 	// Create an instance of Keeper with the mock checker
 	k := keeper.Keeper{
-		CloseLongChecker: mockChecker,
+		CloseEstimationChecker: mockChecker,
 	}
 
 	// Mock behavior
@@ -31,11 +33,11 @@ func TestCloseEstimation_InvalidRequest(t *testing.T) {
 
 func TestCloseEstimation_InvalidAddress(t *testing.T) {
 	// Setup the mock checker
-	mockChecker := new(mocks.CloseLongChecker)
+	mockChecker := new(mocks.CloseEstimationChecker)
 
 	// Create an instance of Keeper with the mock checker
 	k := keeper.Keeper{
-		CloseLongChecker: mockChecker,
+		CloseEstimationChecker: mockChecker,
 	}
 
 	var (
@@ -55,11 +57,11 @@ func TestCloseEstimation_InvalidAddress(t *testing.T) {
 
 func TestCloseEstimation_MTPNotFound(t *testing.T) {
 	// Setup the mock checker
-	mockChecker := new(mocks.CloseLongChecker)
+	mockChecker := new(mocks.CloseEstimationChecker)
 
 	// Create an instance of Keeper with the mock checker
 	k := keeper.Keeper{
-		CloseLongChecker: mockChecker,
+		CloseEstimationChecker: mockChecker,
 	}
 
 	address := sdk.AccAddress([]byte("address"))
@@ -82,53 +84,66 @@ func TestCloseEstimation_MTPNotFound(t *testing.T) {
 }
 
 func TestCloseEstimation_ExistingMTP(t *testing.T) {
-	// Setup the mock checker
-	mockChecker := new(mocks.CloseLongChecker)
+	// Setup the perpetual keeper
+	k, ctx, assetProfileKeeper := keepertest.PerpetualKeeper(t)
 
-	// Create an instance of Keeper with the mock checker
-	k := keeper.Keeper{
-		CloseLongChecker: mockChecker,
-	}
+	// Setup the mock checker
+	mockChecker := new(mocks.CloseEstimationChecker)
+
+	// assign the mock checker to the keeper
+	k.CloseEstimationChecker = mockChecker
 
 	address := sdk.AccAddress([]byte("address"))
 
+	// get swap fee param
+	swapFee := k.GetSwapFee(ctx)
+
 	var (
-		ctx   = sdk.Context{} // Mock or setup a context
 		query = &types.QueryCloseEstimationRequest{
 			Address:    address.String(),
 			PositionId: 1,
 		}
 		mtp = types.MTP{
-			AmmPoolId:        2,
-			CollateralAsset:  ptypes.BaseCurrency,
-			Collateral:       sdk.NewInt(100),
-			CustodyAsset:     "uatom",
-			Custody:          sdk.NewInt(50),
-			LiabilitiesAsset: ptypes.BaseCurrency,
-			Liabilities:      sdk.NewInt(400),
-			TradingAsset:     "uatom",
-			Position:         types.Position_LONG,
+			AmmPoolId:                      2,
+			CollateralAsset:                ptypes.BaseCurrency,
+			Collateral:                     sdk.NewInt(100),
+			CustodyAsset:                   "uatom",
+			Custody:                        sdk.NewInt(50),
+			LiabilitiesAsset:               ptypes.BaseCurrency,
+			Liabilities:                    sdk.NewInt(400),
+			TradingAsset:                   "uatom",
+			Position:                       types.Position_LONG,
+			BorrowInterestUnpaidCollateral: sdk.NewInt(10),
 		}
 		pool = types.Pool{
 			BorrowInterestRate: math.LegacyNewDec(2),
 		}
-		ammPool = ammtypes.Pool{}
+		ammPool      = ammtypes.Pool{}
+		baseCurrency = "usdc"
 	)
 
 	// Mock behavior
-	mockChecker.On("GetMTP", ctx, sdk.MustAccAddressFromBech32(query.Address), query.PositionId).Return(mtp, nil)
-	mockChecker.On("GetPool", ctx, mtp.AmmPoolId).Return(pool, true)
-	mockChecker.On("GetAmmPool", ctx, mtp.AmmPoolId, mtp.CustodyAsset).Return(ammPool, nil)
+	mockChecker.On("GetMTP", ctx, sdk.MustAccAddressFromBech32(query.Address), query.PositionId).Return(mtp, nil).Once()
+	mockChecker.On("GetPool", ctx, mtp.AmmPoolId).Return(pool, true).Once()
+	mockChecker.On("GetAmmPool", ctx, mtp.AmmPoolId, mtp.CustodyAsset).Return(ammPool, nil).Once()
+	mockChecker.On("EstimateSwap", ctx, sdk.NewCoin(mtp.CustodyAsset, mtp.Custody), mtp.CollateralAsset, ammPool).Return(math.NewInt(10000), nil).Once()
+	mockChecker.On("EstimateSwapGivenOut", ctx, sdk.NewCoin(mtp.CollateralAsset, mtp.BorrowInterestUnpaidCollateral), baseCurrency, ammPool).Return(math.NewInt(200), nil).Once()
+	mockChecker.On("EstimateSwapGivenOut", ctx, sdk.NewCoin(baseCurrency, sdk.NewInt(9400)), mtp.CollateralAsset, ammPool).Return(math.NewInt(9400), nil).Once()
+
+	assetProfileKeeper.On("GetEntry", ctx, ptypes.BaseCurrency).Return(atypes.Entry{
+		Denom: baseCurrency,
+	}, true)
 
 	res, err := k.CloseEstimation(ctx, query)
 	assert.NoError(t, err)
 
 	mockChecker.AssertExpectations(t)
+	assetProfileKeeper.AssertExpectations(t)
 
 	assert.Equal(t, mtp.Position, res.Position)
 	assert.Equal(t, mtp.Custody, res.PositionSize.Amount)
 	assert.Equal(t, mtp.Liabilities, res.Liabilities.Amount)
 	assert.Equal(t, sdk.ZeroDec(), res.PriceImpact)
-	assert.Equal(t, sdk.ZeroDec(), res.SwapFee)
-	assert.Equal(t, sdk.Coin{}, res.RepayAmount)
+	assert.Equal(t, swapFee, res.SwapFee)
+	assert.Equal(t, sdk.NewCoin(mtp.CollateralAsset, sdk.NewInt(9400)), res.ReturnAmount)
 }
