797964453a4744e6dc901a22b3b2b8aa3b6b8a8d
CryptoKage2306 <26vivek06@gmail.com>
2024-10-15 18:40:50 +0530

    update

--- x/perpetual/keeper/updated_leverage.go
+++ x/perpetual/keeper/updated_leverage.go
@@ -1,23 +1,30 @@
 package keeper
 
 import (
+	errorsmod "cosmossdk.io/errors"
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	assetprofiletypes "github.com/elys-network/elys/x/assetprofile/types"
 	ptypes "github.com/elys-network/elys/x/parameter/types"
 	"github.com/elys-network/elys/x/perpetual/types"
 )
 
 func (k Keeper) UpdatedLeverage(ctx sdk.Context, mtp types.MTP) (sdk.Dec, error) {
+	entry, found := k.assetProfileKeeper.GetEntry(ctx, ptypes.BaseCurrency)
+	if !found {
+		return sdk.ZeroDec(), errorsmod.Wrapf(assetprofiletypes.ErrAssetProfileNotFound, "asset %s not found", ptypes.BaseCurrency)
+	}
+	baseCurrency := entry.Denom
 	var custody_in_usdc sdk.Dec
-	if mtp.CustodyAsset != ptypes.BaseCurrency {
-		price := k.amm.EstimatePrice(ctx, mtp.CustodyAsset, ptypes.BaseCurrency)
+	if mtp.CustodyAsset != baseCurrency {
+		price := k.amm.EstimatePrice(ctx, mtp.CustodyAsset, baseCurrency)
 		custody_in_usdc = math.LegacyDec(mtp.Custody).Mul(price)
 	}else {
 		custody_in_usdc = math.LegacyDec(mtp.Custody)
 	}
 	var denominator sdk.Dec
-	if mtp.LiabilitiesAsset != ptypes.BaseCurrency {
-		price := k.amm.EstimatePrice(ctx, mtp.CustodyAsset, ptypes.BaseCurrency)
+	if mtp.LiabilitiesAsset != baseCurrency {
+		price := k.amm.EstimatePrice(ctx, mtp.CustodyAsset, baseCurrency)
 		denominator = custody_in_usdc.Sub(math.LegacyDec(mtp.Liabilities).Mul(price))
 	}else {
 		denominator = custody_in_usdc.Sub(math.LegacyDec(mtp.Liabilities))
