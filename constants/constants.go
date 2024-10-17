e85a3b22cbc5bd286e0015a207294c0db17ef8e6
avkr003 <abhinav@elys.network>
2024-10-17 22:50:21 +0530

    fixing params address

--- constants/constants.go
+++ constants/constants.go
@@ -2,7 +2,7 @@ package constants
 
 import authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 
-var ZeroAddress = ""
+var ZeroAddress = authtypes.NewModuleAddress("zero").String() // prefix will be in cosmos1... so this will fail, updated below
 
 // SetZeroAddress Should be called after setting prefixes for address
 func SetZeroAddress() {
