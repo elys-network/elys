9afb971773f45dfa4cc9c5ad6a5200640870976f
Servio <servio.za@gmail.com>
2024-10-18 12:41:53 -0300

    update amm  keeper test

--- x/amm/keeper/keeper_test.go
+++ x/amm/keeper/keeper_test.go
@@ -5,11 +5,6 @@ import (
 
 	sdkmath "cosmossdk.io/math"
 
-<<<<<<< Updated upstream
-	sdkmath "cosmossdk.io/math"
-
-=======
->>>>>>> Stashed changes
 	"github.com/cometbft/cometbft/crypto/ed25519"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
