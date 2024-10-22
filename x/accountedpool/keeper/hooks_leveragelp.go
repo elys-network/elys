1cfcb447bc2198274f57a6b661f68ccb9412fafc
avkr003 <abhinav@elys.network>
2024-10-22 22:54:38 +0530

    returning error

--- x/accountedpool/keeper/hooks_leveragelp.go
+++ x/accountedpool/keeper/hooks_leveragelp.go
@@ -14,7 +14,7 @@ func (k Keeper) OnLeverageLpPoolEnable(ctx sdk.Context, ammPool ammtypes.Pool) e
 	// Check if already exists
 	exists := k.PoolExists(ctx, poolId)
 	if exists {
-		return nil
+		return types.ErrPoolAlreadyExist
 	}
 
 	// Initiate pool
