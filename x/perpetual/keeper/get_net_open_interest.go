b4d43bc041fe55f0d756d3f75df47e39872a490e
Amit Yadav <amy29981@gmail.com>
2024-10-24 14:24:22 +0530

    add to query

--- x/perpetual/keeper/get_net_open_interest.go
+++ x/perpetual/keeper/get_net_open_interest.go
@@ -26,3 +26,31 @@ func (k Keeper) GetNetOpenInterest(ctx sdk.Context, pool types.Pool) math.Int {
 
 	return netOpenInterest
 }
+
+func (k Keeper) GetFundingPaymentRates(ctx sdk.Context, pool types.Pool) (long sdk.Dec, short sdk.Dec) {
+	fundingRateLong, fundingRateShort := k.ComputeFundingRate(ctx, pool)
+
+	// account custody from long position
+	totalCustodyLong := sdk.ZeroInt()
+	for _, asset := range pool.PoolAssetsLong {
+		totalCustodyLong = totalCustodyLong.Add(asset.Custody)
+	}
+
+	// account custody from short position
+	totalLiabilitiesShort := sdk.ZeroInt()
+	for _, asset := range pool.PoolAssetsShort {
+		totalLiabilitiesShort = totalLiabilitiesShort.Add(asset.Liabilities)
+	}
+
+	if fundingRateLong.IsZero() {
+		// short will pay
+		// long will receive
+		unpopular_rate := fundingRateShort.Mul(totalLiabilitiesShort.ToLegacyDec()).Quo(totalCustodyLong.ToLegacyDec())
+		return unpopular_rate, fundingRateShort.Neg()
+	} else {
+		// long will pay
+		// short will receive
+		unpopular_rate := fundingRateLong.Mul(totalCustodyLong.ToLegacyDec()).Quo(totalLiabilitiesShort.ToLegacyDec())
+		return fundingRateLong.Neg(), unpopular_rate
+	}
+}
