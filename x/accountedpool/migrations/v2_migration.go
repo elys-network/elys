a9e11ab07630f62135fec5e6aff4778986cfc071
avkr003 <abhinav@elys.network>
2024-10-30 21:00:53 +0530

    fixing get all perpetual query

--- x/accountedpool/migrations/v2_migration.go
+++ x/accountedpool/migrations/v2_migration.go
@@ -5,9 +5,5 @@ import (
 )
 
 func (m Migrator) V2Migration(ctx sdk.Context) error {
-	allAccountedPool := m.keeper.GetAllAccountedPool(ctx)
-	for _, accountedPool := range allAccountedPool {
-		m.keeper.RemoveAccountedPool(ctx, accountedPool.PoolId)
-	}
 	return nil
 }
