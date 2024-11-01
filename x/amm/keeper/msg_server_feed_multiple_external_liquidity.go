8028f611c06fa9814340c03d119fe73ba647b47a
Amit <amy29981@gmail.com>
2024-11-01 12:32:26 +0530

    update

--- x/amm/keeper/msg_server_feed_multiple_external_liquidity.go
+++ x/amm/keeper/msg_server_feed_multiple_external_liquidity.go
@@ -9,26 +9,29 @@ import (
 	oracletypes "github.com/elys-network/elys/x/oracle/types"
 )
 
-func (k Keeper) SetExternalLiquidityRatio(ctx sdk.Context, pool *types.Pool, amountDepthInfo []types.AssetAmountDepth) error {
-	for _, asset := range pool.PoolAssets {
+func (k Keeper) GetExternalLiquidityRatio(ctx sdk.Context, pool types.Pool, amountDepthInfo []types.AssetAmountDepth) ([]types.PoolAsset, error) {
+	updatedAssets := make([]types.PoolAsset, len(pool.PoolAssets))
+	copy(updatedAssets, pool.PoolAssets)
+
+	for i, asset := range updatedAssets {
 		for _, el := range amountDepthInfo {
 			if asset.Token.Denom == el.Asset {
 				price, found := k.oracleKeeper.GetAssetPrice(ctx, el.Asset)
 				if !found {
-					return fmt.Errorf("asset price not set: %s", el.Asset)
+					return nil, fmt.Errorf("asset price not set: %s", el.Asset)
 				} else {
 					O_Tvl := price.Price.Mul(el.Amount)
 					P_Tvl := asset.Token.Amount.ToLegacyDec().Mul(price.Price)
 
 					// Ensure tvl is not zero to avoid division by zero
 					if P_Tvl.IsZero() {
-						return types.ErrAmountTooLow
+						return nil, types.ErrAmountTooLow
 					}
 
 					liquidityRatio := LiquidityRatioFromPriceDepth(el.Depth)
 					// Ensure tvl is not zero to avoid division by zero
 					if liquidityRatio.IsZero() {
-						return types.ErrAmountTooLow
+						return nil, types.ErrAmountTooLow
 					}
 					asset.ExternalLiquidityRatio = (O_Tvl.Quo(P_Tvl)).Quo(liquidityRatio)
 
@@ -38,8 +41,9 @@ func (k Keeper) SetExternalLiquidityRatio(ctx sdk.Context, pool *types.Pool, amo
 				}
 			}
 		}
+		updatedAssets[i] = asset
 	}
-	return nil
+	return updatedAssets, nil
 }
 
 func LiquidityRatioFromPriceDepth(depth sdk.Dec) sdk.Dec {
@@ -71,13 +75,13 @@ func (k msgServer) FeedMultipleExternalLiquidity(goCtx context.Context, msg *typ
 			return nil, types.ErrInvalidPoolId
 		}
 
-		// Set external liquidity ratio
-		// TODO: Add more comments
-		err := k.SetExternalLiquidityRatio(ctx, &pool, el.AmountDepthInfo)
+		// Set external liquidity ratio for each of the asset separately
+		poolAssets, err := k.GetExternalLiquidityRatio(ctx, pool, el.AmountDepthInfo)
 		if err != nil {
 			return nil, err
 		}
 
+		pool.PoolAssets = poolAssets
 		k.SetPool(ctx, pool)
 	}
 
