f176c00adf39753c7a15f42adfce37efea454280
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-11-04 18:47:44 +0100

    test(commitment): improve code coverage queries and types (#908)

--- x/commitment/types/commitments_test.go
+++ x/commitment/types/commitments_test.go
@@ -2,7 +2,9 @@ package types_test
 
 import (
 	"testing"
+	"time"
 
+	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/elys-network/elys/x/commitment/types"
 	"github.com/stretchr/testify/require"
@@ -71,4 +73,160 @@ func TestLockupAmount_WithdrawCommited(t *testing.T) {
 
 	err := commitments.DeductFromCommitted("lp/1", sdk.NewInt(9000), 3, false)
 	require.Error(t, err)
+}
+
+// Test_Commitments_IsEmpty tests the IsEmpty method of the Commitments type
+func Test_Commitments_IsEmpty(t *testing.T) {
+	commitments := types.Commitments{
+		Creator:         "",
+		CommittedTokens: []*types.CommittedTokens{},
+		VestingTokens:   []*types.VestingTokens{},
+	}
+
+	require.True(t, commitments.IsEmpty())
+
+	commitments.CommittedTokens = append(commitments.CommittedTokens, &types.CommittedTokens{
+		Denom:   "lp/1",
+		Amount:  sdk.NewInt(100),
+		Lockups: nil,
+	})
+
+	require.False(t, commitments.IsEmpty())
+
+	commitments.CommittedTokens = nil
+	commitments.Claimed = sdk.Coins{sdk.NewInt64Coin("lp/1", 100)}
+
+	require.False(t, commitments.IsEmpty())
+
+	commitments.Claimed = nil
+	commitments.VestingTokens = append(commitments.VestingTokens, &types.VestingTokens{
+		Denom:                "lp/1",
+		TotalAmount:          sdk.NewInt(100),
+		ClaimedAmount:        sdk.NewInt(0),
+		NumBlocks:            100,
+		StartBlock:           0,
+		VestStartedTimestamp: 0,
+	})
+
+	require.False(t, commitments.IsEmpty())
+}
+
+// Test_Commitments_GetCommittedAmountForDenom tests the GetCommittedAmountForDenom method of the Commitments type
+func Test_Commitments_GetCommittedAmountForDenom(t *testing.T) {
+	commitments := types.Commitments{
+		Creator:         "",
+		CommittedTokens: []*types.CommittedTokens{},
+		VestingTokens:   []*types.VestingTokens{},
+	}
+
+	commitments.CommittedTokens = append(commitments.CommittedTokens, &types.CommittedTokens{
+		Denom:   "lp/1",
+		Amount:  sdk.NewInt(100),
+		Lockups: nil,
+	})
+
+	commitments.CommittedTokens = append(commitments.CommittedTokens, &types.CommittedTokens{
+		Denom:   "lp/2",
+		Amount:  sdk.NewInt(200),
+		Lockups: nil,
+	})
+
+	require.Equal(t, commitments.GetCommittedAmountForDenom("lp/1"), sdk.NewInt(100))
+	require.Equal(t, commitments.GetCommittedAmountForDenom("lp/2"), sdk.NewInt(200))
+	require.Equal(t, commitments.GetCommittedAmountForDenom("lp/3"), sdk.NewInt(0))
+}
+
+// Test_Commitments_GetCommittedLockUpsForDenom tests the GetCommittedLockUpsForDenom method of the Commitments type
+func Test_Commitments_GetCommittedLockUpsForDenom(t *testing.T) {
+	commitments := types.Commitments{
+		Creator:         "",
+		CommittedTokens: []*types.CommittedTokens{},
+		VestingTokens:   []*types.VestingTokens{},
+	}
+
+	commitments.CommittedTokens = append(commitments.CommittedTokens, &types.CommittedTokens{
+		Denom:   "lp/1",
+		Amount:  sdk.NewInt(100),
+		Lockups: nil,
+	})
+
+	commitments.CommittedTokens = append(commitments.CommittedTokens, &types.CommittedTokens{
+		Denom:  "lp/2",
+		Amount: sdk.NewInt(200),
+		Lockups: []types.Lockup{
+			{
+				Amount:          sdk.NewInt(100),
+				UnlockTimestamp: 100,
+			},
+			{
+				Amount:          sdk.NewInt(200),
+				UnlockTimestamp: 200,
+			},
+		},
+	})
+
+	require.Nil(t, commitments.GetCommittedLockUpsForDenom("lp/1"))
+	require.NotNil(t, commitments.GetCommittedLockUpsForDenom("lp/2"))
+	require.Len(t, commitments.GetCommittedLockUpsForDenom("lp/2"), 2)
+}
+
+// Test_Commitments_GetCommittedLockUpsForDenomNil tests the GetCommittedLockUpsForDenom method of the Commitments type
+// when there are no lockups for the specified denom
+func Test_Commitments_GetCommittedLockUpsForDenomNil(t *testing.T) {
+	commitments := types.Commitments{
+		Creator:         "",
+		CommittedTokens: []*types.CommittedTokens{},
+		VestingTokens:   []*types.VestingTokens{},
+	}
+
+	commitments.CommittedTokens = append(commitments.CommittedTokens, &types.CommittedTokens{
+		Denom:   "lp/1",
+		Amount:  sdk.NewInt(100),
+		Lockups: nil,
+	})
+
+	commitments.CommittedTokens = append(commitments.CommittedTokens, &types.CommittedTokens{
+		Denom:   "lp/2",
+		Amount:  sdk.NewInt(200),
+		Lockups: nil,
+	})
+
+	require.Nil(t, commitments.GetCommittedLockUpsForDenom("lp/3"))
+}
+
+// Test_Commitments_CommittedTokensLocked tests the CommittedTokensLocked method of the Commitments type
+func Test_Commitments_CommittedTokensLocked(t *testing.T) {
+	ctx := sdk.NewContext(nil, tmproto.Header{}, false, nil).
+		WithBlockTime(time.Unix(0, 0))
+
+	commitments := types.Commitments{
+		Creator:         "",
+		CommittedTokens: []*types.CommittedTokens{},
+		VestingTokens:   []*types.VestingTokens{},
+	}
+
+	commitments.CommittedTokens = append(commitments.CommittedTokens, &types.CommittedTokens{
+		Denom:   "lp/1",
+		Amount:  sdk.NewInt(100),
+		Lockups: nil,
+	})
+
+	commitments.CommittedTokens = append(commitments.CommittedTokens, &types.CommittedTokens{
+		Denom:  "lp/2",
+		Amount: sdk.NewInt(200),
+		Lockups: []types.Lockup{
+			{
+				Amount:          sdk.NewInt(100),
+				UnlockTimestamp: 100,
+			},
+			{
+				Amount:          sdk.NewInt(200),
+				UnlockTimestamp: 200,
+			},
+		},
+	})
+
+	locked, unlocked := commitments.CommittedTokensLocked(ctx)
+	require.Equal(t, locked, sdk.Coins{sdk.NewInt64Coin("lp/2", 300)})
+	require.Equal(t, unlocked, sdk.Coins{sdk.NewInt64Coin("lp/1", 100), sdk.NewInt64Coin("lp/2", 200)})
 }
--- x/commitment/types/message_cancel_vest_test.go
+++ x/commitment/types/message_cancel_vest_test.go
@@ -18,56 +18,50 @@ func TestMsgCancelVest_ValidateBasic(t *testing.T) {
 	}{
 		{
 			name: "invalid address",
-			msg: MsgCancelVest{
-				Creator: "invalid_address",
-				Amount:  sdk.ZeroInt(),
-				Denom:   ptypes.Eden,
-			},
-			err: sdkerrors.ErrInvalidAddress,
+			msg:  *NewMsgCancelVest("invalid_address", sdk.ZeroInt(), ptypes.Eden),
+			err:  sdkerrors.ErrInvalidAddress,
 		},
 		{
 			name: "valid address",
-			msg: MsgCancelVest{
-				Creator: sample.AccAddress(),
-				Amount:  sdk.ZeroInt(),
-				Denom:   ptypes.Eden,
-			},
+			msg:  *NewMsgCancelVest(sample.AccAddress(), sdk.ZeroInt(), ptypes.Eden),
 		},
 		{
 			name: "invalid denom",
-			msg: MsgCancelVest{
-				Creator: sample.AccAddress(),
-				Amount:  sdk.ZeroInt(),
-				Denom:   "uusdc",
-			},
-			err: ErrInvalidDenom,
-		},
-		{
-			name: "invalid amount - nil",
-			msg: MsgCancelVest{
-				Creator: sample.AccAddress(),
-				Denom:   ptypes.Eden,
-			},
-			err: ErrInvalidAmount,
+			msg:  *NewMsgCancelVest(sample.AccAddress(), sdk.ZeroInt(), "invalid_denom"),
+			err:  ErrInvalidDenom,
 		},
 		{
 			name: "invalid amount - negative",
-			msg: MsgCancelVest{
-				Creator: sample.AccAddress(),
-				Denom:   ptypes.Eden,
-				Amount:  sdk.NewInt(-200),
-			},
-			err: ErrInvalidAmount,
+			msg:  *NewMsgCancelVest(sample.AccAddress(), sdk.NewInt(-200), ptypes.Eden),
+			err:  ErrInvalidAmount,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.msg.Route(), RouterKey)
+			require.Equal(t, tt.msg.Type(), TypeMsgCancelVest)
 			err := tt.msg.ValidateBasic()
 			if tt.err != nil {
 				require.ErrorIs(t, err, tt.err)
 				return
+			} else {
+				creator := sdk.MustAccAddressFromBech32(tt.msg.Creator)
+				require.Equal(t, tt.msg.GetSigners(), []sdk.AccAddress{creator})
 			}
 			require.NoError(t, err)
 		})
 	}
+}
+
+// GetSigners panics
+func TestMsgCancelVest_GetSigners(t *testing.T) {
+	msg := NewMsgCancelVest("invalid_address", sdk.ZeroInt(), ptypes.Eden)
+	require.Panics(t, func() { msg.GetSigners() })
+}
+
+// GetSignBytes
+func TestMsgCancelVest_GetSignBytes(t *testing.T) {
+	msg := NewMsgCancelVest(sample.AccAddress(), sdk.ZeroInt(), ptypes.Eden)
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	require.Equal(t, msg.GetSignBytes(), sdk.MustSortJSON(bz))
 }
