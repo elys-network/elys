6400eba0c51ce3c61b88d2c622b1b43503071e66
avkr003 <abhinav@elys.network>
2024-11-07 04:23:35 +0530

    fixing unit test cases

--- x/stablestake/keeper/keeper_test.go
+++ x/stablestake/keeper/keeper_test.go
@@ -1,6 +1,10 @@
 package keeper_test
 
 import (
+	"cosmossdk.io/math"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	atypes "github.com/elys-network/elys/x/assetprofile/types"
+	stablestaketypes "github.com/elys-network/elys/x/stablestake/types"
 	"testing"
 
 	"github.com/cosmos/cosmos-sdk/codec"
@@ -59,5 +63,48 @@ func TestKeeper_SetHooks_Panic(t *testing.T) {
 
 	assert.Panics(t, func() {
 		keeper.SetHooks(nil)
+	})
+}
+
+func (suite *KeeperTestSuite) SetStakingParam() error {
+	return suite.app.StakingKeeper.SetParams(suite.ctx, stakingtypes.Params{
+		UnbondingTime:     1209600,
+		MaxValidators:     60,
+		MaxEntries:        7,
+		HistoricalEntries: 10000,
+		BondDenom:         "uelys",
+		MinCommissionRate: math.LegacyNewDec(0),
+	})
+}
+
+func (suite *KeeperTestSuite) SetStableStakeParam() error {
+
+	params := stablestaketypes.DefaultParams()
+	suite.app.StablestakeKeeper.SetParams(suite.ctx, params)
+	return nil
+}
+
+func (suite *KeeperTestSuite) SetupAssetProfile() {
+
+	suite.app.AssetprofileKeeper.SetEntry(suite.ctx, atypes.Entry{
+		BaseDenom:                "uusdc",
+		Decimals:                 6,
+		Denom:                    "uusdc",
+		Path:                     "transfer/channel-12",
+		IbcChannelId:             "channel-12",
+		IbcCounterpartyChannelId: "channel-19",
+		DisplayName:              "USDC",
+		DisplaySymbol:            "uUSDC",
+		Network:                  "",
+		Address:                  "",
+		ExternalSymbol:           "uUSDC",
+		TransferLimit:            "",
+		Permissions:              []string{},
+		UnitDenom:                "uusdc",
+		IbcCounterpartyDenom:     "",
+		IbcCounterpartyChainId:   "",
+		Authority:                "elys10d07y265gmmuvt4z0w9aw880jnsr700j6z2zm3",
+		CommitEnabled:            true,
+		WithdrawEnabled:          true,
 	})
 }
