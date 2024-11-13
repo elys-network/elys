9971ecd327d8caa2c699bf7ed9b9b74534bd1c17
99adarsh <itsadarshkumar@gmail.com>
2024-11-13 18:42:53 +0530

    Add amm module v7 migration

---
+++ x/amm/migrations/v7_migration.go
@@ -0,0 +1,54 @@
+package migrations
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/elys-network/elys/x/amm/types"
+	"github.com/elys-network/elys/x/amm/utils"
+)
+
+func (m Migrator) V7Migration(ctx sdk.Context) error {
+	pools := m.keeper.GetAllPool(ctx)
+	for _, pool := range pools {
+		newPoolAddress := types.NewPoolAddress(pool.PoolId)
+		poolAccountModuleName := types.GetPoolIdModuleName(pool.PoolId)
+		if err := utils.CreateModuleAccount(ctx, m.keeper.GetAccountKeeper(), newPoolAddress, poolAccountModuleName); err != nil {
+			panic(fmt.Errorf("error creating new pool account for %d: %w", pool.PoolId, err))
+		}
+		poolAccAddress := sdk.MustAccAddressFromBech32(pool.Address)
+		// Bank: Transfer funds from prevPoolAddress to new newPoolAddress
+		prevPoolAddressBalances := m.keeper.GetBankKeeper().GetAllBalances(ctx, poolAccAddress)
+		m.keeper.GetBankKeeper().SendCoins(ctx, poolAccAddress, newPoolAddress, prevPoolAddressBalances)
+
+		// AssetProfile: Update authority in assetprofile entry
+		poolBaseDenom := types.GetPoolShareDenom(pool.PoolId)
+
+		entry, found := m.keeper.GetAssetProfileKeeper().GetEntry(ctx, poolBaseDenom)
+		// Should not happen
+		if !found {
+			panic(fmt.Errorf("assetprofile not found for basedenom: %s", poolBaseDenom))
+		}
+
+		entry.Authority = newPoolAddress.String()
+		m.keeper.GetAssetProfileKeeper().SetEntry(ctx, entry)
+
+		pool.Address = newPoolAddress.String()
+
+		m.keeper.SetPool(ctx, pool)
+
+		// Update the name and Symbol of pool share token metadata
+		metadata, found := m.keeper.GetBankKeeper().GetDenomMetaData(ctx, poolBaseDenom)
+		// Should not happen
+		if !found {
+			panic(fmt.Errorf("denom metadata for poolshare denom not found in bank denom metadata: %s", poolBaseDenom))
+		}
+		metadata.Name = metadata.Base
+		metadata.Symbol = metadata.Display
+
+		m.keeper.GetBankKeeper().SetDenomMetaData(ctx, metadata)
+	}
+	ctx.Logger().Info("Bank Module Migration Finished")
+
+	return nil
+}
