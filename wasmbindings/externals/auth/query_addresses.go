9d3122e7ce23845ccb50d6955e8a00f366fddb2f
brilliantkid87 <brilliantkid87@gmail.com>
2024-11-14 11:29:52 +0700

    refactor(auth): improve queryAddresses function robustness and performance

--- wasmbindings/externals/auth/query_addresses.go
+++ wasmbindings/externals/auth/query_addresses.go
@@ -2,25 +2,37 @@ package auth
 
 import (
 	"encoding/json"
-	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
+	"fmt"
 
 	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	wasmbindingstypes "github.com/elys-network/elys/wasmbindings/types"
 )
 
-func (oq *Querier) queryAddresses(ctx sdk.Context, req *authtypes.QueryAccountsRequest) ([]byte, error) {
-	queryServer := authkeeper.NewQueryServer(*oq.keeper)
-	res, err := queryServer.Accounts(sdk.WrapSDKContext(ctx), req)
-	if err != nil {
-		return nil, errorsmod.Wrap(err, "failed to get accounts")
+type QueryAddressesParams struct {
+	Context sdk.Context
+	Request *authtypes.QueryAccountsRequest
+}
+
+func (oq *Querier) queryAddresses(params QueryAddressesParams) ([]byte, error) {
+	if params.Request == nil {
+		return nil, fmt.Errorf("query request cannot be nil")
 	}
 
-	var addresses []string
+	queryServer := authkeeper.NewQueryServer(*oq.keeper)
+	res, err := queryServer.Accounts(sdk.WrapSDKContext(params.Context), params.Request)
+	if err != nil {
+		return nil, errorsmod.Wrapf(err, "failed to get accounts for request: %v", params.Request)
+	}
+
+	// Pre-allocate slice with capacity matching the number of accounts
+	addresses := make([]string, 0, len(res.Accounts))
+
 	for _, account := range res.Accounts {
-		iaccount := account.GetCachedValue().(authtypes.AccountI)
-		if iaccount == nil {
+		iaccount, ok := account.GetCachedValue().(authtypes.AccountI)
+		if !ok || iaccount == nil {
 			continue
 		}
 		addresses = append(addresses, iaccount.GetAddress().String())
@@ -33,7 +45,8 @@ func (oq *Querier) queryAddresses(ctx sdk.Context, req *authtypes.QueryAccountsR
 
 	responseBytes, err := json.Marshal(authAddressesResponse)
 	if err != nil {
-		return nil, errorsmod.Wrap(err, "failed to serialize addresses response")
+		return nil, errorsmod.Wrapf(err, "failed to serialize addresses response: %+v", authAddressesResponse)
 	}
+
 	return responseBytes, nil
 }
