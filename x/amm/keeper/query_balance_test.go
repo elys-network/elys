f899c02f4b99218004fdf03539d25a0d2dbb2b91
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-11-18 14:49:10 +0100

    fix: fix ibc hooks and add amm tests (#955)
    * fix: fix ibc hooks and add amm tests
    
    * ci: untombstone tombstoned validators due to v50 upgrade issues
    
    * ci: temporary solution to use v0.50 binary with patch 1
    
    * ci: build source
    
    * ci: bump snap gen version
    
    * fix: disabled ics4middleware to avoid dependency with wasm
    
    * ci: bump snap gen version
    
    * ci: bump snap gen version
    
    * ci: bump snap gen version
    
    * ci: bump snap gen version
    
    * ci: bump snap gen version
    
    * ci: bump snap gen version
    
    * ci: bump snap gen version
    
    * ci: bump snap gen version
    
    * ci: bump snap gen version
    
    * ci: bump snap gen version

---
+++ x/amm/keeper/query_balance_test.go
@@ -0,0 +1,41 @@
+package keeper_test
+
+import "github.com/elys-network/elys/x/amm/types"
+
+func (suite *AmmKeeperTestSuite) TestQueryBalance() {
+	testCases := []struct {
+		name                 string
+		prerequisiteFunction func()
+		postValidateFunction func()
+	}{
+		{
+			"nil request",
+			func() {
+				suite.ResetSuite()
+			},
+			func() {
+				_, err := suite.app.AmmKeeper.Balance(suite.ctx, nil)
+				suite.Require().Error(err)
+			},
+		},
+		{
+			"invalid address",
+			func() {
+				suite.ResetSuite()
+			},
+			func() {
+				_, err := suite.app.AmmKeeper.Balance(suite.ctx, &types.QueryBalanceRequest{
+					Address: "invalid_address",
+				})
+				suite.Require().Error(err)
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(tc.name, func() {
+			tc.prerequisiteFunction()
+			tc.postValidateFunction()
+		})
+	}
+}
