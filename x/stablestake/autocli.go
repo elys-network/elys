faec04e69ac868b03483f9258b17c0444a8dde01
avkr003 <abhinav@elys.network>
2024-11-20 15:32:25 +0530

    Migrate CLI query commands to autocli
    Replaced custom CLI query commands implementation with auto-generated CLI options using autocli. This includes support for Params and BorrowRatio queries, streamlining the codebase and reducing manual maintenance.

---
+++ x/stablestake/autocli.go
@@ -0,0 +1,51 @@
+package stablestake
+
+import (
+	autocliv1 "cosmossdk.io/api/cosmos/autocli/v1"
+	_ "cosmossdk.io/api/cosmos/crypto/secp256k1" // register to that it shows up in protoregistry.GlobalTypes
+	_ "cosmossdk.io/api/cosmos/crypto/secp256r1" // register to that it shows up in protoregistry.GlobalTypes
+	"github.com/elys-network/elys/api/elys/stablestake"
+)
+
+// AutoCLIOptions implements the autocli.HasAutoCLIConfig interface.
+func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
+	return &autocliv1.ModuleOptions{
+		Query: &autocliv1.ServiceCommandDescriptor{
+			Service: stablestake.Query_ServiceDesc.ServiceName,
+			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
+				{
+					RpcMethod: "Params",
+					Use:       "params",
+					Short:     "Query the current parameters",
+				},
+				{
+					RpcMethod: "BorrowRatio",
+					Use:       "borrow-ratio",
+					Short:     "Query the borrow ratio",
+				},
+			},
+		},
+		Tx: &autocliv1.ServiceCommandDescriptor{
+			Service:              stablestake.Msg_ServiceDesc.ServiceName,
+			EnhanceCustomCommand: false, // use custom commands only until cosmos sdk v0.51
+			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
+				{
+					RpcMethod:      "Bond",
+					Use:            "bond [amount]",
+					Short:          "Bond coins to the stablestake module",
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "amount"}},
+				},
+				{
+					RpcMethod:      "Unbond",
+					Use:            "unbond [amount]",
+					Short:          "Unbond coins from the stablestake module",
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "amount"}},
+				},
+				{
+					RpcMethod: "UpdateParams",
+					Skip:      true, // skipped because authority gated
+				},
+			},
+		},
+	}
+}
