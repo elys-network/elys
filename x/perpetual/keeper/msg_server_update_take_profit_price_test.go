5670dcf911b3e0b5764315a444d816cae2102767
99adarsh <itsadarshkumar@gmail.com>
2024-11-20 14:22:19 +0530

    add tests cases for update take profit

--- x/perpetual/keeper/msg_server_update_take_profit_price_test.go
+++ x/perpetual/keeper/msg_server_update_take_profit_price_test.go
@@ -1,15 +1,19 @@
 package keeper_test
 
 import (
+	"fmt"
+
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/elys-network/elys/testutil/sample"
+	oracletypes "github.com/elys-network/elys/x/oracle/types"
 	ptypes "github.com/elys-network/elys/x/parameter/types"
 	"github.com/elys-network/elys/x/perpetual/keeper"
 	"github.com/elys-network/elys/x/perpetual/types"
 )
 
 func (suite *PerpetualKeeperTestSuite) TestUpdateTakeProfitPrice() {
+	params := suite.app.PerpetualKeeper.GetParams(suite.ctx)
 
 	// Define test cases
 	testCases := []struct {
@@ -119,6 +123,121 @@ func (suite *PerpetualKeeperTestSuite) TestUpdateTakeProfitPrice() {
 				}
 			},
 			"",
+		},
+		{
+			"updated take profit price is less than current market price for long",
+			func() *types.MsgUpdateTakeProfitPrice {
+				suite.ResetSuite()
+				addr := suite.AddAccounts(1, nil)
+				positionCreator := addr[0]
+				_, _, ammPool := suite.SetPerpetualPool(1)
+				tradingAssetPrice, err := suite.app.PerpetualKeeper.GetAssetPrice(suite.ctx, ptypes.ATOM)
+				suite.Require().NoError(err)
+				openPositionMsg := &types.MsgOpen{
+					Creator:         positionCreator.String(),
+					Leverage:        math.LegacyNewDec(2),
+					Position:        types.Position_LONG,
+					PoolId:          ammPool.PoolId,
+					TradingAsset:    ptypes.ATOM,
+					Collateral:      sdk.NewCoin(ptypes.BaseCurrency, math.NewInt(1000)),
+					TakeProfitPrice: tradingAssetPrice.MulInt64(4),
+					StopLossPrice:   math.LegacyZeroDec(),
+				}
+
+				position, err := suite.app.PerpetualKeeper.Open(suite.ctx, openPositionMsg, false)
+				suite.Require().NoError(err)
+
+				suite.app.OracleKeeper.SetPrice(suite.ctx, oracletypes.Price{
+					Asset:     "ATOM",
+					Price:     math.LegacyNewDec(15),
+					Source:    "elys",
+					Provider:  oracleProvider.String(),
+					Timestamp: uint64(suite.ctx.BlockTime().Unix()),
+				})
+
+				return &types.MsgUpdateTakeProfitPrice{
+					Creator: positionCreator.String(),
+					Id:      position.Id,
+					Price:   math.LegacyNewDec(10),
+				}
+			},
+			fmt.Sprintf("take profit price should be between %s and %s times of current market price for long", params.MinimumLongTakeProfitPriceRatio.String(), params.MaximumLongTakeProfitPriceRatio.String()),
+		},
+		{
+			"updated take profit price is greater than Maximum take profit allowed for long",
+			func() *types.MsgUpdateTakeProfitPrice {
+				suite.ResetSuite()
+				addr := suite.AddAccounts(1, nil)
+				positionCreator := addr[0]
+				_, _, ammPool := suite.SetPerpetualPool(1)
+				tradingAssetPrice, err := suite.app.PerpetualKeeper.GetAssetPrice(suite.ctx, ptypes.ATOM)
+				suite.Require().NoError(err)
+				openPositionMsg := &types.MsgOpen{
+					Creator:         positionCreator.String(),
+					Leverage:        math.LegacyNewDec(2),
+					Position:        types.Position_LONG,
+					PoolId:          ammPool.PoolId,
+					TradingAsset:    ptypes.ATOM,
+					Collateral:      sdk.NewCoin(ptypes.BaseCurrency, math.NewInt(1000)),
+					TakeProfitPrice: tradingAssetPrice.MulInt64(4),
+					StopLossPrice:   math.LegacyZeroDec(),
+				}
+
+				position, err := suite.app.PerpetualKeeper.Open(suite.ctx, openPositionMsg, false)
+				suite.Require().NoError(err)
+
+				suite.app.OracleKeeper.SetPrice(suite.ctx, oracletypes.Price{
+					Asset:     "ATOM",
+					Price:     math.LegacyNewDec(15),
+					Source:    "elys",
+					Provider:  oracleProvider.String(),
+					Timestamp: uint64(suite.ctx.BlockTime().Unix()),
+				})
+
+				return &types.MsgUpdateTakeProfitPrice{
+					Creator: positionCreator.String(),
+					Id:      position.Id,
+					Price:   math.LegacyNewDec(200),
+				}
+			},
+			fmt.Sprintf("take profit price should be between %s and %s times of current market price for long", params.MinimumLongTakeProfitPriceRatio.String(), params.MaximumLongTakeProfitPriceRatio.String()),
+		},
+		{
+			"updated take profit price is greater than maximum allowed take profit price for short",
+			func() *types.MsgUpdateTakeProfitPrice {
+				suite.ResetSuite()
+
+				addr := suite.AddAccounts(1, nil)
+				positionCreator := addr[0]
+				_, _, ammPool := suite.SetPerpetualPool(1)
+				openPositionMsg := &types.MsgOpen{
+					Creator:         positionCreator.String(),
+					Leverage:        math.LegacyNewDec(5),
+					Position:        types.Position_SHORT,
+					PoolId:          ammPool.PoolId,
+					TradingAsset:    ptypes.ATOM,
+					Collateral:      sdk.NewCoin(ptypes.BaseCurrency, math.NewInt(1000)),
+					TakeProfitPrice: math.LegacyMustNewDecFromStr("2.9"),
+					StopLossPrice:   math.LegacyZeroDec(),
+				}
+				position, err := suite.app.PerpetualKeeper.Open(suite.ctx, openPositionMsg, false)
+				suite.Require().NoError(err)
+
+				suite.app.OracleKeeper.SetPrice(suite.ctx, oracletypes.Price{
+					Asset:     "ATOM",
+					Price:     math.LegacyNewDec(3),
+					Source:    "elys",
+					Provider:  oracleProvider.String(),
+					Timestamp: uint64(suite.ctx.BlockTime().Unix()),
+				})
+
+				return &types.MsgUpdateTakeProfitPrice{
+					Creator: positionCreator.String(),
+					Id:      position.Id,
+					Price:   math.LegacyMustNewDecFromStr("2.98"),
+				}
+			},
+			fmt.Sprintf("take profit price should be less than %s times of current market price for short", params.MaximumShortTakeProfitPriceRatio.String()),
 		},
 	}
 
