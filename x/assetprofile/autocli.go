001425f5cc448dc99a6fc1df1aaba07fa00c9e9b
Abhinav Kumar <57705190+avkr003@users.noreply.github.com>
2024-11-21 16:30:14 +0530

    adding autocli options for parameter module (#971)
    * adding autocli options for parameter module
    
    * adding autocli options for assetprofile
    
    * Migrate CLI query commands to autocli
    
    Replaced custom CLI query commands implementation with auto-generated CLI options using autocli. This includes support for Params and BorrowRatio queries, streamlining the codebase and reducing manual maintenance.
    
    * fixing pulsar issue
    
    ---------
    
    Co-authored-by: Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>

---
+++ x/assetprofile/autocli.go
@@ -0,0 +1,58 @@
+package assetprofile
+
+import (
+	autocliv1 "cosmossdk.io/api/cosmos/autocli/v1"
+	_ "cosmossdk.io/api/cosmos/crypto/secp256k1" // register to that it shows up in protoregistry.GlobalTypes
+	_ "cosmossdk.io/api/cosmos/crypto/secp256r1" // register to that it shows up in protoregistry.GlobalTypes
+	"github.com/elys-network/elys/api/elys/assetprofile"
+)
+
+// AutoCLIOptions implements the autocli.HasAutoCLIConfig interface.
+func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
+	return &autocliv1.ModuleOptions{
+		Query: &autocliv1.ServiceCommandDescriptor{
+			Service: assetprofile.Query_ServiceDesc.ServiceName,
+			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
+				{
+					RpcMethod: "Params",
+					Use:       "params",
+					Short:     "Query the current parameters",
+				},
+				{
+					RpcMethod:      "Entry",
+					Use:            "show-entry [base-denom]",
+					Short:          "Query asset profile entry by base denom",
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "base_denom"}},
+				},
+				{
+					RpcMethod:      "EntryByDenom",
+					Use:            "show-entry-by-denom [denom]",
+					Short:          "Query asset profile entry by denom",
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "denom"}},
+				},
+				{
+					RpcMethod: "EntryAll",
+					Use:       "list-entry",
+					Short:     "Query all entries",
+				},
+			},
+		},
+		Tx: &autocliv1.ServiceCommandDescriptor{
+			Service: assetprofile.Msg_ServiceDesc.ServiceName,
+			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
+				{
+					RpcMethod: "AddEntry",
+					Skip:      true, // skipped because authority gated
+				},
+				{
+					RpcMethod: "UpdateEntry",
+					Skip:      true, // skipped because authority gated
+				},
+				{
+					RpcMethod: "DeleteEntry",
+					Skip:      true, // skipped because authority gated
+				},
+			},
+		},
+	}
+}
