2e4385a7595f32d4640ff791ba9a96e0289775ca
Amit <amy29981@gmail.com>
2024-11-21 19:11:50 +0530

    add msg server tests

--- x/masterchef/keeper/msg_server_test.go
+++ x/masterchef/keeper/msg_server_test.go
@@ -1,36 +1,138 @@
 package keeper_test
 
 import (
-	"context"
-	"testing"
+	"fmt"
 
+	errorsmod "cosmossdk.io/errors"
 	sdkmath "cosmossdk.io/math"
-
-	keepertest "github.com/elys-network/elys/testutil/keeper"
-	"github.com/elys-network/elys/x/masterchef/keeper"
+	"github.com/cometbft/cometbft/crypto/ed25519"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	"github.com/elys-network/elys/x/masterchef/types"
-	"github.com/stretchr/testify/require"
 )
 
-func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
-	k, ctx := keepertest.MasterchefKeeper(t)
+func (suite *MasterchefKeeperTestSuite) TestMsgUpdateParams() {
 
-	k.SetPoolInfo(ctx, types.PoolInfo{
-		PoolId:               2,
-		RewardWallet:         "elys1d96rzrky937s3s397g5xh5qvcwgkeqysmh8sg2kn359fhfvzeyrsnalu2u",
-		Multiplier:           sdkmath.LegacyMustNewDecFromStr("1.00"),
-		GasApr:               sdkmath.LegacyMustNewDecFromStr("0.00"),
-		EdenApr:              sdkmath.LegacyMustNewDecFromStr("0.50"),
-		DexApr:               sdkmath.LegacyMustNewDecFromStr("0.00"),
-		ExternalIncentiveApr: sdkmath.LegacyMustNewDecFromStr("0.00"),
-		EnableEdenRewards:    false,
-	})
+	pk := ed25519.GenPrivKey().PubKey()
+	nonAuthority := sdk.AccAddress(pk.Address()).String()
+	authority := authtypes.NewModuleAddress(govtypes.ModuleName).String()
+	suite.AddPoolInfo()
 
-	return keeper.NewMsgServerImpl(*k), ctx
+	testCases := []struct {
+		name         string
+		msg          *types.MsgUpdateParams
+		expectErr    bool
+		expectErrMsg error
+	}{
+		{
+			name: "Invalid signer",
+			msg: &types.MsgUpdateParams{
+				Authority: "",
+				Params:    types.DefaultParams(),
+			},
+			expectErr:    true,
+			expectErrMsg: fmt.Errorf("invalid authority"),
+		},
+		{
+			name: "Invalid Signer Address",
+			msg: &types.MsgUpdateParams{
+				Authority: nonAuthority,
+				Params:    types.DefaultParams(),
+			},
+			expectErr:    true,
+			expectErrMsg: errorsmod.Wrapf(govtypes.ErrInvalidSigner, "invalid authority; expected %s, got %s", authority, nonAuthority),
+		},
+		{
+			name: "Happy Flow",
+			msg: &types.MsgUpdateParams{
+				Authority: authority,
+				Params:    types.DefaultParams(),
+			},
+			expectErr:    false,
+			expectErrMsg: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(tc.name, func() {
+			_, err := suite.msgServer.UpdateParams(suite.ctx, tc.msg)
+			if tc.expectErr {
+				suite.Require().Error(err)
+				suite.Require().Contains(err.Error(), tc.expectErrMsg.Error())
+			} else {
+				suite.Require().NoError(err)
+			}
+		})
+	}
 }
 
-func TestMsgServer(t *testing.T) {
-	ms, ctx := setupMsgServer(t)
-	require.NotNil(t, ms)
-	require.NotNil(t, ctx)
+func (suite *MasterchefKeeperTestSuite) TestMsgUpdatePoolMultipliers() {
+
+	pk := ed25519.GenPrivKey().PubKey()
+	nonAuthority := sdk.AccAddress(pk.Address()).String()
+	authority := authtypes.NewModuleAddress(govtypes.ModuleName).String()
+	suite.AddPoolInfo()
+
+	testCases := []struct {
+		name         string
+		msg          *types.MsgUpdatePoolMultipliers
+		expectErr    bool
+		expectErrMsg error
+	}{
+		{
+			name: "Invalid signer",
+			msg: &types.MsgUpdatePoolMultipliers{
+				Authority: "",
+				PoolMultipliers: []types.PoolMultiplier{
+					{
+						PoolId:     1,
+						Multiplier: sdkmath.LegacyOneDec(),
+					},
+				},
+			},
+			expectErr:    true,
+			expectErrMsg: fmt.Errorf("invalid authority"),
+		},
+		{
+			name: "Invalid Signer Address",
+			msg: &types.MsgUpdatePoolMultipliers{
+				Authority: nonAuthority,
+				PoolMultipliers: []types.PoolMultiplier{
+					{
+						PoolId:     1,
+						Multiplier: sdkmath.LegacyOneDec(),
+					},
+				},
+			},
+			expectErr:    true,
+			expectErrMsg: errorsmod.Wrapf(govtypes.ErrInvalidSigner, "invalid authority; expected %s, got %s", authority, nonAuthority),
+		},
+		{
+			name: "Happy Flow",
+			msg: &types.MsgUpdatePoolMultipliers{
+				Authority: authority,
+				PoolMultipliers: []types.PoolMultiplier{
+					{
+						PoolId:     1,
+						Multiplier: sdkmath.LegacyOneDec(),
+					},
+				},
+			},
+			expectErr:    false,
+			expectErrMsg: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(tc.name, func() {
+			_, err := suite.msgServer.UpdatePoolMultipliers(suite.ctx, tc.msg)
+			if tc.expectErr {
+				suite.Require().Error(err)
+				suite.Require().Contains(err.Error(), tc.expectErrMsg.Error())
+			} else {
+				suite.Require().NoError(err)
+			}
+		})
+	}
 }
