292ec3b1091dac62a0c71ffd8938c79ca65a55ad
99adarsh <itsadarshkumar@gmail.com>
2024-11-25 12:30:41 +0530

    add getParams mock in tradeshield and update msgServerTest

--- x/tradeshield/keeper/msg_server_test.go
+++ x/tradeshield/keeper/msg_server_test.go
@@ -2,20 +2,17 @@ package keeper_test
 
 import (
 	"context"
-	"testing"
 
-	keepertest "github.com/elys-network/elys/testutil/keeper"
 	"github.com/elys-network/elys/x/tradeshield/keeper"
 	"github.com/elys-network/elys/x/tradeshield/types"
 )
 
-func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
-	k, ctx, _, _, _, _ := keepertest.TradeshieldKeeper(t)
-	return keeper.NewMsgServerImpl(*k), ctx
+func (suite *TradeshieldKeeperTestSuite) setupMsgServer() (types.MsgServer, context.Context) {
+	return keeper.NewMsgServerImpl(suite.app.TradeshieldKeeper), suite.ctx
 }
 
 func (suite *TradeshieldKeeperTestSuite) TestMsgServer() {
-	ms, ctx := setupMsgServer(suite.T())
+	ms, ctx := suite.setupMsgServer()
 	suite.Require().NotNil(ms)
 	suite.Require().NotNil(ctx)
 }
