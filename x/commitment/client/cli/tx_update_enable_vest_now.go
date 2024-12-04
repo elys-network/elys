5c4fd59eee3115607a55ac205d871f6b8f8d91dc
Amit <amy29981@gmail.com>
2024-12-04 10:43:08 +0530

    add cli

---
+++ x/commitment/client/cli/tx_update_enable_vest_now.go
@@ -0,0 +1,101 @@
+package cli
+
+import (
+	"errors"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
+	"github.com/cosmos/cosmos-sdk/x/gov/client/cli"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	v1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
+	"github.com/elys-network/elys/x/commitment/types"
+	"github.com/spf13/cobra"
+)
+
+func CmdUpdateEnableVestNow() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "update-enable-vest-now",
+		Short: "Broadcast message update-enable-vest-now",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			title, err := cmd.Flags().GetString(cli.FlagTitle)
+			if err != nil {
+				return err
+			}
+
+			summary, err := cmd.Flags().GetString(cli.FlagSummary)
+			if err != nil {
+				return err
+			}
+
+			metadata, err := cmd.Flags().GetString(cli.FlagMetadata)
+			if err != nil {
+				return err
+			}
+
+			expedited, err := cmd.Flags().GetBool(FlagExpedited)
+			if err != nil {
+				return err
+			}
+
+			enableVestNow, err := cmd.Flags().GetBool(FlagEnableVestNow)
+			if err != nil {
+				return err
+			}
+
+			signer := clientCtx.GetFromAddress()
+			if signer == nil {
+				return errors.New("signer address is missing")
+			}
+
+			govAddress := sdk.AccAddress(address.Module(govtypes.ModuleName))
+			msg := types.NewMsgUpdateEnableVestNow(
+				govAddress.String(),
+				enableVestNow,
+			)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			depositStr, err := cmd.Flags().GetString(cli.FlagDeposit)
+			if err != nil {
+				return err
+			}
+
+			deposit, err := sdk.ParseCoinsNormalized(depositStr)
+			if err != nil {
+				return err
+			}
+
+			govMsg, err := v1.NewMsgSubmitProposal([]sdk.Msg{&msg}, deposit, signer.String(), metadata, title, summary, expedited)
+			if err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), govMsg)
+		},
+	}
+
+	cmd.Flags().Bool(FlagExpedited, false, "expedited")
+	cmd.Flags().Bool(FlagEnableVestNow, false, "enable-vest-now")
+	cmd.Flags().String(cli.FlagTitle, "", "title of proposal")
+	cmd.Flags().String(cli.FlagSummary, "", "summary of proposal")
+	cmd.Flags().String(cli.FlagMetadata, "", "metadata of proposal")
+	cmd.Flags().String(cli.FlagDeposit, "", "deposit of proposal")
+	_ = cmd.MarkFlagRequired(FlagEnableVestNow)
+	_ = cmd.MarkFlagRequired(cli.FlagTitle)
+	_ = cmd.MarkFlagRequired(cli.FlagSummary)
+	_ = cmd.MarkFlagRequired(cli.FlagMetadata)
+	_ = cmd.MarkFlagRequired(cli.FlagDeposit)
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
---
+++ x/commitment/keeper/msg_server_update_enable_vest_now.go
@@ -0,0 +1,25 @@
+package keeper
+
+import (
+	"context"
+
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	"github.com/elys-network/elys/x/commitment/types"
+)
+
+// UpdateEnableVestNow add/update specific vesting info by denom on Params
+func (k msgServer) UpdateEnableVestNow(goCtx context.Context, msg *types.MsgUpdateEnableVestNow) (*types.MsgUpdateEnableVestNowResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	if k.authority != msg.Authority {
+		return nil, errorsmod.Wrapf(govtypes.ErrInvalidSigner, "invalid authority; expected %s, got %s", k.authority, msg.Authority)
+	}
+
+	params := k.GetParams(ctx)
+	params.EnableVestNow = msg.EnableVestNow
+
+	k.SetParams(ctx, params)
+	return &types.MsgUpdateEnableVestNowResponse{}, nil
+}
