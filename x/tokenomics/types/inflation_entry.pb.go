c076fe20da838f7fdedcb00c2961af13da15b2fc
Amit <amy29981@gmail.com>
2024-12-08 19:41:59 +0530

    remove

--- x/tokenomics/types/inflation_entry.pb.go
+++ x/tokenomics/types/inflation_entry.pb.go
@@ -22,7 +22,7 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
-type InflationEntry struct {
+type LegacyInflationEntry struct {
 	LmRewards         uint64 `protobuf:"varint,1,opt,name=lm_rewards,json=lmRewards,proto3" json:"lm_rewards,omitempty"`
 	IcsStakingRewards uint64 `protobuf:"varint,2,opt,name=ics_staking_rewards,json=icsStakingRewards,proto3" json:"ics_staking_rewards,omitempty"`
 	CommunityFund     uint64 `protobuf:"varint,3,opt,name=community_fund,json=communityFund,proto3" json:"community_fund,omitempty"`
@@ -30,11 +30,84 @@ type InflationEntry struct {
 	TeamTokensVested  uint64 `protobuf:"varint,5,opt,name=team_tokens_vested,json=teamTokensVested,proto3" json:"team_tokens_vested,omitempty"`
 }
 
+func (m *LegacyInflationEntry) Reset()         { *m = LegacyInflationEntry{} }
+func (m *LegacyInflationEntry) String() string { return proto.CompactTextString(m) }
+func (*LegacyInflationEntry) ProtoMessage()    {}
+func (*LegacyInflationEntry) Descriptor() ([]byte, []int) {
+	return fileDescriptor_fbe674fab3da1fa3, []int{0}
+}
+func (m *LegacyInflationEntry) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *LegacyInflationEntry) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_LegacyInflationEntry.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *LegacyInflationEntry) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_LegacyInflationEntry.Merge(m, src)
+}
+func (m *LegacyInflationEntry) XXX_Size() int {
+	return m.Size()
+}
+func (m *LegacyInflationEntry) XXX_DiscardUnknown() {
+	xxx_messageInfo_LegacyInflationEntry.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_LegacyInflationEntry proto.InternalMessageInfo
+
+func (m *LegacyInflationEntry) GetLmRewards() uint64 {
+	if m != nil {
+		return m.LmRewards
+	}
+	return 0
+}
+
+func (m *LegacyInflationEntry) GetIcsStakingRewards() uint64 {
+	if m != nil {
+		return m.IcsStakingRewards
+	}
+	return 0
+}
+
+func (m *LegacyInflationEntry) GetCommunityFund() uint64 {
+	if m != nil {
+		return m.CommunityFund
+	}
+	return 0
+}
+
+func (m *LegacyInflationEntry) GetStrategicReserve() uint64 {
+	if m != nil {
+		return m.StrategicReserve
+	}
+	return 0
+}
+
+func (m *LegacyInflationEntry) GetTeamTokensVested() uint64 {
+	if m != nil {
+		return m.TeamTokensVested
+	}
+	return 0
+}
+
+type InflationEntry struct {
+	LmRewards         uint64 `protobuf:"varint,1,opt,name=lm_rewards,json=lmRewards,proto3" json:"lm_rewards,omitempty"`
+	IcsStakingRewards uint64 `protobuf:"varint,2,opt,name=ics_staking_rewards,json=icsStakingRewards,proto3" json:"ics_staking_rewards,omitempty"`
+}
+
 func (m *InflationEntry) Reset()         { *m = InflationEntry{} }
 func (m *InflationEntry) String() string { return proto.CompactTextString(m) }
 func (*InflationEntry) ProtoMessage()    {}
 func (*InflationEntry) Descriptor() ([]byte, []int) {
-	return fileDescriptor_fbe674fab3da1fa3, []int{0}
+	return fileDescriptor_fbe674fab3da1fa3, []int{1}
 }
 func (m *InflationEntry) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -77,28 +150,8 @@ func (m *InflationEntry) GetIcsStakingRewards() uint64 {
 	return 0
 }
 
-func (m *InflationEntry) GetCommunityFund() uint64 {
-	if m != nil {
-		return m.CommunityFund
-	}
-	return 0
-}
-
-func (m *InflationEntry) GetStrategicReserve() uint64 {
-	if m != nil {
-		return m.StrategicReserve
-	}
-	return 0
-}
-
-func (m *InflationEntry) GetTeamTokensVested() uint64 {
-	if m != nil {
-		return m.TeamTokensVested
-	}
-	return 0
-}
-
 func init() {
+	proto.RegisterType((*LegacyInflationEntry)(nil), "elys.tokenomics.LegacyInflationEntry")
 	proto.RegisterType((*InflationEntry)(nil), "elys.tokenomics.InflationEntry")
 }
 
@@ -107,28 +160,29 @@ func init() {
 }
 
 var fileDescriptor_fbe674fab3da1fa3 = []byte{
-	// 280 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x4c, 0xd0, 0x31, 0x4e, 0xc3, 0x30,
-	0x14, 0x06, 0xe0, 0x1a, 0x0a, 0x12, 0x96, 0x28, 0xd4, 0x2c, 0x59, 0xb0, 0x10, 0x52, 0x25, 0x24,
-	0x20, 0x19, 0xb8, 0x01, 0x12, 0x48, 0x5d, 0x0b, 0x62, 0x60, 0xb1, 0x52, 0xe7, 0xb5, 0x58, 0x89,
-	0xed, 0xca, 0x7e, 0x69, 0xc9, 0x2d, 0x38, 0x16, 0x63, 0x47, 0x46, 0x48, 0x2e, 0x82, 0xe2, 0xa8,
-	0x81, 0xf1, 0xfd, 0xff, 0xb7, 0xbc, 0x9f, 0x4e, 0xa0, 0xa8, 0x7c, 0x82, 0x36, 0x07, 0x63, 0xb5,
-	0x92, 0x3e, 0x51, 0x66, 0x51, 0xa4, 0xa8, 0xac, 0x11, 0x60, 0xd0, 0x55, 0xf1, 0xca, 0x59, 0xb4,
-	0xec, 0xa4, 0x65, 0xf1, 0x1f, 0xbb, 0xfc, 0x21, 0x74, 0x34, 0xdd, 0xd1, 0x87, 0x56, 0xb2, 0x73,
-	0x4a, 0x0b, 0x2d, 0x1c, 0x6c, 0x52, 0x97, 0xf9, 0x88, 0x5c, 0x90, 0xab, 0xe1, 0xec, 0xa8, 0xd0,
-	0xb3, 0x2e, 0x60, 0x31, 0x3d, 0x53, 0xd2, 0x0b, 0x8f, 0x69, 0xae, 0xcc, 0xb2, 0x77, 0x7b, 0xc1,
-	0x8d, 0x95, 0xf4, 0x4f, 0x5d, 0xb3, 0xf3, 0x13, 0x3a, 0x92, 0x56, 0xeb, 0xd2, 0x28, 0xac, 0xc4,
-	0xa2, 0x34, 0x59, 0xb4, 0x1f, 0xe8, 0x71, 0x9f, 0x3e, 0x96, 0x26, 0x63, 0xd7, 0x74, 0xec, 0xd1,
-	0xa5, 0x08, 0x4b, 0x25, 0x85, 0x03, 0x0f, 0x6e, 0x0d, 0xd1, 0x30, 0xc8, 0xd3, 0xbe, 0x98, 0x75,
-	0x39, 0xbb, 0xa1, 0x0c, 0x21, 0xd5, 0x22, 0x3c, 0xe2, 0xc5, 0x1a, 0x3c, 0x42, 0x16, 0x1d, 0x74,
-	0xba, 0x6d, 0x9e, 0x43, 0xf1, 0x12, 0xf2, 0xfb, 0xe9, 0x67, 0xcd, 0xc9, 0xb6, 0xe6, 0xe4, 0xbb,
-	0xe6, 0xe4, 0xa3, 0xe1, 0x83, 0x6d, 0xc3, 0x07, 0x5f, 0x0d, 0x1f, 0xbc, 0x26, 0x4b, 0x85, 0x6f,
-	0xe5, 0x3c, 0x96, 0x56, 0x27, 0xed, 0x32, 0xb7, 0x06, 0x70, 0x63, 0x5d, 0x1e, 0x8e, 0xe4, 0xfd,
-	0xff, 0x9e, 0x58, 0xad, 0xc0, 0xcf, 0x0f, 0xc3, 0x8c, 0x77, 0xbf, 0x01, 0x00, 0x00, 0xff, 0xff,
-	0x13, 0x73, 0x9c, 0x5b, 0x6f, 0x01, 0x00, 0x00,
+	// 296 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xb4, 0xd1, 0xb1, 0x4a, 0x03, 0x31,
+	0x18, 0x07, 0xf0, 0x9e, 0x56, 0xc1, 0x80, 0xd5, 0x46, 0x87, 0x5b, 0x0c, 0x52, 0x28, 0x08, 0xea,
+	0xdd, 0xe0, 0x1b, 0x08, 0x0a, 0x05, 0xa7, 0x2a, 0x0e, 0x2e, 0x21, 0xcd, 0x7d, 0x3d, 0x43, 0x2f,
+	0x49, 0x49, 0xbe, 0x6b, 0xbd, 0xb7, 0xf0, 0xb1, 0x1c, 0x3b, 0x3a, 0x4a, 0x3b, 0xf8, 0x1a, 0x72,
+	0x39, 0x7a, 0xfa, 0x02, 0x8e, 0xf9, 0xff, 0x7f, 0x19, 0x3e, 0xfe, 0x64, 0x08, 0x45, 0xe5, 0x53,
+	0xb4, 0x33, 0x30, 0x56, 0x2b, 0xe9, 0x53, 0x65, 0xa6, 0x85, 0x40, 0x65, 0x0d, 0x07, 0x83, 0xae,
+	0x4a, 0xe6, 0xce, 0xa2, 0xa5, 0x47, 0x35, 0x4b, 0x7e, 0xd9, 0xe0, 0x3b, 0x22, 0xa7, 0x0f, 0x90,
+	0x0b, 0x59, 0x8d, 0xb6, 0x1f, 0xee, 0x6a, 0x4f, 0xcf, 0x08, 0x29, 0x34, 0x77, 0xb0, 0x14, 0x2e,
+	0xf3, 0x71, 0x74, 0x1e, 0x5d, 0x74, 0xc7, 0x07, 0x85, 0x1e, 0x37, 0x01, 0x4d, 0xc8, 0x89, 0x92,
+	0x9e, 0x7b, 0x14, 0x33, 0x65, 0xf2, 0xd6, 0xed, 0x04, 0xd7, 0x57, 0xd2, 0x3f, 0x36, 0xcd, 0xd6,
+	0x0f, 0x49, 0x4f, 0x5a, 0xad, 0x4b, 0xa3, 0xb0, 0xe2, 0xd3, 0xd2, 0x64, 0xf1, 0x6e, 0xa0, 0x87,
+	0x6d, 0x7a, 0x5f, 0x9a, 0x8c, 0x5e, 0x92, 0xbe, 0x47, 0x27, 0x10, 0x72, 0x25, 0xb9, 0x03, 0x0f,
+	0x6e, 0x01, 0x71, 0x37, 0xc8, 0xe3, 0xb6, 0x18, 0x37, 0x39, 0xbd, 0x22, 0x14, 0x41, 0x68, 0x1e,
+	0xce, 0xf1, 0x7c, 0x01, 0x1e, 0x21, 0x8b, 0xf7, 0x1a, 0x5d, 0x37, 0x4f, 0xa1, 0x78, 0x0e, 0xf9,
+	0x80, 0x93, 0xde, 0xbf, 0x9e, 0x78, 0x3b, 0xfa, 0x58, 0xb3, 0x68, 0xb5, 0x66, 0xd1, 0xd7, 0x9a,
+	0x45, 0xef, 0x1b, 0xd6, 0x59, 0x6d, 0x58, 0xe7, 0x73, 0xc3, 0x3a, 0x2f, 0x69, 0xae, 0xf0, 0xb5,
+	0x9c, 0x24, 0xd2, 0xea, 0xb4, 0x1e, 0xe0, 0xda, 0x00, 0x2e, 0xad, 0x9b, 0x85, 0x47, 0xfa, 0xf6,
+	0x77, 0x36, 0xac, 0xe6, 0xe0, 0x27, 0xfb, 0x61, 0xad, 0x9b, 0x9f, 0x00, 0x00, 0x00, 0xff, 0xff,
+	0x31, 0x5a, 0xc9, 0x85, 0xd6, 0x01, 0x00, 0x00,
 }
 
-func (m *InflationEntry) Marshal() (dAtA []byte, err error) {
+func (m *LegacyInflationEntry) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
 	dAtA = make([]byte, size)
 	n, err := m.MarshalToSizedBuffer(dAtA[:size])
@@ -138,12 +192,12 @@ func (m *InflationEntry) Marshal() (dAtA []byte, err error) {
 	return dAtA[:n], nil
 }
 
-func (m *InflationEntry) MarshalTo(dAtA []byte) (int, error) {
+func (m *LegacyInflationEntry) MarshalTo(dAtA []byte) (int, error) {
 	size := m.Size()
 	return m.MarshalToSizedBuffer(dAtA[:size])
 }
 
-func (m *InflationEntry) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+func (m *LegacyInflationEntry) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	i := len(dAtA)
 	_ = i
 	var l int
@@ -176,6 +230,39 @@ func (m *InflationEntry) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
+func (m *InflationEntry) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *InflationEntry) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *InflationEntry) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.IcsStakingRewards != 0 {
+		i = encodeVarintInflationEntry(dAtA, i, uint64(m.IcsStakingRewards))
+		i--
+		dAtA[i] = 0x10
+	}
+	if m.LmRewards != 0 {
+		i = encodeVarintInflationEntry(dAtA, i, uint64(m.LmRewards))
+		i--
+		dAtA[i] = 0x8
+	}
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintInflationEntry(dAtA []byte, offset int, v uint64) int {
 	offset -= sovInflationEntry(v)
 	base := offset
@@ -187,7 +274,7 @@ func encodeVarintInflationEntry(dAtA []byte, offset int, v uint64) int {
 	dAtA[offset] = uint8(v)
 	return base
 }
-func (m *InflationEntry) Size() (n int) {
+func (m *LegacyInflationEntry) Size() (n int) {
 	if m == nil {
 		return 0
 	}
@@ -211,13 +298,28 @@ func (m *InflationEntry) Size() (n int) {
 	return n
 }
 
+func (m *InflationEntry) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if m.LmRewards != 0 {
+		n += 1 + sovInflationEntry(uint64(m.LmRewards))
+	}
+	if m.IcsStakingRewards != 0 {
+		n += 1 + sovInflationEntry(uint64(m.IcsStakingRewards))
+	}
+	return n
+}
+
 func sovInflationEntry(x uint64) (n int) {
 	return (math_bits.Len64(x|1) + 6) / 7
 }
 func sozInflationEntry(x uint64) (n int) {
 	return sovInflationEntry(uint64((x << 1) ^ uint64((int64(x) >> 63))))
 }
-func (m *InflationEntry) Unmarshal(dAtA []byte) error {
+func (m *LegacyInflationEntry) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
 	iNdEx := 0
 	for iNdEx < l {
@@ -240,10 +342,10 @@ func (m *InflationEntry) Unmarshal(dAtA []byte) error {
 		fieldNum := int32(wire >> 3)
 		wireType := int(wire & 0x7)
 		if wireType == 4 {
-			return fmt.Errorf("proto: InflationEntry: wiretype end group for non-group")
+			return fmt.Errorf("proto: LegacyInflationEntry: wiretype end group for non-group")
 		}
 		if fieldNum <= 0 {
-			return fmt.Errorf("proto: InflationEntry: illegal tag %d (wire type %d)", fieldNum, wire)
+			return fmt.Errorf("proto: LegacyInflationEntry: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		case 1:
@@ -337,6 +439,94 @@ func (m *InflationEntry) Unmarshal(dAtA []byte) error {
 				b := dAtA[iNdEx]
 				iNdEx++
 				m.TeamTokensVested |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		default:
+			iNdEx = preIndex
+			skippy, err := skipInflationEntry(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthInflationEntry
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *InflationEntry) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowInflationEntry
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: InflationEntry: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: InflationEntry: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field LmRewards", wireType)
+			}
+			m.LmRewards = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowInflationEntry
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.LmRewards |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		case 2:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field IcsStakingRewards", wireType)
+			}
+			m.IcsStakingRewards = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowInflationEntry
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.IcsStakingRewards |= uint64(b&0x7F) << shift
 				if b < 0x80 {
 					break
 				}
