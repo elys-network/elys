9ac7258cad52da9cff6cb43da93f8a4a0a556f66
Amit Yadav <amy29981@gmail.com>
2024-12-10 09:37:20 +0530

    restore

--- x/commitment/keeper/commitments.go
+++ x/commitment/keeper/commitments.go
@@ -166,13 +166,6 @@ func (k Keeper) BurnEdenBoost(ctx sdk.Context, creator sdk.AccAddress, denom str
 	amount = amount.Sub(claimedRemovalAmount)
 	if amount.IsZero() {
 		k.SetCommitments(ctx, commitments)
-
-		if k.hooks != nil {
-			err = k.hooks.CommitmentChanged(ctx, creator, sdk.Coins{sdk.NewCoin(denom, amount)})
-			if err != nil {
-				return err
-			}
-		}
 		return nil
 	}
 
