7351ba6351e5e057981748f5e8799d745c1ab6da
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-12-17 14:52:42 +0100

    fix(oracle): increase lifetime in blocks value to avoid price expiration during chain setup

--- app/app.go
+++ app/app.go
@@ -343,6 +343,15 @@ func (app *ElysApp) PreBlocker(ctx sdk.Context, _ *abci.RequestFinalizeBlock) (*
 
 // BeginBlocker application updates every begin block
 func (app *ElysApp) BeginBlocker(ctx sdk.Context) (sdk.BeginBlock, error) {
+	if ctx.BlockHeight() == 115786 || ctx.BlockHeight() == 115787 {
+		params := app.OracleKeeper.GetParams(ctx)
+
+		// increase life time in blocks while we are setting up pools and will be decreased with gov proposal later on
+		params.LifeTimeInBlocks = 150
+
+		app.OracleKeeper.SetParams(ctx, params)
+	}
+
 	return app.mm.BeginBlock(ctx)
 }
 
