7098041fbda3269fa809351eb83341c5cf196868
Amit <amy29981@gmail.com>
2024-12-27 17:55:54 +0530

    add comment

--- x/tradeshield/migrations/v7_migration.go
+++ x/tradeshield/migrations/v7_migration.go
@@ -16,6 +16,10 @@ func (m Migrator) V7Migration(ctx sdk.Context) error {
 			}
 		}
 	} else { // Mainnet
+		// This is in reference to https://elys.atlassian.net/jira/software/projects/DEV/boards/2?assignee=712020%3A1d0833ef-4e27-4457-98ae-bd3075185694&selectedIssue=DEV-2322
+		// Buy orders were not fulfilled due to a issue in arguments passing, although that should have thrown error but
+		// error handling was not done so funds were stuck in the pool. This task was completed in PR #1081, so these orders needs to be
+		// removed
 		ordersIds := []uint64{164, 177, 180}
 		for _, orderId := range ordersIds {
 			m.keeper.RemovePendingSpotOrder(ctx, orderId)
