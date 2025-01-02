f003e4f708e5f11c480b304342a9865cafdc4728
99adarsh <itsadarshkumar@gmail.com>
2025-01-02 18:53:03 +0530

    add maxLeverage field in perpetual pool
    use pool's leverageMax for proxy leverage calculation

--- x/perpetual/keeper/get_net_open_interest_test.go
+++ x/perpetual/keeper/get_net_open_interest_test.go
@@ -19,7 +19,7 @@ func (suite *PerpetualKeeperTestSuite) TestGetFundingPaymentRates() {
 			},
 		},
 	}
-	pool := types.NewPool(ammPool)
+	pool := types.NewPool(ammPool, math.LegacyMustNewDecFromStr("5.5"))
 	pool.PoolAssetsLong = []types.PoolAsset{
 		{
 			Liabilities: math.NewInt(0),
