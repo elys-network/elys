22db09710a4158dfcfccc176ac3d197be1299b1e
avkr003 <abhinav@elys.network>
2025-01-27 12:50:20 +0530

    adding test cases

--- x/stablestake/keeper/amm_pool_test.go
+++ x/stablestake/keeper/amm_pool_test.go
@@ -1,12 +1,31 @@
 package keeper_test
 
-//func TestSetAmmPool(t *testing.T) {
-//	p := types.AmmPool{
-//		Id:               1,
-//		TotalLiabilities: sdk.Coins{sdk.NewInt64Coin(sdk.DefaultBondDenom, 1000)},
-//	}
-//
-//	p.SubLiabilities(sdk.NewInt64Coin(sdk.DefaultBondDenom, 10))
-//	require.Equal(t, p.TotalLiabilities.AmountOf(sdk.DefaultBondDenom), math.NewInt(990))
-//
-//}
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/elys-network/elys/x/stablestake/types"
+)
+
+func (suite *KeeperTestSuite) TestSetAmmPool() {
+	p := types.AmmPool{
+		Id:               1,
+		TotalLiabilities: sdk.Coins{sdk.NewInt64Coin(sdk.DefaultBondDenom, 1000)},
+	}
+
+	suite.app.StablestakeKeeper.SetAmmPool(suite.ctx, p)
+	r := suite.app.StablestakeKeeper.GetAmmPool(suite.ctx, 1)
+	suite.Assert().Equal(p, r)
+
+	r = suite.app.StablestakeKeeper.GetAmmPool(suite.ctx, 2)
+	suite.Assert().Equal(types.AmmPool{
+		Id:               2,
+		TotalLiabilities: sdk.Coins{},
+	}, r)
+
+	all := suite.app.StablestakeKeeper.GetAllAmmPools(suite.ctx)
+	suite.Assert().Equal([]types.AmmPool{p}, all)
+
+	suite.app.StablestakeKeeper.AddPoolLiabilities(suite.ctx, p.Id, sdk.NewInt64Coin(sdk.DefaultBondDenom, 10))
+	r = suite.app.StablestakeKeeper.GetAmmPool(suite.ctx, 1)
+	p.AddLiabilities(sdk.NewInt64Coin(sdk.DefaultBondDenom, 10))
+	suite.Assert().Equal(p, r)
+}
