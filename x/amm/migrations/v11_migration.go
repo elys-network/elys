4595b2a26707b548e355e4895a4fc2a9755a8ad6
avkr003 <abhinav@elys.network>
2025-02-05 14:06:14 +0530

    making exit lockup time as module parameter

--- x/amm/migrations/v11_migration.go
+++ x/amm/migrations/v11_migration.go
@@ -6,7 +6,7 @@ import (
 
 func (m Migrator) V11Migration(ctx sdk.Context) error {
 	legacyParams := m.keeper.GetLegacyParams(ctx)
-	legacyParams.LpLockupDuration = uint64(3600)
+	legacyParams.LpLockupDuration = 3600
 
 	m.keeper.SetParams(ctx, legacyParams)
 	return nil
