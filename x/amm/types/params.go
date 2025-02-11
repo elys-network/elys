ed2a891af7b8711e60fc1fd533a11027d7386a6d
Amit Yadav <amy29981@gmail.com>
2025-02-11 16:13:52 +0530

    add validations

--- x/amm/types/params.go
+++ x/amm/types/params.go
@@ -56,12 +56,18 @@ func (p Params) Validate() error {
 	if p.WeightBreakingFeeExponent.IsNegative() {
 		return errors.New("weightBreakingFeeExponent must be positive")
 	}
+	if p.WeightBreakingFeeExponent.GT(math.LegacyMustNewDecFromStr("3")) {
+		return errors.New("weightBreakingFeeExponent must be less than 3")
+	}
 
 	if p.WeightBreakingFeeMultiplier.IsNil() {
 		return errors.New("weightBreakingFeeMultiplier must not be empty")
 	}
 	if p.WeightBreakingFeeMultiplier.IsNegative() {
 		return errors.New("weightBreakingFeeMultiplier must be positive")
+	}
+	if p.WeightBreakingFeeMultiplier.GT(math.LegacyMustNewDecFromStr("0.0001")) {
+		return errors.New("weightBreakingFeeMultiplier must be less than 0.01%")
 	}
 
 	if p.WeightBreakingFeePortion.IsNil() {
@@ -70,12 +76,18 @@ func (p Params) Validate() error {
 	if p.WeightBreakingFeePortion.IsNegative() {
 		return errors.New("weightBreakingFeePortion must be positive")
 	}
+	if p.WeightBreakingFeePortion.GT(math.LegacyMustNewDecFromStr("1")) {
+		return errors.New("weightBreakingFeePortion must be less than 1")
+	}
 
 	if p.WeightRecoveryFeePortion.IsNil() {
 		return errors.New("weightRecoveryFeePortion must not be empty")
 	}
 	if p.WeightRecoveryFeePortion.IsNegative() {
 		return errors.New("weightRecoveryFeePortion must be positive")
+	}
+	if p.WeightRecoveryFeePortion.GT(math.LegacyMustNewDecFromStr("1")) {
+		return errors.New("weightRecoveryFeePortion must be less than 1")
 	}
 
 	if p.ThresholdWeightDifference.IsNil() {
@@ -84,6 +96,20 @@ func (p Params) Validate() error {
 	if p.ThresholdWeightDifference.IsNegative() {
 		return errors.New("thresholdWeightDifference must be positive")
 	}
+	if p.ThresholdWeightDifference.GT(math.LegacyMustNewDecFromStr("0.1")) {
+		return errors.New("thresholdWeightDifference must be less than 0.1%")
+	}
+
+	if p.ThresholdWeightDifferenceSwapFee.IsNil() {
+		return errors.New("thresholdWeightDifferenceSwapFee must not be empty")
+	}
+	if p.ThresholdWeightDifferenceSwapFee.IsNegative() {
+		return errors.New("thresholdWeightDifferenceSwapFee must be positive")
+	}
+	if p.ThresholdWeightDifferenceSwapFee.GT(math.LegacyMustNewDecFromStr("0.15")) {
+		return errors.New("thresholdWeightDifferenceSwapFee must be less than 0.15%")
+	}
+
 	return nil
 }
 
