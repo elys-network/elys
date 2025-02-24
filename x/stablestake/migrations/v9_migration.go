76c255e16419c599f259c7b4b76cabf9c1139183
Amit Yadav <amy29981@gmail.com>
2025-02-24 22:59:00 +0530

    fix cli (#1172)
    * fix cli
    
    * add migration
    
    * fix

--- x/stablestake/migrations/v9_migration.go
+++ x/stablestake/migrations/v9_migration.go
@@ -2,10 +2,28 @@ package migrations
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/elys-network/elys/x/stablestake/types"
 )
 
 func (m Migrator) V9Migration(ctx sdk.Context) error {
 	m.keeper.MoveAllDebt(ctx)
 	m.keeper.MoveAllInterest(ctx)
+
+	params := m.keeper.GetParams(ctx)
+	pool := types.Pool{
+		Id:                   types.UsdcPoolId,
+		DepositDenom:         params.LegacyDepositDenom,
+		InterestRateDecrease: params.LegacyInterestRateDecrease,
+		InterestRateIncrease: params.LegacyInterestRateIncrease,
+		HealthGainFactor:     params.LegacyHealthGainFactor,
+		MaxLeverageRatio:     params.LegacyMaxLeverageRatio,
+		MaxWithdrawRatio:     params.LegacyMaxWithdrawRatio,
+		InterestRateMax:      params.LegacyInterestRateMax,
+		InterestRateMin:      params.LegacyInterestRateMin,
+		InterestRate:         params.LegacyInterestRate,
+		TotalValue:           params.TotalValue,
+	}
+
+	m.keeper.SetPool(ctx, pool)
 	return nil
 }
