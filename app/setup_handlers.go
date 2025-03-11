aae17348af64c7b09d17b7084a04bf9b908a37d1
avkr003 <abhinav@elys.network>
2025-03-11 11:18:46 +0530

    fixing upgrade version

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -5,6 +5,7 @@ import (
 	storetypes "cosmossdk.io/store/types"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	"fmt"
+	"strings"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
@@ -22,31 +23,30 @@ var NextVersion = "vNEXT"
 
 // generate upgrade version from the current version (v999999.999999.999999 => v999999)
 func generateUpgradeVersion() string {
-	//currentVersion := version.Version
-	//// if current version empty then override it with localnet version
-	//if currentVersion == "v" {
-	//	currentVersion = "v999999.999999.999999"
-	//}
-	//parts := strings.Split(currentVersion, ".")
-	//// Needed for devnet
-	//if len(parts) == 1 {
-	//	return currentVersion
-	//}
-	//if len(parts) != 3 {
-	//	panic(fmt.Sprintf("Invalid version format: %s. Expected format: vX.Y.Z", currentVersion))
-	//}
-	//majorVersion := strings.TrimPrefix(parts[0], "v")
-	//// required for testnet
-	//patchParts := strings.Split(parts[2], "-")
-	//rcVersion := ""
-	//if len(patchParts) > 1 {
-	//	rcVersion = strings.Join(patchParts[1:], "-")
-	//}
-	//if rcVersion != "" {
-	//	return fmt.Sprintf("v%s-%s", majorVersion, rcVersion)
-	//}
-	//return fmt.Sprintf("v%s", majorVersion)
-	return "v2.2.0"
+	currentVersion := version.Version
+	// if current version empty then override it with localnet version
+	if currentVersion == "v" {
+		currentVersion = "v999999.999999.999999"
+	}
+	parts := strings.Split(currentVersion, ".")
+	// Needed for devnet
+	if len(parts) == 1 {
+		return currentVersion
+	}
+	if len(parts) != 3 {
+		panic(fmt.Sprintf("Invalid version format: %s. Expected format: vX.Y.Z", currentVersion))
+	}
+	majorVersion := strings.TrimPrefix(parts[0], "v")
+	// required for testnet
+	patchParts := strings.Split(parts[2], "-")
+	rcVersion := ""
+	if len(patchParts) > 1 {
+		rcVersion = strings.Join(patchParts[1:], "-")
+	}
+	if rcVersion != "" {
+		return fmt.Sprintf("v%s-%s", majorVersion, rcVersion)
+	}
+	return fmt.Sprintf("v%s", majorVersion)
 }
 
 func (app *ElysApp) setUpgradeHandler() {
