c6b181c8dbd6d0d409c5e034cc1ab04935ca8164
99adarsh <itsadarshkumar@gmail.com>
2025-03-12 13:09:14 +0530

    add wasmbinding for wasm module

---
+++ x/amm/client/wasm/query_out_route_by_denom.go
@@ -0,0 +1,22 @@
+package wasm
+
+import (
+	"encoding/json"
+
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	ammtypes "github.com/elys-network/elys/x/amm/types"
+)
+
+func (oq *Querier) queryOutRouteByDenom(ctx sdk.Context, query *ammtypes.QueryOutRouteByDenomRequest) ([]byte, error) {
+	res, err := oq.keeper.OutRouteByDenom(ctx, query)
+	if err != nil {
+		return nil, errorsmod.Wrap(err, "failed to get out route by denom")
+	}
+
+	responseBytes, err := json.Marshal(res)
+	if err != nil {
+		return nil, errorsmod.Wrap(err, "failed to serialize out route by denom response")
+	}
+	return responseBytes, nil
+}
