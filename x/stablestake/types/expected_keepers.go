cfbac56ce611b8d0086781c56e7750c2f6018d0b
avkr003 <abhinav@elys.network>
2025-03-26 20:45:40 +0530

    removing ojo

--- x/stablestake/types/expected_keepers.go
+++ x/stablestake/types/expected_keepers.go
@@ -7,7 +7,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	ammtypes "github.com/elys-network/elys/x/amm/types"
 	assetprofiletypes "github.com/elys-network/elys/x/assetprofile/types"
-	oracletypes "github.com/ojo-network/ojo/x/oracle/types"
+	oracletypes "github.com/elys-network/elys/x/oracle/types"
 )
 
 type AmmKeeper interface {
