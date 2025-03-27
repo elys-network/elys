18349772e74d72abdeec288cd24f0df94d91ca4f
avkr003 <abhinav@elys.network>
2025-03-27 21:23:59 +0530

    fixing migration

--- x/commitment/keeper/total_supply.go
+++ x/commitment/keeper/total_supply.go
@@ -6,6 +6,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/runtime"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/elys-network/elys/x/commitment/types"
+	ptypes "github.com/elys-network/elys/x/parameter/types"
 )
 
 func (k Keeper) GetTotalSupply(ctx sdk.Context) (val types.TotalSupply) {
@@ -28,7 +29,38 @@ func (k Keeper) SetTotalSupply(ctx sdk.Context, val types.TotalSupply) {
 	store.Set(types.TotalSupplyKeyPrefix, b)
 }
 
-func (k Keeper) V11_SetVested(ctx sdk.Context) {
+func (k Keeper) V10_SetEdenEdenBSupply(ctx sdk.Context) {
+	store := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
+	iterator := storetypes.KVStorePrefixIterator(store, types.CommitmentsKeyPrefix)
+
+	defer iterator.Close()
+
+	totalEden := math.ZeroInt()
+	totalEdenB := math.ZeroInt()
+
+	for ; iterator.Valid(); iterator.Next() {
+		var val types.Commitments
+		k.cdc.MustUnmarshal(iterator.Value(), &val)
+		for _, token := range val.CommittedTokens {
+			if token.Denom == ptypes.Eden {
+				totalEden = totalEden.Add(token.Amount)
+			}
+			if token.Denom == ptypes.EdenB {
+				totalEdenB = totalEdenB.Add(token.Amount)
+			}
+		}
+
+		totalEden = totalEden.Add(val.Claimed.AmountOf(ptypes.Eden))
+		totalEdenB = totalEdenB.Add(val.Claimed.AmountOf(ptypes.EdenB))
+	}
+
+	k.SetTotalSupply(ctx, types.TotalSupply{
+		TotalEdenSupply:  totalEden,
+		TotalEdenbSupply: totalEdenB,
+	})
+}
+
+func (k Keeper) V10_SetEdenVested(ctx sdk.Context) {
 	store := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
 	iterator := storetypes.KVStorePrefixIterator(store, types.CommitmentsKeyPrefix)
 
--- x/commitment/migrations/v10_migration.go
+++ x/commitment/migrations/v10_migration.go
@@ -5,6 +5,7 @@ import (
 )
 
 func (m Migrator) V10Migration(ctx sdk.Context) error {
-	//m.keeper.V10_SetSupply(ctx)
+	m.keeper.V10_SetEdenEdenBSupply(ctx)
+	m.keeper.V10_SetEdenVested(ctx)
 	return nil
 }
