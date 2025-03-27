0d56ea5bf33fd4ca0db043324ddc315de6d9585d
Amit Yadav <amy29981@gmail.com>
2025-03-27 21:14:15 +0530

    Leveragelp fixes (#1214)
    * leveragelp fixs
    
    * set order

--- x/stablestake/migrations/v9_migration.go
+++ x/stablestake/migrations/v9_migration.go
@@ -6,6 +6,12 @@ import (
 )
 
 func (m Migrator) V9Migration(ctx sdk.Context) error {
+	allPools := m.keeper.GetAllAmmPools(ctx)
+	for _, pool := range allPools {
+		pool.TotalLiabilities = sdk.Coins{}
+		m.keeper.SetAmmPool(ctx, pool)
+	}
+
 	m.keeper.MoveAllDebt(ctx)
 	m.keeper.MoveAllInterest(ctx)
 
