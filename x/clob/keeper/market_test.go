7ab5ddf84357ddebee3659a6732fa447d5171b40
avkr003 <abhinav@elys.network>
2025-04-03 12:39:36 +0530

    adding market tests

--- x/clob/keeper/market_test.go
+++ x/clob/keeper/market_test.go
@@ -43,3 +43,126 @@ func (suite *KeeperTestSuite) TestGetPerpetualMarket() {
 		})
 	}
 }
+
+func (suite *KeeperTestSuite) TestGetAllPerpetualMarkets() {
+
+	market1 := types.PerpetualMarket{
+		Id: 1,
+	}
+	market2 := types.PerpetualMarket{
+		Id: 2,
+	}
+	testCases := []struct {
+		name  string
+		ids   []uint64
+		setup func()
+	}{
+		{
+			"empty markets",
+			[]uint64{},
+			func() {
+
+			},
+		},
+		{
+			"2 markets",
+			[]uint64{1, 2},
+			func() {
+				suite.app.ClobKeeper.SetPerpetualMarket(suite.ctx, market1)
+				suite.app.ClobKeeper.SetPerpetualMarket(suite.ctx, market2)
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(tc.name, func() {
+			tc.setup()
+			res := suite.app.ClobKeeper.GetAllPerpetualMarket(suite.ctx)
+			suite.Equal(len(res), len(tc.ids))
+		})
+	}
+}
+
+func (suite *KeeperTestSuite) TestCountAllPerpetualMarkets() {
+
+	market1 := types.PerpetualMarket{
+		Id: 1,
+	}
+	market2 := types.PerpetualMarket{
+		Id: 2,
+	}
+	testCases := []struct {
+		name   string
+		result uint64
+		setup  func()
+	}{
+		{
+			"empty markets",
+			0,
+			func() {
+
+			},
+		},
+		{
+			"2 markets",
+			2,
+			func() {
+				suite.app.ClobKeeper.SetPerpetualMarket(suite.ctx, market1)
+				suite.app.ClobKeeper.SetPerpetualMarket(suite.ctx, market2)
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(tc.name, func() {
+			tc.setup()
+			res := suite.app.ClobKeeper.CountAllPerpetualMarket(suite.ctx)
+			suite.Equal(res, tc.result)
+		})
+	}
+}
+
+func (suite *KeeperTestSuite) TestCheckPerpetualMarketAlreadyExists() {
+
+	baseDenom := "uatom"
+	quoteDenom := "uusdc"
+
+	market1 := types.PerpetualMarket{
+		Id:         1,
+		BaseDenom:  baseDenom,
+		QuoteDenom: quoteDenom,
+	}
+	market2 := types.PerpetualMarket{
+		Id:         2,
+		BaseDenom:  baseDenom,
+		QuoteDenom: quoteDenom,
+	}
+	testCases := []struct {
+		name   string
+		result bool
+		setup  func()
+	}{
+		{
+			"does not exist",
+			false,
+			func() {
+				suite.app.ClobKeeper.SetPerpetualMarket(suite.ctx, market1)
+			},
+		},
+		{
+			"exists",
+			true,
+			func() {
+				suite.app.ClobKeeper.SetPerpetualMarket(suite.ctx, market2)
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(tc.name, func() {
+			tc.setup()
+			res := suite.app.ClobKeeper.CheckPerpetualMarketAlreadyExists(suite.ctx, baseDenom, quoteDenom)
+			suite.Equal(res, tc.result)
+		})
+	}
+}
