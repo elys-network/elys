24424cb4f484ad7cf678deac7fd5d6ecc36aa8d2
Adarsh Kumar <itsadarshkumar@gmail.com>
2025-04-10 00:26:44 +0530

    Add more checks in Validate basic for Swap Messages (#1232)
    * route checks
    
    * fix validate basic check for swap exact amount in msg
    
    * fix validate basic check for swap exact amount out msg
    
    * fix validate basic check for upfront swap exact amount in msg
    
    * add test cases for validate basic swap messages
    
    * fix validate checks
    
    * add more checks
    
    ---------
    
    Co-authored-by: Amit <amy29981@gmail.com>

--- x/amm/types/message_swap_exact_amount_out_test.go
+++ x/amm/types/message_swap_exact_amount_out_test.go
@@ -79,7 +79,49 @@ func TestMsgSwapExactAmountOut_ValidateBasic(t *testing.T) {
 				TokenInMaxAmount: math.NewInt(1),
 				Recipient:        sample.AccAddress(),
 			},
-			err: errors.New("token in is zero"),
+			err: errors.New("token out is zero"),
+		},
+		{
+			name: "Invalid routes with same Input and Output denom",
+			msg: types.MsgSwapExactAmountOut{
+				Sender: sample.AccAddress(),
+				Routes: []types.SwapAmountOutRoute{
+					{TokenInDenom: "uusdc"},
+					{TokenInDenom: "uusdc"},
+					{TokenInDenom: "uelys"},
+				},
+				TokenOut:         sdk.Coin{Denom: ptypes.ATOM, Amount: math.NewInt(10)},
+				TokenInMaxAmount: math.NewInt(1),
+				Recipient:        sample.AccAddress(),
+			},
+			err: errors.New("has the same input and output denom as the previous route"),
+		},
+		{
+			name: "Invalid TokenInMaxAmount",
+			msg: types.MsgSwapExactAmountOut{
+				Sender: sample.AccAddress(),
+				Routes: []types.SwapAmountOutRoute{
+					{TokenInDenom: "uusdc"},
+					{TokenInDenom: "uelys"},
+				},
+				TokenOut:         sdk.Coin{Denom: ptypes.ATOM, Amount: math.NewInt(10)},
+				TokenInMaxAmount: math.NewInt(0),
+				Recipient:        sample.AccAddress(),
+			},
+			err: errors.New("TokenInMaxAmount must be positive"),
+		},
+		{
+			name: "Valid multiple routes",
+			msg: types.MsgSwapExactAmountOut{
+				Sender: sample.AccAddress(),
+				Routes: []types.SwapAmountOutRoute{
+					{TokenInDenom: "uusdc"},
+					{TokenInDenom: "uelys"},
+				},
+				TokenOut:         sdk.Coin{Denom: ptypes.ATOM, Amount: math.NewInt(10)},
+				TokenInMaxAmount: math.NewInt(1),
+				Recipient:        sample.AccAddress(),
+			},
 		},
 	}
 	for _, tt := range tests {
