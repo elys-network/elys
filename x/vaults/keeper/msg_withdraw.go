1ce980e3e43f6ac53503e6c540946f33d7511cb9
Amit Yadav <amy29981@gmail.com>
2025-05-29 17:02:47 +0530

    withdraw

--- x/vaults/keeper/msg_withdraw.go
+++ x/vaults/keeper/msg_withdraw.go
@@ -14,7 +14,13 @@ func (k msgServer) Withdraw(goCtx context.Context, req *types.MsgWithdraw) (*typ
 	shareDenom := types.GetShareDenomForVault(req.VaultId)
 	shareCoin := sdk.NewCoin(shareDenom, req.Shares)
 	shareCoins := sdk.NewCoins(shareCoin)
-	err := k.bk.SendCoinsFromAccountToModule(ctx, creator, types.ModuleName, shareCoins)
+
+	err := k.commitement.UncommitTokens(ctx, creator, shareDenom, req.Shares, false)
+	if err != nil {
+		return nil, err
+	}
+
+	err = k.bk.SendCoinsFromAccountToModule(ctx, creator, types.ModuleName, shareCoins)
 	if err != nil {
 		return nil, err
 	}
@@ -28,7 +34,13 @@ func (k msgServer) Withdraw(goCtx context.Context, req *types.MsgWithdraw) (*typ
 	if !found {
 		return nil, types.ErrVaultNotFound
 	}
-	// TODO: commited tokens
+	commitments := k.commitement.GetCommitments(ctx, creator)
+
+	for _, commitment := range commitments.CommittedTokens {
+		amount := commitment.Amount.ToLegacyDec().Mul(shareRatio).RoundInt()
+		toSendCoins = toSendCoins.Add(sdk.NewCoin(commitment.Denom, amount))
+	}
+
 	for _, coin := range vault.AllowedCoins {
 		balance := k.bk.GetBalance(ctx, vaultAddress, coin)
 		amount := balance.Amount.ToLegacyDec().Mul(shareRatio).RoundInt()
@@ -45,97 +57,7 @@ func (k msgServer) Withdraw(goCtx context.Context, req *types.MsgWithdraw) (*typ
 		return nil, err
 	}
 
-	// Commit tokens or not ?
-
 	return &types.MsgWithdrawResponse{}, nil
 }
 
 // TODO: Add withdraw fee for amount received
-
-// Withdraw handles the withdrawal of funds from a vault
-// func (k msgServer) Withdraw(goCtx context.Context, msg *types.MsgWithdraw) (*types.MsgWithdrawResponse, error) {
-// 	ctx := sdk.UnwrapSDKContext(goCtx)
-// 	// Deduct performance fee if applicable
-// 	k.DeductPerformanceFee(ctx)
-
-// 	// Get the vault
-// 	vault, found := k.GetVault(ctx, msg.VaultId)
-// 	if !found {
-// 		return nil, types.ErrVaultNotFound
-// 	}
-
-// 	// Get user's share balance
-// 	shareDenom := types.GetShareDenomForVault(vault.Id)
-// 	userShareBalance := k.bk.GetBalance(ctx, sdk.MustAccAddressFromBech32(msg.Withdrawer), shareDenom)
-// 	if userShareBalance.Amount.LT(msg.Shares) {
-// 		return nil, types.ErrInsufficientShares
-// 	}
-
-// 	// Calculate user's share ratio
-// 	totalShares := k.bk.GetSupply(ctx, shareDenom).Amount
-// 	shareRatio := sdkmath.LegacyNewDecFromInt(userShareBalance.Amount).Quo(sdkmath.LegacyNewDecFromInt(totalShares))
-
-// 	// Burn user's shares
-// 	err := k.bk.BurnCoins(ctx, types.ModuleName, sdk.NewCoins(sdk.NewCoin(shareDenom, msg.Shares)))
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	balancesBefore := k.bk.GetAllBalances(ctx, types.NewVaultAddress(vault.Id))
-
-// 	// Execute withdrawal strategy if it exists
-// 	if vault.WithdrawStrategy != nil {
-// 		for _, action := range vault.WithdrawStrategy {
-// 			// Scale action amounts based on user's share ratio
-// 			scaledAction := scaleActionAmounts(action, shareRatio)
-
-// 			// Create perform action message
-// 			performActionMsg := &types.MsgPerformAction{
-// 				Creator: vault.Manager,
-// 				Action:  scaledAction,
-// 				VaultId: msg.VaultId,
-// 			}
-
-// 			// Execute the action
-// 			_, err := k.PerformAction(goCtx, performActionMsg)
-// 			if err != nil {
-// 				return nil, fmt.Errorf("failed to execute withdrawal strategy action: %w", err)
-// 			}
-// 		}
-// 	}
-
-// 	balancesAfter := k.bk.GetAllBalances(ctx, types.NewVaultAddress(vault.Id))
-// 	amountsToSend := balancesAfter.Sub(balancesBefore...)
-
-// 	// Send coins to user
-// 	err = k.bk.SendCoinsFromModuleToAccount(ctx, types.ModuleName, sdk.MustAccAddressFromBech32(msg.Withdrawer), amountsToSend)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return &types.MsgWithdrawResponse{
-// 		VaultId: msg.VaultId,
-// 		Amount:  amountsToSend,
-// 	}, nil
-// }
-
-// // scaleActionAmounts scales the amounts in an action based on the share ratio
-// func scaleActionAmounts(action *types.Action, shareRatio sdkmath.LegacyDec) *types.Action {
-// 	scaledAction := *action // Create a copy of the action
-
-// 	switch perform_action := action.Action.(type) {
-// 	case *types.Action_ExitPool:
-// 		scaledAction.Action = &types.Action_ExitPool{
-// 			ExitPool: &ammtypes.MsgExitPool{
-// 				Sender:        perform_action.ExitPool.Sender,
-// 				PoolId:        perform_action.ExitPool.PoolId,
-// 				MinAmountsOut: perform_action.ExitPool.MinAmountsOut,
-// 				ShareAmountIn: perform_action.ExitPool.ShareAmountIn.ToLegacyDec().Mul(shareRatio).TruncateInt(),
-// 			},
-// 		}
-// 	}
-
-// 	// TODO: What if multiple exit from a lp shares
-
-// 	return &scaledAction
-// }
--- x/vaults/keeper/unwind.go
+++ x/vaults/keeper/unwind.go
@@ -1,17 +1,55 @@
 package keeper
 
 import (
+	"strconv"
+	"strings"
+
+	errorsmod "cosmossdk.io/errors"
+	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	ammtypes "github.com/elys-network/elys/v5/x/amm/types"
+	"github.com/elys-network/elys/v5/x/vaults/types"
 )
 
-func (k Keeper) UnwindCoin(ctx sdk.Context, coin sdk.Coin) error {
+func (k Keeper) UnwindCoin(ctx sdk.Context, coin sdk.Coin, targetDenom string) error {
 	// Case 1: coin is a LP share
-	// if strings.HasPrefix(coin.Denom, "lp-") {
-	// 	// exit pool
-	// 	_, _, _, _, _, err := k.amm.ExitPool(ctx, vaultAddress, perform_action.ExitPool.PoolId, perform_action.ExitPool.ShareAmountIn, perform_action.ExitPool.MinAmountsOut, perform_action.ExitPool.TokenOutDenom, false, true)
-	// 	if err != nil {
-	// 		return errorsmod.Wrapf(types.ErrInvalidAction, "action failed with error: %s", err)
-	// 	}
-	// }
+	if strings.HasPrefix(coin.Denom, "amm/pool/") {
+		poolId, err := GetPoolIdFromShareDenom(coin.Denom)
+		if err != nil {
+			return errorsmod.Wrapf(types.ErrInvalidAction, "action failed with error: %s", err)
+		}
+		vaultAddress := types.NewVaultAddress(poolId)
+
+		// exit pool
+		exitCoins, _, _, _, _, err := k.amm.ExitPool(ctx, vaultAddress, poolId, coin.Amount, sdk.Coins{}, coin.Denom, false, true)
+		if err != nil {
+			return errorsmod.Wrapf(types.ErrInvalidAction, "action failed with error: %s", err)
+		}
+
+		// Swap these exit coins into targetDenom
+		for _, exitCoin := range exitCoins {
+			_, err := k.amm.SwapByDenom(ctx, &ammtypes.MsgSwapByDenom{
+				Sender:    vaultAddress.String(),
+				Recipient: vaultAddress.String(),
+				DenomIn:   exitCoin.Denom,
+				DenomOut:  targetDenom,
+				Amount:    exitCoin,
+				MinAmount: sdk.NewCoin(targetDenom, sdkmath.NewInt(0)),
+				MaxAmount: sdk.NewCoin(targetDenom, sdkmath.NewInt(0)), // TODO: check here, swap order
+			})
+			if err != nil {
+				return errorsmod.Wrapf(types.ErrInvalidAction, "action failed with error: %s", err)
+			}
+		}
+
+	}
 	return nil
+}
+
+func GetPoolIdFromShareDenom(shareDenom string) (uint64, error) {
+	poolId, err := strconv.Atoi(strings.TrimPrefix(shareDenom, "amm/pool/"))
+	if err != nil {
+		return 0, err
+	}
+	return uint64(poolId), nil
 }
