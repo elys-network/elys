f38f9b1894406386b3e9e1effc3a9d51b999557e
Amit Yadav <amy29981@gmail.com>
2025-05-29 09:33:53 +0530

    comment

--- x/vaults/keeper/unwind.go
+++ x/vaults/keeper/unwind.go
@@ -1,21 +1,17 @@
 package keeper
 
 import (
-	"strings"
-
-	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/elys-network/elys/x/vaults/types"
 )
 
 func (k Keeper) UnwindCoin(ctx sdk.Context, coin sdk.Coin) error {
 	// Case 1: coin is a LP share
-	if strings.HasPrefix(coin.Denom, "lp-") {
-		// exit pool
-		_, _, _, _, _, err := k.amm.ExitPool(ctx, vaultAddress, perform_action.ExitPool.PoolId, perform_action.ExitPool.ShareAmountIn, perform_action.ExitPool.MinAmountsOut, perform_action.ExitPool.TokenOutDenom, false, true)
-		if err != nil {
-			return errorsmod.Wrapf(types.ErrInvalidAction, "action failed with error: %s", err)
-		}
-	}
+	// if strings.HasPrefix(coin.Denom, "lp-") {
+	// 	// exit pool
+	// 	_, _, _, _, _, err := k.amm.ExitPool(ctx, vaultAddress, perform_action.ExitPool.PoolId, perform_action.ExitPool.ShareAmountIn, perform_action.ExitPool.MinAmountsOut, perform_action.ExitPool.TokenOutDenom, false, true)
+	// 	if err != nil {
+	// 		return errorsmod.Wrapf(types.ErrInvalidAction, "action failed with error: %s", err)
+	// 	}
+	// }
 	return nil
 }
