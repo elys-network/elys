a47dee456f42649307c03e12004674d5edca7364
Amit Yadav <amy29981@gmail.com>
2025-05-30 16:15:19 +0530

    add query tests

--- x/vaults/keeper/unwind.go
+++ x/vaults/keeper/unwind.go
@@ -5,9 +5,7 @@ import (
 	"strings"
 
 	errorsmod "cosmossdk.io/errors"
-	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	ammtypes "github.com/elys-network/elys/v5/x/amm/types"
 	"github.com/elys-network/elys/v5/x/vaults/types"
 )
 
@@ -21,26 +19,12 @@ func (k Keeper) UnwindCoin(ctx sdk.Context, coin sdk.Coin, targetDenom string) e
 		vaultAddress := types.NewVaultAddress(poolId)
 
 		// exit pool
-		exitCoins, _, _, _, _, err := k.amm.ExitPool(ctx, vaultAddress, poolId, coin.Amount, sdk.Coins{}, coin.Denom, false, true)
+		_, _, _, _, _, err = k.amm.ExitPool(ctx, vaultAddress, poolId, coin.Amount, sdk.Coins{}, coin.Denom, false, true)
 		if err != nil {
 			return errorsmod.Wrapf(types.ErrInvalidAction, "action failed with error: %s", err)
 		}
 
-		// Swap these exit coins into targetDenom
-		for _, exitCoin := range exitCoins {
-			_, err := k.amm.SwapByDenom(ctx, &ammtypes.MsgSwapByDenom{
-				Sender:    vaultAddress.String(),
-				Recipient: vaultAddress.String(),
-				DenomIn:   exitCoin.Denom,
-				DenomOut:  targetDenom,
-				Amount:    exitCoin,
-				MinAmount: sdk.NewCoin(targetDenom, sdkmath.NewInt(0)),
-				MaxAmount: sdk.NewCoin(targetDenom, sdkmath.NewInt(0)), // TODO: check here, swap order
-			})
-			if err != nil {
-				return errorsmod.Wrapf(types.ErrInvalidAction, "action failed with error: %s", err)
-			}
-		}
+		//now swap required here, only required for fee conversion
 
 	}
 	return nil
