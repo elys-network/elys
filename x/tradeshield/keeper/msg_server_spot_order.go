dcab66678b6a028378cbed1b7c5460c5738cd5b4
Adarsh Kumar <itsadarshkumar@gmail.com>
2025-06-02 10:56:29 +0530

    Remove duplicate event during spot order market buy (#1314)
    remove duplicate event during spot order market buy

--- x/tradeshield/keeper/msg_server_spot_order.go
+++ x/tradeshield/keeper/msg_server_spot_order.go
@@ -25,12 +25,10 @@ func (k msgServer) CreateSpotOrder(goCtx context.Context, msg *types.MsgCreateSp
 
 	// if the order is market buy, execute it immediately
 	if msg.OrderType == types.SpotOrderType_MARKETBUY {
-		res, err := k.ExecuteMarketBuyOrder(ctx, pendingSpotOrder)
+		_, err := k.ExecuteMarketBuyOrder(ctx, pendingSpotOrder)
 		if err != nil {
 			return nil, err
 		}
-
-		ctx.EventManager().EmitEvent(types.NewExecuteMarketBuySpotOrderEvt(pendingSpotOrder, res))
 
 		return &types.MsgCreateSpotOrderResponse{
 			OrderId: pendingSpotOrder.OrderId,
