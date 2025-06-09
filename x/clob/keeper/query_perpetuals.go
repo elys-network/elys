ad5671f6f1500780ec5596e4e2af34412f5e157d
99adarsh <itsadarshkumar@gmail.com>
2025-06-09 18:33:18 +0530

    impl all perpetuals with liquidation price query

--- x/clob/keeper/query_perpetuals.go
+++ x/clob/keeper/query_perpetuals.go
@@ -2,8 +2,9 @@ package keeper
 
 import (
 	"context"
-	"cosmossdk.io/store/prefix"
 	"fmt"
+
+	"cosmossdk.io/store/prefix"
 	"github.com/cosmos/cosmos-sdk/runtime"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/query"
@@ -53,4 +54,62 @@ func (k Keeper) OwnerPerpetuals(goCtx context.Context, req *types.OwnerPerpetual
 	fmt.Println(list)
 
 	return &types.OwnerPerpetualsResponse{List: list, Pagination: pageRes}, nil
+}
+
+func (k Keeper) AllPerpetualsWithLiquidationPrice(goCtx context.Context, req *types.AllPerpetualsWithLiquidationPriceRequest) (*types.AllPerpetualsWithLiquidationPriceResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	if req.Pagination != nil && req.Pagination.Limit > types.MaxPageLimit {
+		return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("page size greater than max %d", types.MaxPageLimit))
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	var perpInfoList []types.PerpetualInfo
+
+	marketCache := make(map[uint64]types.PerpetualMarket)
+
+	prefixStore := prefix.NewStore(runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx)), types.PerpetualPrefix)
+
+	pageRes, err := query.Paginate(prefixStore, req.Pagination, func(key []byte, value []byte) error {
+		var perpetual types.Perpetual
+		if err := k.cdc.Unmarshal(value, &perpetual); err != nil {
+			return err
+		}
+
+		subAccount, err := k.GetSubAccount(ctx, perpetual.GetOwnerAccAddress(), perpetual.SubAccountId)
+		if err != nil {
+			return err
+		}
+
+		market, exists := marketCache[perpetual.MarketId]
+		if !exists {
+			market, err = k.GetPerpetualMarket(ctx, perpetual.MarketId)
+			if err != nil {
+				return err
+			}
+			marketCache[perpetual.MarketId] = market
+		}
+
+		liquidationPrice, err := k.GetLiquidationPrice(ctx, perpetual, market, subAccount)
+		if err != nil {
+			return err
+		}
+
+		perpInfo := types.PerpetualInfo{
+			Perpetual:        &perpetual,
+			LiquidationPrice: liquidationPrice,
+		}
+		perpInfoList = append(perpInfoList, perpInfo)
+		return nil
+	})
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.AllPerpetualsWithLiquidationPriceResponse{
+		PerpetualInfos: perpInfoList,
+		Pagination:     pageRes,
+	}, nil
 }
