040c6a12f205ef24627ab43707ced74ffabbbe6f
avkr003 <abhinav@elys.network>
2025-06-11 20:46:43 +0530

    updating perpetual PoolMaxLiabilitiesThreshold

--- x/perpetual/migrations/v19_migration.go
+++ x/perpetual/migrations/v19_migration.go
@@ -1,11 +1,21 @@
 package migrations
 
 import (
+	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (m Migrator) V19Migration(ctx sdk.Context) error {
-	err := m.keeper.ResetStore(ctx)
+
+	params := m.keeper.GetParams(ctx)
+	params.PoolMaxLiabilitiesThreshold = math.LegacyMustNewDecFromStr("0.3")
+
+	err := m.keeper.SetParams(ctx, &params)
+	if err != nil {
+		return err
+	}
+
+	err = m.keeper.ResetStore(ctx)
 	if err != nil {
 		return err
 	}
