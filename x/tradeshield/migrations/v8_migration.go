dd6cb44f64bb0824f25736475df45340a31ec104
avkr003 <abhinav@elys.network>
2025-06-18 13:59:08 +0530

    code review comments

--- x/tradeshield/migrations/v8_migration.go
+++ x/tradeshield/migrations/v8_migration.go
@@ -5,8 +5,5 @@ import (
 )
 
 func (m Migrator) V8Migration(ctx sdk.Context) error {
-	//m.keeper.SetAllLegacySpotOrderPriceToNewOrderPriceStructure(ctx)
-	//m.keeper.SetAllLegacyPerpetualTriggerPriceToNewTriggerPriceStructure(ctx)
-
 	return nil
 }
