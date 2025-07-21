198196d2d45a272eee47911a00103232de0a5829
avkr003 <abhinav@elys.network>
2025-07-22 04:43:07 +0530

    fixing unit test case

--- x/leveragelp/keeper/adl_test.go
+++ x/leveragelp/keeper/adl_test.go
@@ -2,7 +2,6 @@ package keeper_test
 
 import (
 	sdkmath "cosmossdk.io/math"
-	"errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
@@ -170,95 +169,13 @@ func (suite *KeeperTestSuite) TestClosePositionsOnADL() {
 	leveragePool, found = suite.app.LeveragelpKeeper.GetPool(suite.ctx, 1)
 	suite.Require().True(found)
 
-	ammPool, err = suite.app.LeveragelpKeeper.GetAmmPool(suite.ctx, 1)
-	suite.Require().NoError(err)
-	currentLeverageRatio = leveragePool.LeveragedLpAmount.ToLegacyDec().Quo(ammPool.TotalShares.Amount.ToLegacyDec())
-
-	err = suite.app.LeveragelpKeeper.ClosePositionsOnADL(suite.ctx, leveragePool, currentLeverageRatio)
+	err = suite.app.LeveragelpKeeper.ClosePositionsOnADL(suite.ctx, leveragePool)
 	suite.Require().NoError(err)
 
 	leveragePool, found = suite.app.LeveragelpKeeper.GetPool(suite.ctx, 1)
 	suite.Require().True(found)
 
-	ammPool, err = suite.app.LeveragelpKeeper.GetAmmPool(suite.ctx, 1)
+	err = suite.app.LeveragelpKeeper.ClosePositionsOnADL(suite.ctx, leveragePool)
 	suite.Require().NoError(err)
-	currentLeverageRatio = leveragePool.LeveragedLpAmount.ToLegacyDec().Quo(ammPool.TotalShares.Amount.ToLegacyDec())
-
-	err = suite.app.LeveragelpKeeper.ClosePositionsOnADL(suite.ctx, leveragePool, currentLeverageRatio)
-	suite.Require().NoError(err)
-
-}
-
-func (suite *KeeperTestSuite) TestClosePositionsOnADL_NegativeClosingRatio() {
-	suite.ResetSuite()
-	suite.SetupCoinPrices(suite.ctx)
-	addresses := simapp.AddTestAddrs(suite.app, suite.ctx, 5, sdkmath.NewInt(1000000))
-	asset1 := ptypes.ATOM
-	asset2 := ptypes.BaseCurrency
-	initializeForADL(suite, addresses, asset1, asset2)
-
-	params := suite.app.LeveragelpKeeper.GetParams(suite.ctx)
-	params.NumberPerBlock = 2
-	err := suite.app.LeveragelpKeeper.SetParams(suite.ctx, &params)
-	suite.Require().NoError(err)
-
-	leverage := sdkmath.LegacyMustNewDecFromStr("2")
-	collateralAmount := sdkmath.NewInt(5_000_000_000)
-
-	leveragePool, found := suite.app.LeveragelpKeeper.GetPool(suite.ctx, 1)
-	suite.Require().True(found)
-
-	ammPool, err := suite.app.LeveragelpKeeper.GetAmmPool(suite.ctx, 1)
-	suite.Require().NoError(err)
-	currentLeverageRatio := leveragePool.LeveragedLpAmount.ToLegacyDec().Quo(ammPool.TotalShares.Amount.ToLegacyDec())
-
-	for _, address := range addresses {
-		leveragePool, found = suite.app.LeveragelpKeeper.GetPool(suite.ctx, 1)
-		suite.Require().True(found)
-
-		ammPool, err = suite.app.LeveragelpKeeper.GetAmmPool(suite.ctx, 1)
-		suite.Require().NoError(err)
-		currentLeverageRatio = leveragePool.LeveragedLpAmount.ToLegacyDec().Quo(ammPool.TotalShares.Amount.ToLegacyDec())
-
-		if currentLeverageRatio.GT(leveragePool.MaxLeveragelpRatio) {
-			break
-		}
-		msg := types.MsgOpen{
-			Creator:          address.String(),
-			CollateralAsset:  ptypes.BaseCurrency,
-			CollateralAmount: collateralAmount,
-			AmmPoolId:        1,
-			Leverage:         leverage,
-			StopLossPrice:    sdkmath.LegacyMustNewDecFromStr("50.0"),
-		}
-		cacheCtx, write := suite.ctx.CacheContext()
-		_, err = suite.app.LeveragelpKeeper.Open(cacheCtx, &msg)
-		if err == nil {
-			write()
-		}
-	}
-
-	suite.ctx = suite.ctx.WithBlockHeight(suite.ctx.BlockHeight() + 10000).WithBlockTime(suite.ctx.BlockTime().Add(2 * time.Hour))
-
-	exitPool := ammtypes.MsgExitPool{
-		Sender:        addresses[0].String(),
-		PoolId:        1,
-		MinAmountsOut: sdk.NewCoins(sdk.NewInt64Coin(ptypes.ATOM, 1), sdk.NewInt64Coin(ptypes.BaseCurrency, 1)),
-		ShareAmountIn: sdkmath.LegacyMustNewDecFromStr("1000000000000000000000").TruncateInt(),
-		TokenOutDenom: "",
-	}
-
-	_, _, _, _, _, err = suite.app.AmmKeeper.ExitPool(suite.ctx, addresses[0], 1, exitPool.ShareAmountIn, exitPool.MinAmountsOut, exitPool.TokenOutDenom, false, false)
-	suite.Require().NoError(err)
-
-	leveragePool, found = suite.app.LeveragelpKeeper.GetPool(suite.ctx, 1)
-	suite.Require().True(found)
-
-	ammPool, err = suite.app.LeveragelpKeeper.GetAmmPool(suite.ctx, 1)
-	suite.Require().NoError(err)
-	currentLeverageRatio = leveragePool.LeveragedLpAmount.ToLegacyDec().Quo(ammPool.TotalShares.Amount.ToLegacyDec())
-
-	err = suite.app.LeveragelpKeeper.ClosePositionsOnADL(suite.ctx, leveragePool, currentLeverageRatio)
-	suite.Require().Error(err, errors.New("closing ratio is <= 0 for pool while triggering adl 1"))
 
 }
