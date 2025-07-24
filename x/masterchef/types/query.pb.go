d960b625f8aec637434531d33526a00c0ca2e356
Amit Yadav <amy29981@gmail.com>
2025-07-24 23:29:41 +0530

    Use limit price in estimation query by final amount (#1394)
    * use limit price
    
    * fix

--- x/masterchef/types/query.pb.go
+++ x/masterchef/types/query.pb.go
@@ -2638,6 +2638,7 @@ func _Query_PendingRewards_Handler(srv interface{}, ctx context.Context, dec fun
 	return interceptor(ctx, in, info, handler)
 }
 
+var Query_serviceDesc = _Query_serviceDesc
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "elys.masterchef.Query",
 	HandlerType: (*QueryServer)(nil),
