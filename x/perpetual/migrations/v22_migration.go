84cebe67f8ad01acc178573004bcf233abd77704
avkr003 <abhinav@elys.network>
2025-08-22 03:26:20 +0530

    fixing for SUT

--- x/perpetual/migrations/v22_migration.go
+++ x/perpetual/migrations/v22_migration.go
@@ -8,6 +8,7 @@ import (
 func (m Migrator) V22Migration(ctx sdk.Context) error {
 	params := m.keeper.GetParams(ctx)
 	params.MinimumFundingRate = math.LegacyMustNewDecFromStr("0.1")
+	params.SecondLiquidationTriggerRatio = math.LegacyMustNewDecFromStr("0.67")
 	err := m.keeper.SetParams(ctx, &params)
 	if err != nil {
 		return err
