06a96c355356beb078e676acbee80227b923b710
avkr003 <abhinav@elys.network>
2025-10-13 20:10:29 +0530

    resetting perpetual for upgrade

---
+++ x/perpetual/migrations/v23_migration.go
@@ -0,0 +1,10 @@
+package migrations
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// Need to do this for testnet upgrade testing as some features is being removed from testnet
+func (m Migrator) V23Migration(_ sdk.Context) error {
+	return nil
+}
