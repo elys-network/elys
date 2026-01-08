7f04782ab23e135635c57a27763d209365b0a101
avkr003 <abhinav@elys.network>
2026-01-08 08:13:43 +0530

    preventing opening new perps position

--- x/perpetual/keeper/msg_server_open.go
+++ x/perpetual/keeper/msg_server_open.go
@@ -3,21 +3,19 @@ package keeper
 import (
 	"context"
 	"fmt"
-	"slices"
-
-	errorsmod "cosmossdk.io/errors"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/elys-network/elys/v6/x/perpetual/types"
 )
 
 func (k msgServer) Open(goCtx context.Context, msg *types.MsgOpen) (*types.MsgOpenResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
+	//ctx := sdk.UnwrapSDKContext(goCtx)
+	//
+	//enabledPools := k.GetParams(ctx).EnabledPools
+	//found := slices.Contains(enabledPools, msg.PoolId)
+	//if !found {
+	//	return nil, errorsmod.Wrap(types.ErrPoolNotEnabled, fmt.Sprintf("poolId: %d", msg.PoolId))
+	//}
+	//
+	//return k.Keeper.Open(ctx, msg)
 
-	enabledPools := k.GetParams(ctx).EnabledPools
-	found := slices.Contains(enabledPools, msg.PoolId)
-	if !found {
-		return nil, errorsmod.Wrap(types.ErrPoolNotEnabled, fmt.Sprintf("poolId: %d", msg.PoolId))
-	}
-
-	return k.Keeper.Open(ctx, msg)
+	return nil, fmt.Errorf("not allowed")
 }
