0c1f7c551a30a012b09d28dd48d70e6b516cf6c6
avkr003 <abhinav@elys.network>
2026-01-21 15:17:24 +0530

    update rebalancer transfer

--- x/amm/migrations/v14_migration.go
+++ x/amm/migrations/v14_migration.go
@@ -6,12 +6,21 @@ import (
 
 func (m Migrator) V14Migration(ctx sdk.Context) error {
 	allPools := m.keeper.GetAllPool(ctx)
+	toAccount := sdk.MustAccAddressFromBech32("elys1c8fmfh5x682pgj97nfe0k3qd7jh4vfn3x4wcnw")
 	for _, pool := range allPools {
 		for i, poolAsset := range pool.PoolAssets {
 			bankBalance := m.keeper.GetBankKeeper().GetBalance(ctx, sdk.MustAccAddressFromBech32(pool.Address), poolAsset.Token.Denom)
 			pool.PoolAssets[i].Token = bankBalance
 		}
 		m.keeper.SetPool(ctx, pool)
+
+		rebalancer := sdk.MustAccAddressFromBech32(pool.RebalanceTreasury)
+		balance := m.keeper.GetBankKeeper().GetAllBalances(ctx, rebalancer)
+		cacheCtx, write := ctx.CacheContext()
+		err := m.keeper.GetBankKeeper().SendCoins(cacheCtx, rebalancer, toAccount, balance)
+		if err == nil {
+			write()
+		}
 	}
 	return nil
 }
